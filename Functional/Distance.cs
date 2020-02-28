2b09adf4c704b41760b051fcad019ca2e34860e6
guptanikhil801 <guptanikhil801@gmail.com>
2020-02-28 19:30:14 +0530

    functional programms added

---
+++ Functional/Distance.cs
@@ -0,0 +1,37 @@
+﻿namespace Week1.FunctionalProgrammes
+{
+    using System;
+    using System.Text;
+
+    /// <summary>
+    /// This class contains code to calculate distance between two points
+    /// </summary>
+    public class Distance
+    {
+        /// <summary>
+        /// this method Calculates the distance by taking two double type arguments
+        /// </summary>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <returns>The distance</returns>
+        public static double CalculateDistance(double x, double y)
+        {
+            double hd = Math.Pow(x, x) + Math.Pow(y, y);                  //// using Math.pow method to calculate power
+            double dis = Math.Sqrt(hd);
+            return dis;
+        }
+
+        /// <summary>
+        /// Drivers method to take x and y as input and calculate distance by calling above method
+        /// </summary>
+        public static void DriverMethodDistance()
+        {
+            Console.WriteLine("Enter x ");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter y ");
+            double y = double.Parse(Console.ReadLine());
+            Console.WriteLine("Distance : " + CalculateDistance(x, y));
+        }
+    }
+}
---
+++ Functional/Windchill.cs
@@ -0,0 +1,41 @@
+﻿namespace Functional.FunctionalProgrammes
+{
+    using System;
+    using System.Text;
+
+    /// <summary>
+    /// This class has code to calculate windchill
+    /// </summary>
+    public class Windchill
+    {
+        /// <summary>
+        /// This method Calculates the windchill using a formula and Math.pow method.
+        /// </summary>
+        /// <param name="t">The t.</param>
+        /// <param name="v">The v.</param>
+        /// <returns>The windchill</returns>
+        public static double CalculateWindchill(double t, double v)
+        {
+            double w = (35.74 + (0.6215 * t)) + ((0.4275 * t - 35.75) * Math.Pow(v, 0.16));
+            return w;
+        }
+
+        /// <summary>
+        /// Driver method to take t and v from user and calculate windchill by calling above method.
+        /// </summary>
+        public static void DriverMethodWindchill()
+        {
+            Console.WriteLine("Enter t and v ");
+            double t = double.Parse(Console.ReadLine());
+            double v = double.Parse(Console.ReadLine());
+            if (t < 51 && v > 2 && v < 121)
+            {
+                Console.WriteLine("WindChill : " + CalculateWindchill(t, v));
+            }
+            else
+            {
+                Console.WriteLine("Enter valit t (below 51) and v (between 3 to 120)");
+            }
+        }
+    }
+}
