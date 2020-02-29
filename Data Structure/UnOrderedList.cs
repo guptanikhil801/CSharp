8eb2ee9f91d4663f94266a674a38a0788b0bec51
guptanikhil801 <guptanikhil801@gmail.com>
2020-02-29 13:01:06 +0530

    comments added for classes

--- Data Structure/UnOrderedList.cs
+++ Data Structure/UnOrderedList.cs
@@ -1,12 +1,26 @@
 ﻿namespace DataStructure.DS_Programmes
 {
     using System;
+    using System.Collections.Generic;
     using System.Text;
+
+    /// <summary>
+    /// List class created to use generics
+    /// </summary>
+    /// <typeparam name="T">t type </typeparam>
     class List <T>
     {
+
+        /// <summary>
+        /// Node class for making Node data type
+        /// </summary>
         class Node
         {
             T data;
+            Node next;
         }
+
+        
+
     }
 }
