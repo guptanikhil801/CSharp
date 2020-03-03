9e3c1cb1bccd9462557af38df6c7bdfb552c3f25
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-03 17:54:40 +0530

    [Nikhil] Queue class created

---
+++ DS/Queue.cs
@@ -0,0 +1,13 @@
+﻿namespace DataStructure.DS_Programmes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    class Queue<T>
+    {
+        T[] queue;
+        int size;
+        int front;
+        int rear;
+    }
+}
