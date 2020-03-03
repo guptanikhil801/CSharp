9ccc552fee8b64db95468d82e0d4e2a1ae53c2aa
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-03 13:04:45 +0530

    class variable added

--- DS/StackImpl.cs
+++ DS/StackImpl.cs
@@ -5,9 +5,34 @@
     using System.Text;
 
     /// <summary>
-   /// This class contains code for implementation of stack
+    /// This class contains code for implementation of stack
     /// </summary>
-    class StackImpl
+    public class Stack<T>
     {
-    }
+        T[] Ele;
+        int Top;
+        int Max;
+
+        public Stack(int size)
+        {
+            Ele = new T[size];
+            Top = -1;
+            Max = size;
+        }
+       public void Push(T data)
+        { 
+          if(Top==(Max-1))
+            {
+                Console.WriteLine("Stack Overflow");
+            }
+            else
+            {
+                Ele[++Top] = data;           
+            }
+        }
+
+
+
+    }    
+
 }
