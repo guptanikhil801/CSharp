d898126277c56e4a1da09ac7e3af18bd283a9b7e
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-04 12:06:31 +0530

    [Nikhil] Ispalindrome method added in  palindromechecker class

--- DS/PalindromeChecker.cs
+++ DS/PalindromeChecker.cs
@@ -5,5 +5,27 @@
     using System.Text;
     class PalindromeChecker
     {
+        public static bool IsPalindrome(string str)
+        {
+            ////Adding elements to queue
+            Dequeue<char> dq = new Dequeue<char>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                dq.AddRear(str[i]);
+            }
+            while(!dq.IsEmpty())
+            {
+                if(dq.Size()==1)
+                {
+                    break;
+                }
+                if(!(dq.RemoveFront().Equals(dq.RemoveRear())))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
