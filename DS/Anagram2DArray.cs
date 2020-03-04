23796c8adb4646c77d67e23db9b8bc36ca818364
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-04 22:53:25 +0530

    [Nikhil]refactor anagramdetectionstring() method added for class prime2DArray

--- DS/Anagram2DArray.cs
+++ DS/Anagram2DArray.cs
@@ -19,5 +19,31 @@
             return true;
         }
 
+        public static bool AnagramDetectionString(string str1, string str2)
+        {
+            //// check the length of both string
+            if (str1.Length != str2.Length)
+            {
+                return false;                       //// if length is not same return false
+            }
+
+            //// convert both string into character array and sort array using Array.sort method
+            char[] chstr1 = str1.ToCharArray();
+            char[] chstr2 = str2.ToCharArray();
+            Array.Sort(chstr1);
+            Array.Sort(chstr2);
+
+            //// After sorting check the value at both array index is same or not 
+            for (int i = 0; i < chstr1.Length; i++)
+            {
+                if (chstr1[i] != chstr2[i])
+                {
+                    return false;                   //// if value is not same return false
+                }
+            }
+
+            return true;                            //// if value is same return true
+        }
+
     }
 }
