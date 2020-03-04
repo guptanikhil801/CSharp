1f86b5bdfb4a61eec886bb73ab1e577afd468d19
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-04 22:48:53 +0530

    [Nikhil] new class created for prime2DArray along with a method

---
+++ DS/Anagram2DArray.cs
@@ -0,0 +1,23 @@
+﻿namespace DataStructure.DS_Programmes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    class Anagram2DArray
+    {
+        public static bool CheckPrime(int num)
+        {
+            int m = num / 2;
+            for (int i = 2; i <= m; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+    }
+}
