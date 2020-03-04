57d2bb98d9f9f36ebd7b70125c6b4a6dc70fe4c6
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-04 12:52:48 +0530

    [Nikhil]new Prime2Dimensional classs created

---
+++ DS/Prime2dimensional.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure.DS_Programmes
+{
+    class Prime2dimensional
+    {
+        public static bool CheckPrime(int num)
+        {
+            int m = num / 2;
+            //// the loop will continue till half of number
+            for (int i = 2; i <= m; i++)
+            {
+                //// if number is divisible by i value then returns false
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;       //// otherwise if not divisible then true
+        }
+    }
+}
