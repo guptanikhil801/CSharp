d20d2896396ad5d8412cb3fdd138bd09bdf22c3e
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-04 16:21:17 +0530

    [Nikhil] StorePrimeToArray method added in  Prime2Dimensional

--- DS/Prime2dimensional.cs
+++ DS/Prime2dimensional.cs
@@ -21,5 +21,50 @@ namespace DataStructure.DS_Programmes
 
             return true;       //// otherwise if not divisible then true
         }
+
+        public static int CountPrime()
+        {
+            int range1 = 0;
+            int range2 = 100;
+            int count = 0;
+            for (int i = range1; i < range2; i++)
+            {
+                if (CheckPrime(i))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public int[,] StorePrimeToArray()
+        {
+            int[,] pArray = new int[20, 30];
+            int m = 0, n = 100;
+            for (int i = 0; i < pArray.GetLength(0) && n < 1001; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    for (int j = m, k = 0; j < n; j++)
+                    {
+                        if (CheckPrime(j))
+                        {
+                            pArray[i, k++] = j;
+                        }
+                    }
+                    m = n;
+                    n = n + 100;
+                }
+                else
+                {
+                    pArray[i, 0] = m;
+                    pArray[i, 1] = n;
+                }
+            }
+
+            return pArray;
+        }
+
     }
 }
