ef01201cb2c8d458fcbfa724c705cad8b464574b
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-03-05 14:36:37 +0530

    [Nikhil] new Hashing class created

---
+++ DS/Hashing.cs
@@ -0,0 +1,17 @@
+﻿namespace DataStructure
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    class Hashing
+    {
+        public const int Size = 10;
+        public class Node<T>
+        {
+            public T data;
+            public Node<T> next;
+        }
+
+
+    }
+}
