524bd1e678f9f64574de7f5de6d75243abee2542
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-03-05 15:08:42 +0530

    [Nikhil] refactor AddData() method added in Hashing class

--- DS/Hashing.cs
+++ DS/Hashing.cs
@@ -8,10 +8,52 @@
         public const int Size = 10;
         public class Node<T>
         {
-            public T data;
-            public Node<T> next;
+            public T Data;
+            public Node<T> Next;
         }
 
+        private static void AddData(Node<int>[] hashTable, int item)
+        {
+            Node<int> node = new Node<int>();
+            node.Data = item;
+            node.Next = null;
+            int temp = item % (Size + 1);
+            if (hashTable[temp] == null)
+            {
+                hashTable[temp] = node;
+            }
+            else
+            {
+                Node<int> head = hashTable[temp];
+                Node<int> prev = hashTable[temp];
+                Node<int> t = hashTable[temp];
+                while(t.Next!=null)
+                {
+                    if(node.Data<head.Data)
+                    {
+                        node.Next = head;
+                        hashTable[temp] = node;
+                        break;
+                    }
+                    else
+                    {
+                        prev = head;
+                        head = head.Next;
+                    }
 
+                    t = t.Next;
+                }
+
+                if(t.Next == null && head.Data<node.Data)
+                {
+                    head.Next = node;
+                }
+                else if (t.Next == null && head.Data>node.Data)
+                {
+                    node.Next = prev;
+                    hashTable[temp] = node;
+                }
+            }
+        }
     }
 }
