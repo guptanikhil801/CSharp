7be29499ae4a497d176d01b49e46c146315ddd44
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-03-06 10:46:50 +0530

    [Nikhil] refactor added Display() method

--- DS/AnagramStack.cs
+++ DS/AnagramStack.cs
@@ -26,5 +26,23 @@
             node.Next = Top;
             Top = node;
         }
+
+        public void Display()
+        {
+            if(Top==null)
+            {
+                Console.WriteLine("Stack underflow");
+                return;
+            }
+            else
+            {
+                Node temp = Top;
+                while(temp!=null)
+                {
+                    Console.Write(temp.Data + " ");
+                    temp = temp.Next;
+                }
+            }
+        }
     }
 }
