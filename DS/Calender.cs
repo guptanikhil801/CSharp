39fb43d3a7e57a861ef1565ec78de8c30bdc6928
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-03-06 15:56:42 +0530

    [Nikhil] refactor MakeCalender() method added in Calender.cs

--- DS/Calender.cs
+++ DS/Calender.cs
@@ -5,6 +5,7 @@
     using System.Text;
     public class Calender
     {
+        public static string[] month = { "", "january", "Febuary", "March", "April", "May", "Jun", "July", "August", "September", "October", "November", "December" };
         public static bool IsLeap(int year)
         {
             if ((year % 400 == 0) || ((year % 100 != 0) && (year % 4 == 0)))
@@ -22,6 +23,30 @@
             int d0 = (d + x + 31 * m0 / 12) % 7;
             return d0;
         }
+        public static int[,] MakeCaleneder(int m, int y)
+        {
+            int[] daysinmonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            if (IsLeap(y))
+            {
+                daysinmonth[2] = 29;
+            }
+            int[,] cal = new int[2, daysinmonth[m] + 1];
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 1; j < cal.GetLength(1); j++)
+                {
+                    if (i == 0 && j <= 6)
+                    {
+                        cal[i, j] = j;
+                    }
+                    if (i == 1)
+                    {
+                        cal[i, j] = j;
+                    }
+                }
+            }
+            return cal;
+        }
     }
-   
+
 }
