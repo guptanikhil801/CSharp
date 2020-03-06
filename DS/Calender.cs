c63db2f398d173711b05c28784191936c89bfe8f
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-03-06 22:40:46 +0530

    [Nikhil] final modification done

--- DS/Calender.cs
+++ DS/Calender.cs
@@ -1,22 +1,18 @@
-﻿namespace DataStructure
+﻿namespace DataStructure.DS_Programmes
 {
     using System;
     using System.Collections.Generic;
     using System.Text;
-
-    /// <summary>
-    /// This class contains code to print a calender using 2D Array
-    /// </summary>
     public class Calender
     {
-        public static string[] monthstr = { "", "january", "Febuary", "March", "April", "May", "Jun", "July", "August", "September", "October", "November", "December" };
+        private static string[] monthstr = { "", "january", "Febuary", "March", "April", "May", "Jun", "July", "August", "September", "October", "November", "December" };
 
         /// <summary>
         /// This method checks a year is leap year or not
         /// </summary>
         /// <param name="year">year to be checked</param>
         /// <returns></returns>
-        public static bool IsLeap(int year)
+        private static bool IsLeap(int year)
         {
             if ((year % 400 == 0) || ((year % 100 != 0) && (year % 4 == 0)))
             {
@@ -33,7 +29,7 @@
         /// <param name="m">the month</param>
         /// <param name="y">the year</param>
         /// <returns>the day number</returns>
-        public static int DayOffWeek(int d, int m, int y)
+        private static int DayOffWeek(int d, int m, int y)
         {
             int y0 = y - (14 - m) / 12;
             int x = y0 + (y0 / 4) - (y0 / 100) + (y0 / 400);
@@ -42,11 +38,16 @@
             return d0;
         }
 
-
-        public static int MonthLength(int month, int year)
+        /// <summary>
+        /// returns  length of Monthes
+        /// </summary>
+        /// <param name="month">The month.</param>
+        /// <param name="year">The year.</param>
+        /// <returns>total day in a month</returns>
+        private static int MonthLength(int month, int year)
         {
-            
-            int monthdays=0;
+
+            int monthdays = 0;
             if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
             {
                 monthdays = 31;
@@ -75,12 +76,12 @@
         /// <param name="m">the month</param>
         /// <param name="y">the yea</param>
         /// <returns>whole calender</returns>
-        public static int[,] MakeCalender(int mon, int ye)
+        private static int[,] MakeCalender(int mon, int ye)
         {
-
+            
             int md = MonthLength(mon, ye);
 
-            int [,] cal = new int[1, md];
+            int[,] cal = new int[1, md];
             for (int k = 1; k <= md; k++)
             {
                 for (int i = 0; i < cal.GetLength(0); i++)
@@ -96,35 +97,51 @@
             return cal;
         }
 
-        public static void DisplayArray(int[,] arr, int mo, int yea)
+        /// <summary>
+        /// Displays the array.
+        /// </summary>
+        /// <param name="arr">The array.</param>
+        /// <param name="mo">The month.</param>
+        /// <param name="yea">The yeaar.</param>
+        private static void DisplayArray(int[,] arr, int mo, int yea)
         {
+            int sp = DayOffWeek(1, mo, yea);
             int ml = MonthLength(mo, yea);
+            int d = 0;
+            for (int l = 0; l < sp; l++)
+            {
+                d++;
+                Console.Write(" ");                 ////printing space before date
+            }
+
             for (int i = 0; i < arr.GetLength(0); i++)
             {
                 for (int j = 0; j < arr.GetLength(1); j++)
                 {
-                    if (arr[i, j] != 0&& arr[i,j]<=ml)
+                    if (arr[i, j] != 0 && arr[i, j] <= ml)
                     {
-                        if(j%6==0)
+                        if (d % 7 == 0)
                         {
-                            Console.WriteLine();
+                            Console.WriteLine();          //// for next line
                         }
+                        d++;
                         if (arr[i, j] < 10)
                         {
                             Console.Write(arr[i, j] + "  ");
+                           
                         }
                         else
                         {
-                            Console.Write(arr[i, j] + " ");
+                            Console.Write(arr[i, j] +" ");
                         }
-                                                                             //// for printing value
                     }
                 }
-
-                
             }
         }
 
+        /// <summary>
+        /// Driver method for the calender.
+        /// </summary>
         public static void DriverCalender()
         {
             Console.WriteLine("Enter month");
@@ -133,29 +150,33 @@
             int year = int.Parse(Console.ReadLine());
             int sp = DayOffWeek(1, month, year);
             Console.Clear();
-           
+
             char[] week = { 'S', 'M', 'T', 'W', 'T', 'F', 'S' };
 
-            
+            Console.WriteLine("Calender Using 2D Matrix");
             int[,] ar = MakeCalender(month, year);
-            Console.WriteLine(monthstr[month] + "   " + year);
+            Console.WriteLine(monthstr[month] + "    " + year);
             for (int i = 0; i < week.Length; i++)
             {
-                Console.Write(week[i] + " ");
+                Console.Write(week[i] + "  ");
             }
             Console.WriteLine();
-            
-            for (int a = 0;a<sp;a++)
+
+            for (int a = 0; a < sp; a++)
             {
                 Console.Write("  ");
             }
-            
-            DisplayArray(ar,month,year);
+
+            DisplayArray(ar, month, year);
 
         }
 
     }
-
-
 }
 
+/*for(int l=0;i<sp1;l++)
+ * {
+ * arr[i, j] != 0
+ * }
+ * 
+ */
