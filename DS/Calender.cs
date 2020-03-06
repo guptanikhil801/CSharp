ac759a6c5200bbb2739dbb83475747ac5732c863
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-03-06 14:58:42 +0530

    [Nikhil] refactor IsLeap() method added in Calender.cs

--- DS/Calender.cs
+++ DS/Calender.cs
@@ -3,7 +3,17 @@
     using System;
     using System.Collections.Generic;
     using System.Text;
-   public class Calender
+    public class Calender
     {
+        public static bool IsLeap(int year)
+        {
+            if ((year % 400 == 0) || ((year % 100 !=  0) && (year % 4 == 0)))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
     }
 }
