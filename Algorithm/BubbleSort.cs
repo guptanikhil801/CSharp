b805d4cb16e4e8c7c19afd49890b6b540a2e685e
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-09 15:13:56 +0530

    folder added

---
+++ Algorithm/BubbleSort.cs
@@ -0,0 +1,56 @@
+﻿namespace Algorithm
+{
+    using System;
+    using System.Text;
+
+    /// <summary>
+    /// This class has code to sort the integer in an array by using bubble sort Algorithm
+    /// </summary>
+    public class BubbleSort
+    {
+        /// <summary>
+        /// This method takes integer type array and does sorting.
+        /// </summary>
+        /// <param name="intarr">The integer type array.</param>
+        /// <returns>sorted integer array</returns>
+        public static int[] DoBubbleSort(int[] intarr)
+        {
+            for (int i = 0; i < intarr.Length; i++)
+            {
+                int temp;
+                for (int j = 0; j < intarr.Length - 1; j++)
+                {
+                    if (intarr[j] > intarr[j + 1])
+                    {
+                        temp = intarr[j];
+                        intarr[j] = intarr[j + 1];
+                        intarr[j + 1] = temp;
+                    }
+                }
+            }
+
+            return intarr;
+        }
+
+        /// <summary>
+        /// Drivers method to take integer in an array and after that sort the array by calling above method.
+        /// </summary>
+        public static void DriverBubbleSort()         // program-4(Algorithm)
+        {
+            Console.WriteLine("enter Array size");
+            int l = int.Parse(Console.ReadLine());
+            int[] arr = new int[l];
+            Console.WriteLine("Enter integer numbers one by one ");
+            for (int i = 0; i < l; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            DoBubbleSort(arr);
+            for (int j = 0; j < l; j++)
+            {
+                Console.Write(arr[j] + " ");
+            }
+        }
+    }
+}
---
+++ Algorithm/MergeSortAlgorithm.cs
@@ -0,0 +1,115 @@
+﻿namespace Algorithm
+{
+    using System;
+    using System.Text;
+
+    /// <summary>
+    /// This class contains code to sorting of integer by using merge sort algorithm.
+    /// </summary>
+    public class MergeSortAlgorithm
+    {
+        /// <summary>
+        /// This method merges the 
+        /// </summary>
+        /// <param name="arr">The array.</param>
+        /// <param name="l">The l.</param>
+        /// <param name="m">The m.</param>
+        /// <param name="r">The r.</param>
+        public static void Merge(int[] arr, int l, int m, int r)
+        {
+            int n1 = m - l + 1;
+            int n2 = r - m;
+
+            int[] left = new int[n1];
+            int[] right = new int[n2];
+
+            for (int f = 0; f < n1; ++f)
+            {
+                left[f] = arr[l + f];
+            }
+
+            for (int d = 0; d < n2; ++d)
+            {
+                right[d] = arr[m + 1 + d];
+            }
+
+            int i = 0, j = 0;
+            int k = l;
+            while (i < n1 && j < n2)
+            {
+                if (left[i] <= right[j])
+                {
+                    arr[k] = left[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = right[j];
+                    j++;
+                }
+
+                k++;
+            }
+
+            while (i < n1)
+            {
+                arr[k] = left[i];
+                i++;
+                k++;
+            }
+
+            while (j < n2)
+            {
+                arr[k] = right[j];
+                j++;
+                k++;
+            }
+        }
+
+        /// <summary>
+        /// It Sorts the specified arr.
+        /// </summary>
+        /// <param name="arr"> array.</param>
+        /// <param name="l">The l.</param>
+        /// <param name="r">The r.</param>
+        public static void Sort(int[] arr, int l, int r)
+        {
+            if (l < r)
+            {
+                int m = (l + r) / 2;
+
+                Sort(arr, l, m);
+                Sort(arr, m + 1, r);
+
+                Merge(arr, l, m, r);
+            }
+        }
+
+        /// <summary>
+        /// it Prints the array after sorting.
+        /// </summary>
+        /// <param name="arr">the array .</param>
+        public static void PrintArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+            {
+                Console.Write(arr[i] + " ");
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// Drivers  method to do merge sort and display sorted arrays.
+        /// </summary>
+        public static void DriverMethodMergeSort()
+        {
+            int[] arr = { 98, 101, 43, 15, 89, 71 };          
+            Console.WriteLine("Given Array is");
+            PrintArray(arr);
+            Sort(arr, 0, arr.Length - 1);
+            Console.WriteLine("After Sorting array is : ");
+            PrintArray(arr);
+        }
+    }
+}
