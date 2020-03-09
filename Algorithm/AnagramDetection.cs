684968ef33672a7efa4950ff19d828b5c1ea2444
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-03-09 15:33:23 +0530

    [Nikhil] All file added in Algorithm branch

---
+++ Algorithm/AnagramDetection.cs
@@ -0,0 +1,62 @@
+﻿namespace Algorithm
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// This class contains code to check two string is anagram or not
+    /// </summary>
+    public class AnagramDetection
+    {
+        /// <summary>
+        /// This method check two string is anagram or not by converting it to char array and then sorting and checks.
+        /// </summary>
+        /// <param name="str1">The STR1.</param>
+        /// <param name="str2">The STR2.</param>
+        /// <returns>bool value true or false</returns>
+        public static bool AnagramDetectionString(string str1, string str2)
+        {
+            //// check the length of both string
+            if (str1.Length != str2.Length)
+            {
+                return false;                       //// if length is not same return false
+            }
+
+            //// convert both string into character array and sort array using Array.sort method
+            char[] chstr1 = str1.ToCharArray();
+            char[] chstr2 = str2.ToCharArray();
+            Array.Sort(chstr1);
+            Array.Sort(chstr2);
+
+            //// After sorting check the value at both array index is same or not 
+            for (int i = 0; i < chstr1.Length; i++)
+            {
+                if (chstr1[i] != chstr2[i])
+                {
+                    return false;                   //// if value is not same return false
+                }
+            }
+
+            return true;                            //// if value is same return true
+        }
+
+        /// <summary>
+        /// Drivers  method to take two input and check anagram .
+        /// </summary>
+        public static void DriverMethodAnagramString()  //// program-6(Algorithm)
+        {
+            Console.WriteLine("Enter 1st and second word ");
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+            if (AnagramDetectionString(str1, str2))
+            {
+                Console.WriteLine("It is Anagram ");
+            }
+            else
+            {
+                Console.WriteLine("it is not anagram");
+            }
+        }
+    }
+}
---
+++ Algorithm/StringPermutationRecursion.cs
@@ -0,0 +1,64 @@
+﻿namespace Week1.FunctionalProgrammes
+{
+    using System;
+    using System.Text;
+
+    /// <summary>
+    /// This class has code to print possible permutation of a string
+    /// </summary>
+    public class StringPermutationRecursion
+    {
+        /// <summary>
+        /// This method Swaps the character.
+        /// </summary>
+        /// <param name="a">string type input</param>
+        /// <param name="i">starting index</param>
+        /// <param name="j">last index.</param>
+        /// <returns>The string</returns>
+        public static string SwapChar(string a, int i, int j)
+        {
+            char temp;                                //// char type temporary variable to swap i and j
+            char[] charr = a.ToCharArray();           //// converting string into char array
+            temp = charr[i];                         //// swapping
+            charr[i] = charr[j];
+            charr[j] = temp;
+            string st = new string(charr);
+            return st;
+        }
+
+        /// <summary>
+        /// This method Permutes the specified string.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <param name="l">The l.</param>
+        /// <param name="r">The r.</param>
+        public static void Permute(string str, int l, int r)
+        {
+            if (l == r)
+            {
+                Console.WriteLine(str);
+            }
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    str = SwapChar(str, l, i);
+                    Permute(str, l + 1, r);
+                    str = SwapChar(str, l, i);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Drivers method to take string input from user and print possible permutation.
+        /// </summary>
+        public static void DriverMethodPermutation()
+        {
+            Console.WriteLine("enter string");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("permutations are");
+            int sl = str1.Length - 1;
+            Permute(str1, 0, sl);
+        }
+    }
+}
