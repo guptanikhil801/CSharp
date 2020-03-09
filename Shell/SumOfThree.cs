58b9c708526232faed3e6180b5683ccd29e39e2a
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-09 15:55:45 +0530

    Shell programs added

---
+++ Shell/SumOfThree.cs
@@ -0,0 +1,43 @@
+﻿namespace Week1.FunctionalProgrammes
+{
+    using System;
+    using System.Text;
+
+    /// <summary>
+    /// This class contains code to check in an int array find triplets that sums equal to zero
+    /// </summary>
+     public class SumOfThree
+    {
+        /// <summary>
+        /// Drivers method to take input integers and calculate sum of three.
+        /// </summary>
+        public static void DriverMethodSumOfThree()
+        {
+            Console.WriteLine("enter number of elements ");
+            int s = int.Parse(Console.ReadLine());
+            int[] intarr = new int[s];
+            Console.WriteLine("Enter Array Elements");
+
+            for (int k = 0; k < s; k++)
+            {
+                intarr[k] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("sum of three adds to zero are :");
+
+            for (int i = 0; i < s - 2; i++)
+            {
+                for (int j = i + 1; j < s - 1; j++)
+                {
+                    for (int k = j + 1; k < s; k++)
+                    {
+                        if (intarr[i] + intarr[j] + intarr[k] == 0)
+                        {
+                            Console.WriteLine(intarr[i] + " " + intarr[j] + " " + intarr[k]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
