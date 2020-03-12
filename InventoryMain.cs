cb030b404f6f258a4b27c23dc6505afe12fdc223
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-12 17:28:20 +0530

    [Nikhil] refactor SerializationInventory() method added in inventoryMain.cs

--- InventoryMain.cs
+++ InventoryMain.cs
@@ -3,19 +3,43 @@
     using System;
     using System.Collections.Generic;
     using System.Text;
-   public class InventoryMain:Inventory
+    using Newtonsoft.Json;
+    using System.Text.Json.Serialization;
+    using System.IO;
+    public class InventoryMain
     {
-        public static void DriverInventory()
+        public static string SerializationInventory()
         {
-            Inventory id = new Inventory();
-            Rice rice = new Rice()
-            {
-                Name = "Daawat",
-                Weight = 3,
-                Price = 105.0
-            };
-            
-            
+            Inventory ivt = new Inventory();
+            FoodItems fi = new FoodItems();
+            ivt.Rice = new List<FoodItems>();
+            fi.Name = "Daawat";
+            fi.Price = 145;
+            fi.Weight = 10;
+            ivt.Rice.Add(fi);
+
+            FoodItems fip = new FoodItems();
+            ivt.Pulses = new List<FoodItems>();
+            fip.Name = "chickpeas";
+            fip.Price = 123;
+            fip.Weight = 5;
+            ivt.Pulses.Add(fip);
+
+            FoodItems fiw = new FoodItems();
+            ivt.Weights = new List<FoodItems>();
+            fiw.Name = "Ashirvad";
+            fiw.Price = 42;
+            fiw.Weight = 10;
+            ivt.Weights.Add(fiw);
+            Console.WriteLine("serialized and file added");
+            string jsonstr = JsonConvert.SerializeObject(ivt, Formatting.Indented);
+            string filepath = @"C:\Users\yempc69\source\repos\CSharpMonth1\OopsPrograms\InventoryData\Inventory.json";
+            File.WriteAllText(filepath, jsonstr);
+            return filepath;
         }
+       
+
+        
     }
 }
+
