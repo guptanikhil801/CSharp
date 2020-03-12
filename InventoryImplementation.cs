6e13edc26b6eca109817baa2f91d5ff96d59359f
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-12 18:28:36 +0530

    [Nikhil] new class added in  InventoryImplementation.cs

--- InventoryImplementation.cs
+++ InventoryImplementation.cs
@@ -3,7 +3,20 @@
     using System;
     using System.Collections.Generic;
     using System.Text;
+    public class InventoryM : InventoryData.Inventory
+    {
+        public int Sum { get; set; }
+    }
+    public class FoodItemsM : InventoryData.FoodItems
+    {
+        public int TotalPrice;
+        public override string ToString()
+        {
+            return string.Format("name:\t{0}\nPrice per KG:\t{1}\nWeight:\t{2}\nTotalPrice:\t{3}", this.Name, this.Price, this.Weight, this.TotalPrice);
+        }
+    }
     public class InventoryImplementation
     {
+
     }
 }
