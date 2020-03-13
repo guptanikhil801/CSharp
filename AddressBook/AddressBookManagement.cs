9837a05c3c33939f2a2a7c66b6e651f133f70b15
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-13 18:53:58 +0530

    [Nikhil] new class added AddressBookManagement.cs

---
+++ AddressBook/AddressBookManagement.cs
@@ -0,0 +1,34 @@
+﻿namespace OopsPrograms.AddressBook
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class AddressBookManagement
+    {
+        public static void DriverAddressBook()
+        {
+            AddressBookFactory abf = new AddressBookFactory();
+            Console.WriteLine("enter path");
+            string path = Console.ReadLine();
+            Console.WriteLine("Enter your choice");
+            Console.WriteLine("1:   Add,   2:    Delete,    3:     Edit,     4:    Display All Contact");
+            int choice = int.Parse(Console.ReadLine());
+            switch(choice)
+            {
+                case 1:abf.Add(path);
+                    break;
+                case 2:
+                    abf.Delete(path);
+                    break;
+                case 3:
+                    abf.Edit(path);
+                    break;
+                case 4:
+                    abf.DisplayContact(path);
+                    break;
+                default: Console.WriteLine("invalid choice");
+                    break;
+            }
+        }
+    }
+}
