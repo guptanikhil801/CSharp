d2c1512d470462f9f50863c175e4dca0e54970af
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-13 12:31:55 +0530

    [Nikhil] refactor person class added in AddressBookFactory.cs

--- AddressBook/AddressBookFactory.cs
+++ AddressBook/AddressBookFactory.cs
@@ -3,6 +3,17 @@
     using System;
     using System.Collections.Generic;
     using System.Text;
+    
+    public class Person
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+    }
+
     public class AddressBookFactory
     {
     }
