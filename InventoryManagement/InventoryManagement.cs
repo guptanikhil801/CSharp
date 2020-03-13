ea94d4e380055993699925223eb972357090ecd4
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-03-13 11:05:51 +0530

    [Nikhil] New file driver class added  InventoryManagement.cs

---
+++ InventoryManagement/InventoryManagement.cs
@@ -0,0 +1,33 @@
+﻿namespace OopsPrograms.InventoryManagement
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class InventoryManagement
+    {
+        public static void DriverInventoryManagement()
+        {
+            Console.WriteLine("Enter file path");
+            string filepath = Console.ReadLine();
+            InventoryFactory ifi = new InventoryFactory();
+            Console.WriteLine("enter your choice");
+            Console.WriteLine("A:     Add,  D:     Delete      O:     output");
+            char choice = char.Parse(Console.ReadLine());
+            switch(choice) {
+                case 'A':
+                    ifi.Add(filepath);
+                    break;
+                case 'D':
+                    ifi.Delete(filepath);
+                    break;
+                case 'O':
+                    ifi.DisplayInventory(filepath);
+                    break;
+                default:
+                    Console.WriteLine("invalid choice");
+                    break;
+            }
+        }
+    }
+}
