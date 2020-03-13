ede497419e20a8fcf32b8ba9e01ef80551231a8e
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-13 18:06:00 +0530

    [Nikhil] place of classes change and file header added in AddressBookFactory.cs

--- AddressBook/AddressBookFactory.cs
+++ AddressBook/AddressBookFactory.cs
@@ -1,28 +1,16 @@
-﻿namespace OopsPrograms.AddressBook
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AddressBookFactory.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace OopsPrograms.AddressBook
 {
     using System;
     using System.Collections.Generic;
     using System.IO;
     using System.Text;
     using Newtonsoft.Json;
-
-    public class Contact
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string Zip { get; set; }
-
-        public override string ToString()
-        {
-            return string.Format("FirtsName:   {0}\nlastName:    {1}\n" +
-                "phoneNumber: {2}\ncity:        {3}\nstate:       {4}\nzip:         {5}\naddress:     {6}"
-                , FirstName, LastName, PhoneNumber, City, State, Zip, Address);
-        }
-    }
 
     public class AddressBookFactory
     {
@@ -148,6 +136,23 @@
                 Console.WriteLine();
             }
 
+        }
+    }
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("FirtsName:   {0}\nlastName:    {1}\n" +
+                "phoneNumber: {2}\ncity:        {3}\nstate:       {4}\nzip:         {5}\naddress:     {6}"
+                , FirstName, LastName, PhoneNumber, City, State, Zip, Address);
         }
     }
 }
