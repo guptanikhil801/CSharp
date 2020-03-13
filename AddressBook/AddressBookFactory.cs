0b717e31efdd37de635497d1c09914e77a74e736
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-13 12:37:46 +0530

    [Nikhil] refactoroverriden tostring method in person class  in AddressBookFactory.cs

--- AddressBook/AddressBookFactory.cs
+++ AddressBook/AddressBookFactory.cs
@@ -6,12 +6,20 @@
     
     public class Person
     {
-        public string Name { get; set; }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
         public string State { get; set; }
         public string Zip { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("FirtsName:   {0}\nlastName:    {1}\n" +
+                "phoneNumber: {2}\ncity:        {3}\nstate:       {4}\nzip:         {5}\naddress:     {6}"
+                , FirstName, LastName, PhoneNumber, City, State, Zip, Address);
+        }
     }
 
     public class AddressBookFactory
