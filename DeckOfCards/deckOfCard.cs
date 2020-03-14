7aacc21d39be0ed3aeb0cd669cca371b02718adc
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-14 18:07:07 +0530

    [Nikhil]new final modification done

--- DeckOfCards/deckOfCard.cs
+++ DeckOfCards/deckOfCard.cs
@@ -1,50 +1,84 @@
-﻿namespace OopsPrograms.DeckOfCards
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DeckOfCard.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace OopsPrograms.DeckOfCards
 {
     using System;
     using System.Collections.Generic;
     using System.Text;
+
     /// <summary>
-    ///main class of cards 
+    /// main class of cards 
     /// </summary>
-    public class DeckOfCards
+    public class DeckOfCard
     {
-        private Card[] deck;
-        private int currentcard;
+        /// <summary>
+        /// to store value of total cards
+        /// </summary>
         private readonly int TotalCards = 52;
+       
+        /// <summary>
+        /// card type deck array
+        /// </summary>
+        private Card[] deck;
+
+        /// <summary>
+        /// to refer current card
+        /// </summary>
+        private int currentcard;
+
+        /// <summary>
+        /// to create random numbers
+        /// </summary>
         private Random randomNum;
-        public DeckOfCards()
+
+        /// <summary>
+        /// Initializes a new instance of the DeckOfCard class
+        /// </summary>
+        public DeckOfCard()
         {
-            string[] ranks = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king" };
-            string[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
-            deck = new Card[TotalCards];
-            currentcard = 0;
-            randomNum = new Random();
-            for (int i = 0; i < deck.Length; i++)
+            string[] ranks = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king" };             //// assigning all ranks to a array.
+            string[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };             //// assigning all suits to a array.
+            this.deck = new Card[this.TotalCards];             //// initializing deck[] of type card
+            this.currentcard = 0;             //// initializing value of current card
+            this.randomNum = new Random();
+            for (int i = 0; i < this.deck.Length; i++)
             {
-                deck[i] = new Card(ranks[i % 13], suits[i / 13]);
+                this.deck[i] = new Card(ranks[i % 13], suits[i / 13]);               ////  storing deck array with cards
             }
         }
+
+        /// <summary>
+        /// method to shuffle the cards 
+        /// </summary>
         public void Shuffle()
         {
-            currentcard = 0;
-            for (int first = 0; first < deck.Length; first++)
+            this.currentcard = 0;
+            for (int first = 0; first < this.deck.Length; first++)
             {
-                int second = randomNum.Next(52);
-                Card temp = deck[first];
-                deck[first] = deck[second];
+                int second = this.randomNum.Next(52);
+                Card temp = this.deck[first];                          //// shuffling cardsby using random numbers
+                this.deck[first] = this.deck[second];
             }
         }
+
+        /// <summary>
+        /// method to distribute cards
+        /// </summary>
+        /// <returns>the card</returns>
         public Card DistributeCard()
         {
-            if (currentcard < TotalCards)
+            if (this.currentcard < this.TotalCards)
             {
-                return deck[currentcard++];
+                return this.deck[this.currentcard++];
             }
             else
             {
                 return null;
             }
         }
-
     }
 }
