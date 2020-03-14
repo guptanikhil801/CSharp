28680b0b8baadb27c6fec85e1054d5c7ded617e1
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-14 09:28:27 +0530

    [Nikhil] properties added in stock class at stock.cs

--- Stock/Stock.cs
+++ Stock/Stock.cs
@@ -5,24 +5,12 @@
     using System.Text;
     public class Stock
     {
-       
         public string ShareName { get; set; }
-      
+
         public int Numberofshare { get; set; }
 
         public double SharePrice { get; set; }
         public double Stockvalue { get; set; }
-        public override string ToString()
-        {
-            return string.Format("Stockname:\t{0}\nNumber of share:\t{1}\nSharePrice:\t{2}\nTotal stock price:\t{3}", this.ShareName, this.Numberofshare, this.SharePrice, this.Stockvalue);
-        }
     }
-    public class Portfolio
-    {
-        public List<Stock> Portfoliodata;
-        public double Portfoliovalue { get; set; }
-
-    }
-
 
 }
