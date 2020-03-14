e24f6ae8bea22e331270ed69ad4b60a687cfb56b
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-14 11:34:48 +0530

    [Nikhil] new class DeckOfCards created

---
+++ DeckOfCards/deckOfCard.cs
@@ -0,0 +1,16 @@
+﻿namespace OopsPrograms.DeckOfCards
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class Card
+    {
+
+    }
+
+   public class deckOfCard
+    {
+
+    }
+}
