cdaa773b8daf71ee4fcfb8aff7dad9184d1eb159
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-14 09:45:52 +0530

    [Nikhil] refactor all header added and warnings removed from stock.cs

--- Stock/Stock.cs
+++ Stock/Stock.cs
@@ -1,4 +1,10 @@
-﻿namespace OopsPrograms.Stock
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Stock.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace OopsPrograms.Stock
 {
     using System;
     using System.Collections.Generic;
@@ -9,10 +15,30 @@
     /// </summary>
     public class Stock
     {
+        /// <summary>
+        /// Gets or sets ShareName
+        /// </summary>
         public string ShareName { get; set; }
+
+        /// <summary>
+        /// Gets or sets number of Share
+        /// </summary>
         public int Numberofshare { get; set; }
+
+        /// <summary>
+        /// Gets or sets Share price
+        /// </summary>
         public double SharePrice { get; set; }
+
+        /// <summary>
+        /// Gets or sets value of stock
+        /// </summary>
         public double Stockvalue { get; set; }
+
+        /// <summary>
+        /// to override string
+        /// </summary>
+        /// <returns>the string</returns>
         public override string ToString()
         {
             return string.Format("Stockname:\t{0}\nNumber of share:\t{1}\nSharePrice:\t{2}\nTotal stock price:\t{3}", this.ShareName, this.Numberofshare, this.SharePrice, this.Stockvalue);
@@ -24,8 +50,14 @@
     /// </summary>
     public class Portfolio
     {
+        /// <summary>
+        /// list type stock
+        /// </summary>
         public List<Stock> Portfoliodata;
-        public double Portfoliovalue { get; set; }
 
+        /// <summary>
+        /// Gets or sets value of whole portfolio
+        /// </summary>
+        public double Portfoliovalue { get; set; }
     }
 }
