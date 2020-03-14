554ee7a7b43415d310710233c64d06067ec12688
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-14 13:19:14 +0530

    [Nikhil] instance variable added and constructor initialized in DeckOfCards.cs

--- DeckOfCards/deckOfCard.cs
+++ DeckOfCards/deckOfCard.cs
@@ -19,8 +19,24 @@
         }
     }
 
-   public class DeckOfCard
+   public class DeckOfCards
     {
+        private Card[] deck;
+        private int currentcard;
+        private readonly int TotalCards = 52;
+        private Random randomNum;
 
+        public DeckOfCards()
+        {
+            string[] ranks = {"Ace","2","3","4","5","6","7","8","9","10","jack","queen","king"};
+            string[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
+            deck = new Card[TotalCards];
+            currentcard = 0;
+            randomNum = new Random();
+            for(int i =0;i<deck.Length;i++)
+            {
+                deck[i] = new Card(ranks[i % 13], suits[i / 13]);
+            }
+        }
     }
 }
