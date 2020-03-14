e83fb8dbbb5fed6ff6f50dedb6835f5b7bb98d0c
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-14 17:40:33 +0530

    [Nikhil]new final modification done

--- DeckOfCards/Card.cs
+++ DeckOfCards/Card.cs
@@ -1,21 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Card.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------------------- 
 namespace OopsPrograms.DeckOfCards
 {
-   public class Card
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// this is to card data type
+    /// </summary>
+    public class Card
     {
+        /// <summary>
+        /// for suit
+        /// </summary>
         private string Suit;
+        
+        /// <summary>
+        /// for Rank
+        /// </summary>
         private string Rank;
+        
+        /// <summary>
+        /// Initializes a new instance of card class
+        /// </summary>
+        /// <param name="cardsuit">suit of card</param>
+        /// <param name="cardrank">rank of card</param>
         public Card(string cardsuit, string cardrank)
         {
             this.Suit = cardsuit;
             this.Rank = cardrank;
         }
+        
+        /// <summary>
+        /// to override object
+        /// </summary>
+        /// <returns>the string</returns>
         public override string ToString()
         {
-            return Rank + " Of " + Suit;
+            return this.Rank + " Of " + this.Suit;
         }
     }
 }
