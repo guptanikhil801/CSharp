43fabf6b4b51154412572627dfc2b9411a8f8355
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-14 09:32:28 +0530

    [Nikhil] refactor tostring() method added in stock class at stock.cs

--- Stock/Stock.cs
+++ Stock/Stock.cs
@@ -6,11 +6,13 @@
     public class Stock
     {
         public string ShareName { get; set; }
-
         public int Numberofshare { get; set; }
-
         public double SharePrice { get; set; }
         public double Stockvalue { get; set; }
+        public override string ToString()
+        {
+            return string.Format("Stockname:\t{0}\nNumber of share:\t{1}\nSharePrice:\t{2}\nTotal stock price:\t{3}", this.ShareName, this.Numberofshare, this.SharePrice, this.Stockvalue);
+        }
     }
 
 }
