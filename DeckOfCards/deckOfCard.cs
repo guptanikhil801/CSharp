1e9948212c6e11c87213da7787a56faa2ed0e8ab
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-14 15:30:49 +0530

    [Nikhil] Distributecard method added in DeckOfCards.cs

--- DeckOfCards/deckOfCard.cs
+++ DeckOfCards/deckOfCard.cs
@@ -3,8 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.Text;
-
-   public class Card
+    public class Card
     {
         private string Suit;
         private string Rank;
@@ -18,8 +17,7 @@
             return Rank + " Of " + Suit;
         }
     }
-
-   public class DeckOfCards
+    public class DeckOfCards
     {
         private Card[] deck;
         private int currentcard;
@@ -27,12 +25,12 @@
         private Random randomNum;
         public DeckOfCards()
         {
-            string[] ranks = {"Ace","2","3","4","5","6","7","8","9","10","jack","queen","king"};
+            string[] ranks = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king" };
             string[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
             deck = new Card[TotalCards];
             currentcard = 0;
             randomNum = new Random();
-            for(int i =0;i<deck.Length;i++)
+            for (int i = 0; i < deck.Length; i++)
             {
                 deck[i] = new Card(ranks[i % 13], suits[i / 13]);
             }
@@ -40,11 +38,22 @@
         public void Shuffle()
         {
             currentcard = 0;
-            for(int first =0;first<deck.Length;first++)
+            for (int first = 0; first < deck.Length; first++)
             {
                 int second = randomNum.Next(52);
                 Card temp = deck[first];
                 deck[first] = deck[second];
+            }
+        }
+        public Card DistributeCard()
+        {
+            if (currentcard < TotalCards)
+            {
+                return deck[currentcard++];
+            }
+            else
+            {
+                return null;
             }
         }
     }
