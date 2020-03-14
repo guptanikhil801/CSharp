f3821a01a7de7861863dfbef6d75c04a22eec7d4
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-14 11:58:22 +0530

    [Nikhil] implementation done in card class of DeckOfCards.cs

--- DeckOfCards/deckOfCard.cs
+++ DeckOfCards/deckOfCard.cs
@@ -4,9 +4,19 @@
     using System.Collections.Generic;
     using System.Text;
 
-    class Card
+   public class Card
     {
-
+        private string Suit;
+        private string Rank;
+        public Card(string cardsuit, string cardrank)
+        {
+            this.Suit = cardsuit;
+            this.Rank = cardrank;
+        }
+        public override string ToString()
+        {
+            return Rank + " Of " + Suit;
+        }
     }
 
    public class DeckOfCard
