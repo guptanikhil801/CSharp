125e227d70a40e35d625e00e66f8523be399a7dc
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-16 12:31:31 +0530

    [Nikhil] new method sortedcardRanking() added

--- DeckOfCardQueue/PlayerQueue.cs
+++ DeckOfCardQueue/PlayerQueue.cs
@@ -12,7 +12,7 @@ namespace OopsPrograms.DeckOfCardQueue
     }
     public class CardsPlayersInQueue
     {
-        public static string[] CardsToString()
+        private static string[] RandomCards()
         {
             DeckOfCard dc = new DeckOfCard();
             dc.Shuffle();                               //// to shuffle cards before storing
@@ -25,6 +25,34 @@ namespace OopsPrograms.DeckOfCardQueue
                 strarr[j] = strc;                           //// in string
             }
             return strarr;
+        }
+        public static string[] SortedCardRanking()
+        {
+            string[] aftersort = new string[9];
+
+            string[] sortcards = { "Clubs Of Ace", "Diamonds Of Ace", "Hearts Of Ace", "Spades Of Ace" , "Clubs of king",
+                                 "Diamonds Of king","Hearts Of king","Spades Of king","Clubs Of queen","Diamonds Of queen", "Hearts Of queen",
+                                   "Spades Of queen","Clubs Of jack","Diamonds Of jack","Hearts Of jack","Spades Of jack",
+                            "Clubs Of 10","Diamonds Of 10","Hearts Of 10","Spades Of 10","Clubs Of 9","Diamonds Of 9","Hearts Of 9","Spades Of 9",
+                            "Clubs Of 8","Diamonds Of 8","Hearts Of 8","Spades Of 8","Clubs Of 7","Diamonds Of 7","Hearts Of 7","Spades Of 7",
+                            "Clubs Of 6","Diamonds Of 6","Hearts Of 6","Spades Of 6","Clubs Of 5","Diamonds Of 5","Hearts Of 5","Spades Of 5","Clubs Of 4",
+          "Diamonds Of 4","Hearts Of 4","Spades Of 4","Clubs Of 3","Diamonds Of 3","Hearts Of 3","Spades Of 3","Clubs Of 2","Diamonds Of 2","Hearts Of 2","Spades Of 2"};
+            string[] cts = RandomCards();
+            int l = 0;
+            for (int i = 0; i < sortcards.Length; i++)
+            {
+                for (int j = 0; j < cts.Length; j++)
+                {
+                    string s = sortcards[i].ToString();
+                    string c = cts[j].ToString();
+                    if (s.Equals(c) && (s != null) && (c != null))
+                    {
+                        aftersort[l] = c;
+                        l++;
+                    }
+                }
+            }
+            return aftersort;
         }
     }
 }
