2e99200eeb093d5cbbe1483093f9a63d693e0809
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-16 12:29:00 +0530

    [Nikhil] new method added

--- DeckOfCardQueue/PlayerQueue.cs
+++ DeckOfCardQueue/PlayerQueue.cs
@@ -10,4 +10,21 @@ namespace OopsPrograms.DeckOfCardQueue
         public Queue<string> cards;
         public Queue<string> sortedcards;
     }
+    public class CardsPlayersInQueue
+    {
+        public static string[] CardsToString()
+        {
+            DeckOfCard dc = new DeckOfCard();
+            dc.Shuffle();                               //// to shuffle cards before storing
+            Card[] cardarr = new Card[9];           //// to store 9 cards
+            string[] strarr = new string[9];
+            for (int j = 0; j < cardarr.Length; j++)
+            {
+                cardarr[j] = dc.DistributeCard();            //// storing cards to array
+                string strc = cardarr[j].ToString();
+                strarr[j] = strc;                           //// in string
+            }
+            return strarr;
+        }
+    }
 }
