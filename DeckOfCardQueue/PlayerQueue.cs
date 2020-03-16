a2fe174295490622a6e33cb3dff7ce04015aa428
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-16 12:25:11 +0530

    [Nikhil] new class PlayerQueue.cs created

---
+++ DeckOfCardQueue/PlayerQueue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OopsPrograms.DeckOfCards;
+
+namespace OopsPrograms.DeckOfCardQueue
+{
+    public class Player
+    {
+        public Queue<string> cards;
+        public Queue<string> sortedcards;
+    }
+}
