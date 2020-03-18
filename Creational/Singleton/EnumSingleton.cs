7fed93da3efa1f2f46d704ff773746880dcd68e6
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-18 17:16:38 +0530

    [Nikhil]new class Singleton.cs created

---
+++ Creational/Singleton/EnumSingleton.cs
@@ -0,0 +1,19 @@
+﻿namespace DesignPattern.Creational.Singleton
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class EnumExample
+    {
+         enum EnumSingleton
+        {
+            Instance
+        }
+    }
+}
+
+
+
+
+
