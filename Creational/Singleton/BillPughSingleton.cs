630699c6887dd106ffa4e39575499f9b777f384f
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-18 17:04:32 +0530

    [Nikhil]new class BillPughSingleton.cs created

---
+++ Creational/Singleton/BillPughSingleton.cs
@@ -0,0 +1,24 @@
+﻿namespace DesignPattern.Creational.Singleton
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+   public class BillPughSingleton
+    {
+        private BillPughSingleton()
+        {
+            Console.WriteLine("Billpugh singleton class called");
+        }
+        private static class InnerClassBillPugh
+        {
+            public static readonly BillPughSingleton InstanceOfBillPugh = new BillPughSingleton();
+        }
+        public static BillPughSingleton GetInstanceOfBillPugh()
+        {
+            
+            {
+                return BillPughSingleton.InnerClassBillPugh.InstanceOfBillPugh;
+            }
+        }
+    }
+}
