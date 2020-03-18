0365e4345c7f935b1ba5af6c7d9689a4670d1c9f
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-18 16:23:49 +0530

    [Nikhil] new class EagerSingleton created

---
+++ Creational/Singleton/EagerSingleton.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPattern.Creational.Singleton
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public sealed class EagerSingleton
+    {
+        
+    }
+}
