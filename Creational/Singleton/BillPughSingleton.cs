2a9e6bc3cee247594f37d8b34b4e0b1115ca09f5
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-18 17:14:00 +0530

    [Nikhil]refactor final modification done in BillPughSingleton.cs

--- Creational/Singleton/BillPughSingleton.cs
+++ Creational/Singleton/BillPughSingleton.cs
@@ -9,19 +9,37 @@ namespace DesignPattern.Creational.Singleton
     using System;
     using System.Collections.Generic;
     using System.Text;
-   public class BillPughSingleton
+
+    /// <summary>
+    /// Example of singleton class using Bill pugh
+    /// </summary>
+    public class BillPughSingleton
     {
+        /// <summary>
+        /// Prevents a default instance of the <see cref="BillPughSingleton"/> class from being created.
+        /// </summary>
         private BillPughSingleton()
         {
             Console.WriteLine("Billpugh singleton class called");
         }
+
+        /// <summary>
+        /// inner class 
+        /// </summary>
         private static class InnerClassBillPugh
         {
+            /// <summary>
+            /// creation of instance of bill pugh
+            /// </summary>
             public static readonly BillPughSingleton InstanceOfBillPugh = new BillPughSingleton();
         }
+
+        /// <summary>
+        /// Gets the instance of bill pugh.
+        /// </summary>
+        /// <returns>The instance</returns>
         public static BillPughSingleton GetInstanceOfBillPugh()
         {
-
             {
                 return BillPughSingleton.InnerClassBillPugh.InstanceOfBillPugh;
             }
