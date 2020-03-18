4cf39466e6bfbfa4bda935aadf8f9e3e6685a445
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-18 17:43:09 +0530

    [Nikhil]refactor final modificatio done

--- Creational/Singleton/StaticBlockInitialization.cs
+++ Creational/Singleton/StaticBlockInitialization.cs
@@ -10,22 +10,39 @@ namespace DesignPattern.Creational.Singleton
     using System.Collections.Generic;
     using System.Text;
 
+    /// <summary>
+    /// Example of singleton class using StaticBlockInitialization
+    /// </summary>
     public class StaticBlockInitialization
     {
+        /// <summary>
+        /// The instance
+        /// </summary>
         private static StaticBlockInitialization sInstance;
+
+        /// <summary>
+        /// Prevents a default instance of the <see cref="StaticBlockInitialization"/> class from being created.
+        /// </summary>
         private StaticBlockInitialization()
         {
             Console.WriteLine("singleton design pattern using static block");
         }
+
+        /// <summary>
+        /// static block for instance of class.
+        /// </summary>
         static StaticBlockInitialization()
         {
             sInstance = new StaticBlockInitialization();
         }
+
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        /// <returns>the instance</returns>
         public static StaticBlockInitialization GetInstance()
         {
             return sInstance;
         }
     }
-
 }
-
