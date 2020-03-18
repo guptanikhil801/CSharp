916df1b096bcfa3e62a59da802688d74ee402f15
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-18 17:44:11 +0530

    [Nikhil]refactor new class added

---
+++ Creational/Singleton/ThreadSafetySingleton.cs
@@ -0,0 +1,32 @@
+﻿namespace DesignPattern.Creational.Singleton
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+  public  class ThreadSafetySingleton
+    {
+        private static readonly object obj = new object();
+        private ThreadSafetySingleton()
+        {
+            Console.WriteLine("Singleton class created with thread safety using double lock");
+        }
+        private static ThreadSafetySingleton TSInstance = null;
+        public static ThreadSafetySingleton GetInstanceOfThreadSafetySingleton
+        {
+            get
+            {
+                if(TSInstance==null)
+                {
+                    lock(obj)
+                    {
+                        if(TSInstance==null)
+                        {
+                            TSInstance = new ThreadSafetySingleton();
+                        }
+                    }
+                }
+                return TSInstance;
+            }
+        }
+    }
+}
