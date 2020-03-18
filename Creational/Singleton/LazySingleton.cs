02ae076e247f7271dfbb075695d877972a582881
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-18 16:58:34 +0530

    [Nikhil]refactor commented in LazySingleton.cs

--- Creational/Singleton/LazySingleton.cs
+++ Creational/Singleton/LazySingleton.cs
@@ -3,13 +3,31 @@
     using System;
     using System.Collections.Generic;
     using System.Text;
+
+    /// <summary>
+    /// Example of singleton class using Lazy initialization
+    /// </summary>
     public sealed class LazySingleton
     {
+        /// <summary>
+        /// Prevents a default instance of the <see cref="LazySingleton"/> class from being created.
+        /// </summary>
         private LazySingleton()
         {
             Console.WriteLine("Singleton class created using Lazy Initialization ");
         }
+
+        /// <summary>
+        /// The ls instance
+        /// </summary>
         private static readonly Lazy<LazySingleton> LSInstance = new Lazy<LazySingleton>(() => new LazySingleton());
+
+        /// <summary>
+        /// Gets the instance of lazy singleton.
+        /// </summary>
+        /// <value>
+        /// instance of lazysingleton.
+        /// </value>
         public static LazySingleton getInstanceOfLazySingleton
         {
             get
