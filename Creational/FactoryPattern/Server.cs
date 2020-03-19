db88a0e494e178e10decd4847775640c2fb9cd2b
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-19 23:50:24 +0530

    [Nikhil]refactor final modification done in Server.cs

--- Creational/FactoryPattern/Server.cs
+++ Creational/FactoryPattern/Server.cs
@@ -1,36 +1,109 @@
-﻿namespace DesignPattern.Creational.FactoryPattern
+﻿// -----------------------------------------------------------------------------------------------------------
+// <copyright file="Server.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -----------------------------------------------------------------------------------------------------------
+namespace DesignPattern.Creational.FactoryPattern
 {
     using System;
     using System.Collections.Generic;
     using System.Text;
-    public class Server:Computer
+
+    /// <summary>
+    /// implementation class of IComputer
+    /// </summary>
+    /// <seealso cref="DesignPattern.Creational.FactoryPattern.IComputer" />
+    public class Server : IComputer
     {
+        /// <summary>
+        /// The Ram
+        /// </summary>
         private int ram;
+
+        /// <summary>
+        /// The Hard disk
+        /// </summary>
         private int hdd;
+
+        /// <summary>
+        /// The cpu
+        /// </summary>
         private string cpu;
+
+        /// <summary>
+        /// The manufacturer
+        /// </summary>
         private string manuf;
-        Server(int ram, int hdd, string cpu, string manuf)
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Server"/> class.
+        /// </summary>
+        /// <param name="ram">The ram.</param>
+        /// <param name="hdd">The HDD.</param>
+        /// <param name="cpu">The cpu.</param>
+        /// <param name="manuf">The manufacturer.</param>
+        public Server(int ram, int hdd, string cpu, string manuf)
         {
             this.ram = ram;
             this.hdd = hdd;
             this.cpu = cpu;
             this.manuf = manuf;
         }
-        public string cpuname()
+
+        /// <summary>
+        /// Cpu names of this instance.
+        /// </summary>
+        /// <returns>
+        /// Cpu name
+        /// </returns>
+        public string Cpuname()
         {
             return this.cpu;
         }
-        public int hddcapacity()
+
+        /// <summary>
+        /// Hard disk capacity of the instance.
+        /// </summary>
+        /// <returns>
+        /// Hard disk capacity
+        /// </returns>
+        public int Hddcapacity()
         {
             return this.hdd;
         }
-        public string manufacturer()
+
+        /// <summary>
+        /// Manufacturer of  this instance.
+        /// </summary>
+        /// <returns>
+        /// the Manufacturer
+        /// </returns>
+        public string Manufacturer()
         {
             return this.manuf;
         }
-        public int ramcapacity()
+
+        /// <summary>
+        /// Ram capacity of the instance.
+        /// </summary>
+        /// <returns>
+        /// Ram capacity
+        /// </returns>
+        public int Ramcapacity()
         {
             return this.ram;
+        }
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return "Manufactured by: " + this.manuf + " Ram : " + this.ram + " Hard Disk: " + this.hdd + " Cpu: " + this.cpu;
         }
     }
 }
