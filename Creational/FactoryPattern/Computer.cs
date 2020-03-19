f8078daee83772bd50d295ed208b688a8a866073
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-19 23:17:45 +0530

    [Nikhil]refactor final modification done in computer class

--- Creational/FactoryPattern/Computer.cs
+++ Creational/FactoryPattern/Computer.cs
@@ -1,13 +1,42 @@
-﻿namespace DesignPattern.Creational.FactoryPattern
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Computer.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -----------------------------------------------------------------------------------------------------------
+namespace DesignPattern.Creational.FactoryPattern
 {
     using System;
     using System.Collections.Generic;
     using System.Text;
-    interface Computer
+
+    /// <summary>
+    /// computer interface
+    /// </summary>
+    public interface IComputer
     {
-        int ramcapacity();
-        int hddcapacity();
-        string cpu();
-        string manufacturer();
+        /// <summary>
+        /// Ram capacity of the instance.
+        /// </summary>
+        /// <returns>Ram capacity</returns>
+        int Ramcapacity();
+
+        /// <summary>
+        /// Hard disk capacity of the instance.
+        /// </summary>
+        /// <returns>Hard disk capacity</returns>
+        int Hddcapacity();
+
+        /// <summary>
+        /// Cpu names this instance.
+        /// </summary>
+        /// <returns>Cpu name</returns>
+        string Cpuname();
+
+        /// <summary>
+        /// Manufacturers this instance.
+        /// </summary>
+        /// <returns>the Manufacturer</returns>
+        string Manufacturer();
     }
 }
