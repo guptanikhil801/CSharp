4897032a933404f5b6a7d2abd45c24eb493a920b
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-19 23:24:17 +0530

    [Nikhil]refactor final modification done in computerfactory

--- Creational/FactoryPattern/ComputerFactory.cs
+++ Creational/FactoryPattern/ComputerFactory.cs
@@ -10,9 +10,20 @@ namespace DesignPattern.Creational.FactoryPattern
     using System.Collections.Generic;
     using System.Text;
 
+    /// <summary>
+    /// to create instances
+    /// </summary>
     public class ComputerFactory
     {
-       
+        /// <summary>
+        /// Gets the instance of computer.
+        /// </summary>
+        /// <param name="instancetype">The instance type.</param>
+        /// <param name="ram">The ram.</param>
+        /// <param name="hdd">The HDD.</param>
+        /// <param name="cpu">The cpu.</param>
+        /// <param name="mfn">The MFN.</param>
+        /// <returns>the instance</returns>
         public static IComputer GetInstanceOfComputer(string instancetype, int ram, int hdd, string cpu, string mfn)
         {
             if (instancetype.Equals("pc"))
