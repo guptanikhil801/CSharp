68391ff8b039d9533efa1f6061d2961efc5264f1
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-19 23:19:51 +0530

    [Nikhil]refactor file header added

--- Creational/FactoryPattern/ComputerFactory.cs
+++ Creational/FactoryPattern/ComputerFactory.cs
@@ -1,11 +1,33 @@
-﻿namespace DesignPattern.Creational.FactoryPattern
+﻿// -----------------------------------------------------------------------------------------------------------
+// <copyright file="ComputerFactory.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -----------------------------------------------------------------------------------------------------------
+namespace DesignPattern.Creational.FactoryPattern
 {
     using System;
     using System.Collections.Generic;
     using System.Text;
 
-   public class ComputerFactory
+    public class ComputerFactory
     {
-
+       
+        public static IComputer GetInstanceOfComputer(string instancetype, int ram, int hdd, string cpu, string mfn)
+        {
+            if (instancetype.Equals("pc"))
+            {
+                return new Pc(ram, hdd, cpu, mfn);
+            }
+            else if (instancetype.Equals("server"))
+            {
+                return new Server(ram, hdd, cpu, mfn);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Computer type");
+                return null;
+            }
+        }
     }
 }
