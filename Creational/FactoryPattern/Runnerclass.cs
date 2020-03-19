9ea4d2db06d967b83042487115c20e4aa4d5391a
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-19 23:04:47 +0530

    [Nikhil]new Runnerclass added

---
+++ Creational/FactoryPattern/Runnerclass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Creational.FactoryPattern
+{
+    class Runnerclass
+    {
+        public static void DriverMethod()
+        {
+            Console.WriteLine("Enter Instance type");
+            string it = Console.ReadLine();
+            Console.WriteLine("Enter Ram Capacity");
+            int r = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Hard Disk Capacity");
+            int hd = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Cpu Name");
+            string cp = Console.ReadLine();
+            Console.WriteLine("Enter Manufacturer Name");
+            string mf = Console.ReadLine();
+            ComputerFactory.GetInstanceOfComputer(it, r, hd, cp, mf);
+            Console.WriteLine("Specifications are");
+            Console.WriteLine(ComputerFactory.GetInstanceOfComputer(it, r, hd, cp, mf));
+        }
+    }
+}
