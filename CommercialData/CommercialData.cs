1c17c938476f2c07bc11b08863c7d002c47834dd
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-03-19 11:14:52 +0530

    [Nikhil] refactor takedata method created with some implementation

--- CommercialData/CommercialData.cs
+++ CommercialData/CommercialData.cs
@@ -5,5 +5,17 @@
     using System.Text;
     public class CommercialData
     {
+        public void TakeData(string customername)
+        {
+            StockAccount sa = new StockAccount();
+            sa.customerName = customername;
+            Console.WriteLine("In How many company's stock u want to do transaction");
+            sa.numberofStocks = int.Parse(Console.ReadLine());
+            sa.shareName = new List<string>();
+            sa.sharePrice = new List<int>();
+            sa.shareamount = new List<int>();
+
+
+        }
     }
 }
