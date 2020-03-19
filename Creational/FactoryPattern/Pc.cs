5f72e96f0646ccbbf23542ab06cd5ced43aa807c
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-19 21:27:17 +0530

    [Nikhil]new pc class created

---
+++ Creational/FactoryPattern/Pc.cs
@@ -0,0 +1,28 @@
+﻿namespace DesignPattern.Creational.FactoryPattern
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class Pc : Computer
+    {
+        public string cpuname()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int hddcapacity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string manufacturer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ramcapacity()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
