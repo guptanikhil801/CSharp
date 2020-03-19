eb6dd22fbcda66273f1ca631c99673ce96bf3b86
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-03-19 10:47:39 +0530

    [Nikhil] refactor implementation done in stockaccount class

--- CommercialData/StockAccount.cs
+++ CommercialData/StockAccount.cs
@@ -5,5 +5,13 @@
     using System.Text;
     public class StockAccount
     {
+        public string customerName { get; set; }
+        public int numberofStocks { get; set; }
+        public int MyProperty { get; set; }
+        public List<string> shareName { get; set; }
+        public List<int> numberofShare { get; set; }
+        public List<int> sharePrice { get; set; }
+
+        public DateTime transactiontime = DateTime.Now;
     }
 }
