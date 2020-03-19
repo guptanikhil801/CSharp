6e689a811e176d1d8f4aca54a00cec09dc9996af
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-03-19 16:40:05 +0530

    [Nikhil] new class creatted

---
+++ CommercialData/Management.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopsPrograms.CommercialData
+{
+    public class Management
+    {
+        CommercialData cd = new CommercialData();
+        public void DriverMethod()
+        {
+            
+        }
+        public void RemoveAccount()
+        {
+
+        }
+        public void AddAccount()
+        {
+
+        }
+    }
+}
