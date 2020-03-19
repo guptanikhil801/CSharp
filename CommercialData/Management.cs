4d535f311a5b682a971f5167e328189128eb2d34
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-03-19 17:14:49 +0530

    [Nikhil] driver method added

--- CommercialData/Management.cs
+++ CommercialData/Management.cs
@@ -11,12 +11,30 @@ namespace OopsPrograms.CommercialData
         CommercialData cd = new CommercialData();
         public void DriverMethod()
         {
-            
+            Console.WriteLine("Enter customer name");
+            string cus = Console.ReadLine();
+            Console.WriteLine("Enter Path");
+            string filepath = Console.ReadLine();
+            Console.WriteLine("Enter 1 to dispaly account report\n" +"Enter 2 to remove an account\n" +
+              "Enter 3 to Add a New account");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Display(cus);
+                    break;
+                case 2:
+                    RemoveAccount(cus,filepath);
+                    break;
+                case 3:
+                    AddAccount(filepath);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
         }
-        public void RemoveAccount()
-        {
-
-        }
+        
         public void AddAccount(string file)
         {
             List<StockAccount> ls;
@@ -41,6 +59,51 @@ namespace OopsPrograms.CommercialData
                 serializer.Serialize(stream, ls);
             }
             Console.WriteLine("Successfully added");
+        }
+        public void RemoveAccount(string custname, string file)
+        {
+            string jfile = File.ReadAllText(file);
+
+            List<StockAccount> ls;
+            if (jfile.Length < 1)
+            {
+                ls = new List<StockAccount>();
+            }
+            else
+            {
+                ls = JsonConvert.DeserializeObject<List<StockAccount>>(jfile);
+            }
+
+            for (int i = 0; i < ls.Count; i++)
+            {
+                if (ls[i].customerName.Equals(custname))
+                {
+                    ls.Remove(ls[i]);
+                    break;
+                }
+            }
+
+            //// serialization
+            using (StreamWriter stream = File.CreateText(file))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(stream, ls);
+            }
+            Console.WriteLine("removed successfully");
+        }
+        public void Display(string custname)
+        {
+
+            List<StockAccount> ls;
+            ls = JsonConvert.DeserializeObject<List<StockAccount>>(custname);
+            for (int i = 0; i < ls.Count; i++)
+            {
+                if (ls[i].customerName.Equals(custname))
+                {
+                    cd.PrintReport(custname);
+                    break;
+                }
+            }
         }
     }
 }
