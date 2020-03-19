a2becd164e8df251341e0f5ffc31378442c9faac
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-03-19 17:34:57 +0530

    [Nikhil] final modification done

--- CommercialData/StockAccount.cs
+++ CommercialData/StockAccount.cs
@@ -1,8 +1,18 @@
-﻿namespace OopsPrograms.CommercialData
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Management.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -----------------------------------------------------------------------------------------
+namespace OopsPrograms.CommercialData
 {
     using System;
     using System.Collections.Generic;
     using System.Text;
+
+    /// <summary>
+    /// to create stock account data type
+    /// </summary>
     public class StockAccount
     {
         public string customerName { get; set; }
@@ -10,11 +20,17 @@
         public List<string> transactiontype { get; set; }
         public List<string> shareName { get; set; }
         public List<int> numberofshare { get; set; }
-        // public List<int> shareamountindollar { get; set; }
         public int[] shareamountindollar { get; set; }
         public List<int> sharePrice { get; set; }
         public DateTime transactiontime { get; set; }
         public double valueof { get; set; }
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
         public override string ToString()
         {
             return (string.Format("\t{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}", customerName, numberofStocks, transactiontime, shareName, numberofshare, sharePrice, shareamountindollar, transactiontype, valueof));
