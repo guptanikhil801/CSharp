f2d107226974461370df2ae5af5b8845ae823c60
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-03-19 16:11:06 +0530

    [Nikhil] to string method overridden

--- CommercialData/StockAccount.cs
+++ CommercialData/StockAccount.cs
@@ -7,11 +7,17 @@
     {
         public string customerName { get; set; }
         public int numberofStocks { get; set; }
-        public int MyProperty { get; set; }
+        public List<string> transactiontype { get; set; }
         public List<string> shareName { get; set; }
-        public List<int> numberofShare { get; set; }
+        public List<int> numberofshare { get; set; }
+        // public List<int> shareamountindollar { get; set; }
+        public int[] shareamountindollar { get; set; }
         public List<int> sharePrice { get; set; }
-
-        public DateTime transactiontime = DateTime.Now;
+        public DateTime transactiontime { get; set; }
+        public double valueof { get; set; }
+        public override string ToString()
+        {
+            return (string.Format("\t{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}", customerName, numberofStocks, transactiontime, shareName, numberofshare, sharePrice, shareamountindollar, transactiontype, valueof));
+        }
     }
 }
