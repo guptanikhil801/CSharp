c835e4518d676c8cb419ce67fd856b5bb50081a7
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-19 23:42:51 +0530

    [Nikhil]refactor final modification done in pc.cs

--- Creational/FactoryPattern/Pc.cs
+++ Creational/FactoryPattern/Pc.cs
@@ -20,29 +20,72 @@ namespace DesignPattern.Creational.FactoryPattern
         private int hdd;
         private string cpu;
         private string manuf;
-       public Pc (int ram, int hdd, string cpu, string manuf)
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Pc"/> class.
+        /// </summary>
+        /// <param name="ram">The ram.</param>
+        /// <param name="hdd">The HDD.</param>
+        /// <param name="cpu">The cpu.</param>
+        /// <param name="manuf">The manufacturer.</param>
+        public Pc(int ram, int hdd, string cpu, string manuf)
         {
             this.ram = ram;
             this.hdd = hdd;
             this.cpu = cpu;
             this.manuf = manuf;
         }
+
+        /// <summary>
+        /// Cpu names of this instance.
+        /// </summary>
+        /// <returns>
+        /// Cpu name
+        /// </returns>
         public string Cpuname()
         {
             return this.cpu;
         }
+
+        /// <summary>
+        /// Hard disk capacity of the instance.
+        /// </summary>
+        /// <returns>
+        /// Hard disk capacity
+        /// </returns>
         public int Hddcapacity()
         {
             return this.hdd;
         }
+
+        /// <summary>
+        /// Manufacturers of this instance.
+        /// </summary>
+        /// <returns>
+        /// the Manufacturer
+        /// </returns>
         public string Manufacturer()
         {
             return this.manuf;
         }
+
+        /// <summary>
+        /// Ram capacity of this instance.
+        /// </summary>
+        /// <returns>
+        /// Ram capacity
+        /// </returns>
         public int Ramcapacity()
         {
             return this.ram;
         }
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
         public override string ToString()
         {
             return ("Manufactured by: " + this.manuf + " Ram : " + this.ram + " Hard Disk: " + this.hdd + " Cpu: " + this.cpu);
