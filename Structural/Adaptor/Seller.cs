219d7eedf5ea87fd831c2ff7ef18b677655df6a9
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-20 22:16:01 +0530

    [Nikhil]new seller class implementation done

--- Structural/Adaptor/Seller.cs
+++ Structural/Adaptor/Seller.cs
@@ -5,6 +5,15 @@
     using System.Text;
     public class Seller
     {
-
+        public List<string> GetListOfproducts()
+        {
+            List<string> products = new List<string>();
+            products.Add("Television");
+            products.Add("Mobile");
+            products.Add("Shirts");
+            products.Add("Spare parts");
+            products.Add("sunglass");
+            return products;
+        }
     }
 }
