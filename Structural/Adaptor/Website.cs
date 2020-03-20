1f8cf0a9f9b8bc21f1ef3c21469ac244c4f88ffb
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-20 22:41:57 +0530

    [Nikhil]refactor final modification done

--- Structural/Adaptor/Website.cs
+++ Structural/Adaptor/Website.cs
@@ -1,10 +1,24 @@
-﻿namespace DesignPattern.Structural.Adaptor
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="IWebsite.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
+namespace DesignPattern.Structural.Adaptor
 {
     using System;
     using System.Collections.Generic;
     using System.Text;
-    interface Website
+
+    /// <summary>
+    /// main interface between company and seller
+    /// </summary>
+   public interface IWebsite
     {
+        /// <summary>
+        /// Gets the product list.
+        /// </summary>
+        /// <returns>list of product</returns>
         List<string> GetProducts();
     }
 }
