88029baa6b0f18db5ca1cd63325ca8e78eba25de
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-20 22:22:01 +0530

    [Nikhil]new sellerAdaptor class created and implementated

---
+++ Structural/Adaptor/SellerAdaptor.cs
@@ -0,0 +1,14 @@
+﻿namespace DesignPattern.Structural.Adaptor
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    class SellerAdaptor : Website
+    {
+        public List<string> GetProducts()
+        {
+            Seller sp = new Seller();
+            return sp.GetListOfproducts();
+        }
+    }
+}
