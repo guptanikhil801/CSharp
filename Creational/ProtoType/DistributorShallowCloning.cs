fa806b51800d3c586df381d3d88f68f058940fd4
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-20 12:36:25 +0530

    [Nikhil]refactor file name changed

---
+++ Creational/ProtoType/DistributorShallowCloning.cs
@@ -0,0 +1,43 @@
+﻿namespace DesignPattern.Creational.ProtoType
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+   public class DistributorShallowCloning:ICloneable
+    {
+        public string Location { get; set; }
+        public List<Vehicle> vehicle { get; set; }
+        public void FeedData()
+        {
+            Vehicle v = new Vehicle();
+            v.TypeOfVehicle = "car";
+            v.Brand = "Rolce Royce";
+            v.Price = 30000000;
+            vehicle.Add(v);
+            Vehicle va = new Vehicle();
+            v.TypeOfVehicle = "car";
+            v.Brand = "Mustang";
+            v.Price = 10000000;
+            vehicle.Add(va);
+            Vehicle vb = new Vehicle();
+            v.TypeOfVehicle = "Bike";
+            v.Brand = "Harley Davidson";
+            v.Price = 600000;
+            vehicle.Add(vb);
+            Vehicle vc = new Vehicle();
+            v.TypeOfVehicle = "Bike";
+            v.Brand = "Triumph";
+            v.Price = 400000;
+            vehicle.Add(vc);
+        }
+        public override string ToString()
+        {
+            return "Distributor Location : " + Location + ",\n Vehicle : " + vehicle;
+        }
+       public object Clone()
+        {
+            DistributorShallowCloning dc = (DistributorShallowCloning)this.MemberwiseClone();
+            return dc;
+        }
+    }
+}
