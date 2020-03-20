f6ea8c26ebbaecfc3a895719d8d1799f3cc9b843
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-20 16:03:29 +0530

    [Nikhil]refactor vehicle class implementation changed

--- Creational/ProtoType/Vehicle.cs
+++ Creational/ProtoType/Vehicle.cs
@@ -3,14 +3,27 @@
     using System;
     using System.Collections.Generic;
     using System.Text;
-    public class Vehicle
+    public class Vehicle:ICloneable
     {
-        public string TypeOfVehicle { get; set; }
-        public string Brand { get; set; }
-        public int Price { get; set; }
+        public string Typeofvehicle;
+        public string Brand;
+        public int Price;
+
+        public Vehicle(string typeofvehicle, string brand, int price)
+        {
+            Typeofvehicle = typeofvehicle;
+            Brand = brand;
+            Price = price;
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
         public override string ToString()
         {
-            return "Vehicle Type, : " + TypeOfVehicle + ", Brand : " + Brand + " Price : " + Price;
+            return "Vehicle Type, : " + this.Typeofvehicle + ", Brand : " + this.Brand + " Price : " + this.Price;
         }
     }
 }
