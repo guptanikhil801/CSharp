a7eda33ba84ae65339a643a8c819bd61a4852392
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-20 22:54:54 +0530

    [Nikhil]refactor final modification done

--- Structural/Adaptor/Seller.cs
+++ Structural/Adaptor/Seller.cs
@@ -1,10 +1,24 @@
-﻿namespace DesignPattern.Structural.Adaptor
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="Seller.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
+namespace DesignPattern.Structural.Adaptor
 {
     using System;
     using System.Collections.Generic;
     using System.Text;
+
+    /// <summary>
+    /// this class has list of product
+    /// </summary>
     public class Seller
     {
+        /// <summary>
+        /// Gets the list of products.
+        /// </summary>
+        /// <returns>product list</returns>
         public List<string> GetListOfproducts()
         {
             List<string> products = new List<string>();
