67f7f33cccc892a2fe7be1a8323922c2e657f2fe
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-20 17:00:52 +0530

    [Nikhil]refactor class updatde

--- Creational/ProtoType/ProtoTypeRunnerClass.cs
+++ Creational/ProtoType/ProtoTypeRunnerClass.cs
@@ -6,6 +6,27 @@ namespace DesignPattern.Creational.ProtoType
 {
     public class ProtoTypeRunnerClass
     {
+        public static void DriverMethod()
+        {
+            DistributorShallowCloning sc = new DistributorShallowCloning();
+            sc.Location = "Bangalore";
+            sc.vehicle = new Vehicle("Car", "Rolls Roycse", 45000000);
+            Console.WriteLine("Original object of shallow cloning");
+            Console.WriteLine(sc);
+            DistributorShallowCloning clonedshallow = (DistributorShallowCloning)sc.Clone();
+            clonedshallow.Location = "Ranchi";
+            Console.WriteLine("Cloned object using shallow cloning");
+            Console.WriteLine(clonedshallow);
+            DistributorDeepCloning dc = new DistributorDeepCloning();
+            sc.Location = "Jamshedpur";
+            sc.vehicle = new Vehicle("Car", "fortuner", 3500000);
+            Console.WriteLine("Original object of Deep cloning");
+            Console.WriteLine(dc);
+            DistributorDeepCloning cloneddeep = (DistributorDeepCloning)dc.Clone();
+            cloneddeep.Location = "Hyderabad";
+            Console.WriteLine("Cloned object using deep cloning");
+            Console.WriteLine(cloneddeep);
 
+        }
     }
 }
