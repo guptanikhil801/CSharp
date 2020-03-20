6e3b5ea5c522fa16897bac5b402dfeeb55582fe9
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-20 10:28:30 +0530

    [Nikhil]refactor properties added

--- Creational/ProtoType/Vehicle.cs
+++ Creational/ProtoType/Vehicle.cs
@@ -3,7 +3,14 @@
     using System;
     using System.Collections.Generic;
     using System.Text;
-    public class Vehicle
+    public class Vehicle : ICloneable
     {
+        public string TypeOfVehicle { get; set; }
+        public string Brand { get; set; }
+        public int Price { get; set; }
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
