ab17a582016ee2608668becf2bf4f3fb40b809fa
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-20 22:34:55 +0530

    [Nikhil]new Ecommercecompany class created and implementated

---
+++ Structural/Adaptor/EcommerceCompany.cs
@@ -0,0 +1,19 @@
+﻿namespace DesignPattern.Structural.Adaptor
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    class EcommerceCompany
+    {
+        public static void DriverMethod()
+        {
+            IWebsite web = new SellerAdaptor();
+            Console.WriteLine("Product List");
+            foreach(string product in web.GetProducts())
+            {
+                Console.WriteLine(product);
+            }
+            Console.WriteLine();
+        }
+    }
+}
