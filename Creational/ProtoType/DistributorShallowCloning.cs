b29b2992182484cd9f418d0a88eb9ac23aa91ff9
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-20 17:13:11 +0530

    [Nikhil]refactor final modification done

--- Creational/ProtoType/DistributorShallowCloning.cs
+++ Creational/ProtoType/DistributorShallowCloning.cs
@@ -1,18 +1,42 @@
-﻿namespace DesignPattern.Creational.ProtoType
+﻿// -----------------------------------------------------------------------------------------
+// <copyright file="DistributorShallowCloning.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -----------------------------------------------------------------------------------------
+namespace DesignPattern.Creational.ProtoType
 {
     using System;
     using System.Collections.Generic;
     using System.Text;
-   public class DistributorShallowCloning:ICloneable
+
+    /// <summary>
+    /// for shallow cloning
+    /// </summary>
+    /// <seealso cref="System.ICloneable" />
+    public class DistributorShallowCloning : ICloneable
     {
         public string Location;
         public Vehicle vehicle;
-        
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
         public override string ToString()
         {
-            return "Location : " + this.Location + "vehicle Details " + this.vehicle;
+            return "Location : " + this.Location + ", vehicle Details -" + this.vehicle;
         }
-       public object Clone()
+
+        /// <summary>
+        /// Creates a new object that is a copy of the current instance.
+        /// </summary>
+        /// <returns>
+        /// copy of this instance.
+        /// </returns>
+        public object Clone()
         {
             return this.MemberwiseClone();
         }
