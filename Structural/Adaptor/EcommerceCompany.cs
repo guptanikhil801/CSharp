10c5b267c0a26066b543cc60920b0d8f48208098
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-20 22:50:47 +0530

    [Nikhil]refactor final modification done

--- Structural/Adaptor/EcommerceCompany.cs
+++ Structural/Adaptor/EcommerceCompany.cs
@@ -1,18 +1,32 @@
-﻿namespace DesignPattern.Structural.Adaptor
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="EcommerceCompany.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
+namespace DesignPattern.Structural.Adaptor
 {
     using System;
     using System.Collections.Generic;
     using System.Text;
-    class EcommerceCompany
+
+    /// <summary>
+    /// client class
+    /// </summary>
+    public class EcommerceCompany
     {
+        /// <summary>
+        /// Driver method.
+        /// </summary>
         public static void DriverMethod()
         {
             IWebsite web = new SellerAdaptor();
             Console.WriteLine("Product List");
-            foreach(string product in web.GetProducts())
+            foreach (string product in web.GetProducts())
             {
                 Console.WriteLine(product);
             }
+
             Console.WriteLine();
         }
     }
