63590d9a151a262b8a7ee0d1f2520d1ace8ff90a
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-20 21:25:45 +0530

    [Nikhil]new Website Interface created

---
+++ Structural/Adaptor/Website.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPattern.Structural.Adaptor
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    interface Website
+    {
+        List<string> GetProducts();
+    }
+}
