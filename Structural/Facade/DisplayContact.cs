baa836f2153f66b0afd73b212ef6d867be979502
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-21 13:36:14 +0530

    [Nikhil]refactor final modification done

--- Structural/Facade/DisplayContact.cs
+++ Structural/Facade/DisplayContact.cs
@@ -1,12 +1,27 @@
-﻿namespace DesignPattern.Structural.Facade
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="DisplayContact.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
+namespace DesignPattern.Structural.Facade
 {
     using System;
     using System.Collections.Generic;
     using System.IO;
     using System.Text;
     using Newtonsoft.Json;
+
+    /// <summary>
+    /// class to display contacts
+    /// </summary>
+    /// <seealso cref="DesignPattern.Structural.Facade.IAddressBook" />
     public class DisplayContact : IAddressBook
     {
+        /// <summary>
+        /// Does the operation of displaying
+        /// </summary>
+        /// <param name="filepath">The filepath.</param>
         public void DoOperation(string filepath)
         {
             string contactjson = File.ReadAllText(filepath);
