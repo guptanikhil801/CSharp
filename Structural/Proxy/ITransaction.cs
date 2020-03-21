376a0049b8afa4a062ae7b7df8354936abe56897
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-21 17:59:12 +0530

    [Nikhil]refactor final modification done

--- Structural/Proxy/ITransaction.cs
+++ Structural/Proxy/ITransaction.cs
@@ -1,13 +1,38 @@
-﻿namespace DesignPattern.Structural.Proxy
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="ITransaction.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
+namespace DesignPattern.Structural.Proxy
 {
     using System;
     using System.Collections.Generic;
     using System.Text;
-    interface ITransaction
+
+    /// <summary>
+    /// Transaction interface
+    /// </summary>
+    public interface ITransaction
     {
+        /// <summary>
+        /// Withdraws method declaration
+        /// </summary>
         void Withdraw();
+
+        /// <summary>
+        /// Deposits  method declaration
+        /// </summary>
         void Deposit();
+
+        /// <summary>
+        /// Pinchange method declaration
+        /// </summary>
         void PinChange();
+
+        /// <summary>
+        /// Statements method declaration.
+        /// </summary>
         void Statement();
     }
 }
---
+++ Structural/Proxy/ProxyCardClass.cs
@@ -0,0 +1,44 @@
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="ProxyCardClass.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
+namespace DesignPattern.Structural.Proxy
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// class to hold object of cash class
+    /// </summary>
+    public class ProxyCardClass
+    {
+        private static List<string> eligiblecards;
+        private Cash cash = new Cash();
+
+        /// <summary>
+        /// Starts the transaction.
+        /// </summary>
+        /// <param name="cardtype">The card type.</param>
+        public void StartTransaction(string cardtype)
+        {
+            eligiblecards = new List<string>();
+            eligiblecards.Add("rupay");
+            eligiblecards.Add("visa");
+            eligiblecards.Add("master");
+            if (eligiblecards.Contains(cardtype.ToLower()))
+            {
+                this.cash.Statement();
+                this.cash.Deposit();
+                this.cash.Withdraw();
+                this.cash.PinChange();
+            }
+            else
+            {
+                Console.WriteLine("Invalid Card type");
+            }
+        }
+    }
+}
