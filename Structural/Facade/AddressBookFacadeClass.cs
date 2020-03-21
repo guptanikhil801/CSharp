efbb179aaf63ab9f3195b259b0712c87cde07d07
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-21 13:24:05 +0530

    [Nikhil]refactor final modification done

--- Structural/Facade/AddressBookFacadeClass.cs
+++ Structural/Facade/AddressBookFacadeClass.cs
@@ -1,36 +1,70 @@
-﻿namespace DesignPattern.Structural.Facade
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="AddressBookFacadeClass.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
+namespace DesignPattern.Structural.Facade
 {
     using System;
     using System.Collections.Generic;
     using System.Text;
+
+    /// <summary>
+    /// Facade class of AddressBook
+    /// </summary>
     public class AddressBookFacadeClass
     {
         private AddContact addContact;
         private DeleteContact deleteContact;
         private EditContact editContact;
         private DisplayContact displayContact;
+
+        /// <summary>
+        /// Initializes a new instance of this class <see cref="AddressBookFacadeClass"/> class.
+        /// </summary>
         public AddressBookFacadeClass()
         {
-            addContact = new AddContact();
-            deleteContact = new DeleteContact();
-            editContact = new EditContact();
-            displayContact = new DisplayContact();
+            this.addContact = new AddContact();
+            this.deleteContact = new DeleteContact();
+            this.editContact = new EditContact();
+            this.displayContact = new DisplayContact();
         }
+
+        /// <summary>
+        /// calls add contact class
+        /// </summary>
+        /// <param name="file">The file.</param>
         public void DoAddContact(string file)
         {
-            addContact.DoOperation(file);
+            this.addContact.DoOperation(file);
         }
+
+        /// <summary>
+        /// calls the editcontact class method
+        /// </summary>
+        /// <param name="file">The file.</param>
         public void DoEditContact(string file)
         {
-            editContact.DoOperation(file);
+            this.editContact.DoOperation(file);
         }
+
+        /// <summary>
+        /// calls the delete contact class's  method 
+        /// </summary>
+        /// <param name="file">The file.</param>
         public void DoDeleteContact(string file)
         {
-            deleteContact.DoOperation(file);
+            this.deleteContact.DoOperation(file);
         }
+
+        /// <summary>
+        ///  calls the display contact class's  method 
+        /// </summary>
+        /// <param name="file">The file.</param>
         public void DoDisplayContact(string file)
         {
-            displayContact.DoOperation(file);
+            this.displayContact.DoOperation(file);
         }
     }
 }
