b1d77b24c82b63e47f55916bb2a9b16bf8feac42
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-21 11:38:24 +0530

    [Nikhil]refactor implementation done in Addcontact class

--- Structural/Facade/AddContact.cs
+++ Structural/Facade/AddContact.cs
@@ -1,9 +1,53 @@
 ﻿namespace DesignPattern.Structural.Facade
 {
+    using Newtonsoft.Json;
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Text;
-    public class AddContact
+    public class AddContact : IAddressBook
     {
+        public void DoOperation(string filepath)
+        {
+            string jsonstr = File.ReadAllText(filepath);
+            List<Contact> cont;
+            if (jsonstr.Length != 0)
+            {
+                cont = (List<Contact>)JsonConvert.DeserializeObject<List<Contact>>(jsonstr);
+            }
+            else
+            {
+                cont = new List<Contact>();
+            }
+
+            Contact ct = new Contact();
+            Console.WriteLine("Enter your first Name");
+            ct.FirstName = Console.ReadLine();
+            foreach (Contact c in cont)
+            {
+                if (c.FirstName.Equals(ct.FirstName))
+                {
+                    Console.WriteLine("This contact already exist...Enter with some difference or a new one");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Enter your Last Name");
+            ct.LastName = Console.ReadLine();
+            Console.WriteLine("Enter Phone Number");
+            ct.PhoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            ct.Address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            ct.City = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            ct.State = Console.ReadLine();
+            Console.WriteLine("Enter Zip Code");
+            ct.Zip = Console.ReadLine();
+            cont.Add(ct);
+            Console.WriteLine("Contact Successfully added");
+            string jsoncontact = JsonConvert.SerializeObject(cont, Formatting.Indented);
+            File.WriteAllText(filepath, jsoncontact);
+        }
     }
 }
