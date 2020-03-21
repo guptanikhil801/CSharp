5922fd7cb1495024d7cd65f2c1c092abfd5b0778
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-21 11:58:55 +0530

    [Nikhil]refactor Displaycontact class implemented

--- Structural/Facade/DisplayContact.cs
+++ Structural/Facade/DisplayContact.cs
@@ -2,9 +2,20 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Text;
-    public class DisplayContact
+    using Newtonsoft.Json;
+    public class DisplayContact : IAddressBook
     {
-
+        public void DoOperation(string filepath)
+        {
+            string contactjson = File.ReadAllText(filepath);
+            List<Contact> cj = (List<Contact>)JsonConvert.DeserializeObject<List<Contact>>(contactjson);
+            foreach (Contact co in cj)
+            {
+                Console.WriteLine(co);
+                Console.WriteLine();
+            }
+        }
     }
 }
