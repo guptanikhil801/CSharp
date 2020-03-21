8da76a55728758534f71f44ad30c18390b1ac94f
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-21 11:51:04 +0530

    [Nikhil]refactor implementation done in Deletecontact class

--- Structural/Facade/DeleteContact.cs
+++ Structural/Facade/DeleteContact.cs
@@ -1,10 +1,32 @@
 ﻿namespace DesignPattern.Structural.Facade
 {
+    using Newtonsoft.Json;
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Text;
-    public class DeleteContact
+    public class DeleteContact : IAddressBook
     {
+        public void DoOperation(string filepath)
+        {
 
+            Console.WriteLine("Enter First name");
+            string first = Console.ReadLine();
+            string jsoncont = File.ReadAllText(filepath);
+            List<Contact> ctlist;
+            ctlist = (List<Contact>)JsonConvert.DeserializeObject<List<Contact>>(jsoncont);
+            foreach (Contact co in ctlist)
+            {
+                if (co.FirstName.Equals(first))
+                {
+                    ctlist.Remove(co);
+                    break;
+                }
+            }
+
+            string contactlist = JsonConvert.SerializeObject(ctlist, Formatting.Indented);
+            File.WriteAllText(filepath, contactlist);
+            Console.WriteLine("Contact deleted successfully");
+        }
     }
 }
