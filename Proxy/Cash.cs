d3440436f423f8cbaba99bd9fb67f25a7ecf68ef
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-21 16:30:31 +0530

    [Nikhil]refactor Cash class implementation done

---
+++ Proxy/Cash.cs
@@ -0,0 +1,37 @@
+﻿namespace DesignPattern.Structural.Proxy
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class Cash : ITransaction
+    {
+        public void Deposit()
+        {
+            Console.WriteLine("Enter amount to deposit");
+            int dep = int.Parse(Console.ReadLine());
+            Console.WriteLine(dep+"  deposited ");
+        }
+
+        public void PinChange()
+        {
+            Console.WriteLine("Enter New pin ");
+            int pin = int.Parse(Console.ReadLine());
+            Console.WriteLine("pin changed to "+pin);
+        }
+
+        public void Statement()
+        {
+            Console.WriteLine("Statement :");
+            Console.WriteLine("5000 debited at 10-03-2020");
+            Console.WriteLine("8000 credited at 20-03-2020");
+            Console.WriteLine("current balance : 12500");
+        }
+
+        public void Withdraw()
+        {
+            Console.WriteLine("Enter amount to withdraw");
+            int wid = int.Parse(Console.ReadLine());
+            Console.WriteLine(wid + "  withdrawl from your account ");
+        }
+    }
+}
---
+++ Proxy/ITransaction.cs
@@ -0,0 +1,13 @@
+﻿namespace DesignPattern.Structural.Proxy
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    interface ITransaction
+    {
+        void Withdraw();
+        void Deposit();
+        void PinChange();
+        void Statement();
+    }
+}
