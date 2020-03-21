9598770a159f0c6ff4a2524020945299df09ccc4
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-21 12:14:46 +0530

    [Nikhil]refactor instance variable and constructor initialized in AddressBookFacade class

--- Structural/Facade/AddressBookFacadeClass.cs
+++ Structural/Facade/AddressBookFacadeClass.cs
@@ -5,5 +5,16 @@
     using System.Text;
     public class AddressBookFacadeClass
     {
+        private AddContact addContact;
+        private DeleteContact deleteContact;
+        private EditContact editContact;
+        private DisplayContact displayContact;
+        public AddressBookFacadeClass()
+        {
+            addContact = new AddContact();
+            deleteContact = new DeleteContact();
+            editContact = new EditContact();
+            displayContact = new DisplayContact();
+        }
     }
 }
