dda9151fae80a3088575aea87852215ba8565393
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-21 13:46:56 +0530

    [Nikhil]refactor final modification done

--- Structural/Facade/IAddressBook.cs
+++ Structural/Facade/IAddressBook.cs
@@ -1,10 +1,24 @@
-﻿namespace DesignPattern.Structural.Facade
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="IAddressBook.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
+namespace DesignPattern.Structural.Facade
 {
     using System;
     using System.Collections.Generic;
     using System.Text;
+
+    /// <summary>
+    /// address book interface
+    /// </summary>
     public interface IAddressBook
     {
-        void DoOperation();
+        /// <summary>
+        /// abstract method to do operation
+        /// </summary>
+        /// <param name="filepath">The filepath.</param>
+        void DoOperation(string filepath);
     }
 }
