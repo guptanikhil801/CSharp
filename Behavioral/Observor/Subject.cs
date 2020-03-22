dbf6d517b6bbb45a621f73ba5cc4a758f0460742
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-22 19:43:25 +0530

    [Nikhil]new Subject Interface created

---
+++ Behavioral/Observor/Subject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Behavioral.Observor
+{
+    interface Subject
+    {
+        void CustomerDatabase(Customer cus);
+    }
+}
