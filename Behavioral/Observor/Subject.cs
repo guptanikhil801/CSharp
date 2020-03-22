1f761fee0d02e6c81dce674582e3da56099602f2
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-22 20:46:41 +0530

    [Nikhil]refactor final modification done

--- Behavioral/Observor/Subject.cs
+++ Behavioral/Observor/Subject.cs
@@ -1,11 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="Subject.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
 namespace DesignPattern.Behavioral.Observor
 {
-    interface Subject
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// interface for subject
+    /// </summary>
+   public interface ISubject
     {
+        /// <summary>
+        /// Customer database.
+        /// </summary>
+        /// <param name="cus">The customer.</param>
         void CustomerDatabase(Customer cus);
     }
 }
