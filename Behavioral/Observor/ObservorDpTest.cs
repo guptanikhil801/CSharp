18d618065f37da578bae3a7457da413766eac2c0
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-22 20:20:43 +0530

    [Nikhil]refactor Observordptest class implementation done

--- Behavioral/Observor/ObservorDpTest.cs
+++ Behavioral/Observor/ObservorDpTest.cs
@@ -6,6 +6,21 @@ namespace DesignPattern.Behavioral.Observor
 {
    public class ObservorDpTest
     {
-
+        public static void ObservorDpDriverMethod()
+        {
+            MakeMyTrip test = new MakeMyTrip();
+            Customer c1 = new Customer("Nikhil", 9031378339);
+            Customer c2 = new Customer("Gupta", 8603020007);
+            Customer c3 = new Customer("Krishna", 9877045632);
+            test.CustomerDatabase(c1);
+            test.CustomerDatabase(c2);
+            test.CustomerDatabase(c3);
+            c1.OfferNotification();
+            c1.PriceNotification();
+            c2.OfferNotification();
+            c2.PriceNotification();
+            c3.OfferNotification();
+            c3.PriceNotification();
+        }
     }
 }
