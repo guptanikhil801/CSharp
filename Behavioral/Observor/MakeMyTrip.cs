8b0fe8658d748d459f38b6dad48b01dc8571a223
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-22 20:29:16 +0530

    [Nikhil]refactor file header added

--- Behavioral/Observor/MakeMyTrip.cs
+++ Behavioral/Observor/MakeMyTrip.cs
@@ -1,9 +1,22 @@
-﻿namespace DesignPattern.Behavioral.Observor;
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="Customer.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
+namespace DesignPattern.Behavioral.Observor
 {
     using System;
     using System.Collections.Generic;
     using System.Text;
-    public class MakeMyTrip
+    public class MakeMyTrip:Subject
     {
+        private List<Customer> customer = new List<Customer>();
+
+        public void CustomerDatabase(Customer cus)
+        {
+            customer.Add(cus);
+        }
+       
     }
 }
