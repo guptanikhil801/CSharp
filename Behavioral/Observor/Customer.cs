c8aa6a7e8a186598c730da9d8b29c002317968b5
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-22 19:07:14 +0530

    [Nikhil]refactor some implementation done

--- Behavioral/Observor/Customer.cs
+++ Behavioral/Observor/Customer.cs
@@ -1,9 +1,18 @@
-﻿namespace DesignPattern.Behavioral
+﻿namespace DesignPattern.Behavioral.Observor;
 {
     using System;
     using System.Collections.Generic;
     using System.Text;
     public class Customer
     {
+    private string name;
+    private int contactnumber;
+    private MakeMyTrip mmt = new MakeMyTrip();
+
+    public Customer(string name, int mobile)
+    {
+        this.name = name;
+        this.contactnumber = mobile;
+    }
     }
 }
