3851590e8ea23b38f5ae910919a1b8773590c213
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-22 20:38:14 +0530

    [Nikhil]refactor final modification done

--- Behavioral/Observor/Observor.cs
+++ Behavioral/Observor/Observor.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="Observor.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
 namespace DesignPattern.Behavioral.Observor
 {
-    interface Observor
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Interface for observor
+    /// </summary>
+   public interface IObservor
     {
+        /// <summary>
+        /// Price notification.
+        /// </summary>
         public void PriceNotification();
+
+        /// <summary>
+        /// Offer notification.
+        /// </summary>
         public void OfferNotification();
     }
 }
