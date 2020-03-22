536b2382a572631fb43948d508f1193d5ad499b0
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-22 19:51:29 +0530

    [Nikhil]new Observor Interface created with implementation

---
+++ Behavioral/Observor/Observor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Behavioral.Observor
+{
+    interface Observor
+    {
+        public void PriceNotification();
+        public void OfferNotification();
+    }
+}
