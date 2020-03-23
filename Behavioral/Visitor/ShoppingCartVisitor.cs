fb4e3fb293187c0cd25d8b42e51dae44b73ec4bc
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-23 16:24:36 +0530

    [Nikhil]refactor final modification done

--- Behavioral/Visitor/ShoppingCartVisitor.cs
+++ Behavioral/Visitor/ShoppingCartVisitor.cs
@@ -1,5 +1,4 @@
-﻿
-// -------------------------------------------------------------------------------------------------------
+﻿// -------------------------------------------------------------------------------------------------------
 // <copyright file="ShoppingCartVisitor.cs" company="Bridgelabz">
 //   Copyright © 2020 Company="BridgeLabz"
 // </copyright>
@@ -10,9 +9,24 @@ namespace DesignPattern.Behavioral.Visitor
     using System;
     using System.Collections.Generic;
     using System.Text;
-   public interface ShoppingCartVisitor
+
+    /// <summary>
+    /// interface for ShoppingCartVisitor
+    /// </summary>
+    public interface ShoppingCartVisitor
     {
+        /// <summary>
+        /// method for who Visits mobile.
+        /// </summary>
+        /// <param name="mobile">The mobile.</param>
+        /// <returns>total price of mobile</returns>
         int Visit(Mobile mobile);
+
+        /// <summary>
+        /// method for who Visits apparel.
+        /// </summary>
+        /// <param name="apparel">The apparel.</param>
+        /// <returns>total price of Apparel</returns>
         int Visit(Apparel apparel);
     }
 }
