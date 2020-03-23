8ca6d77d84934b5f2f8da081515c2bdc7334c91c
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-23 15:18:11 +0530

    [Nikhil]refactor one method added

--- Behavioral/Visitor/ShoppingCartClient.cs
+++ Behavioral/Visitor/ShoppingCartClient.cs
@@ -6,5 +6,15 @@ namespace DesignPattern.Behavioral.Visitor
     using System.Text;
    public class ShoppingCartClient
     {
+        private static int CalculateCartPrice(IItemElement[] items)
+        {
+            ShoppingCartVisitor visitor = new ShoppingCartImpl();
+            int totalcartvalue = 0;
+            foreach(IItemElement ie in items)
+            {
+                totalcartvalue = totalcartvalue + ie.Accept(visitor);
+            }
+            return totalcartvalue;
+        }
     }
 }
