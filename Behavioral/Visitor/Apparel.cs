c385a9c1ed8587f213f667ebf8a1ea87262c42b0
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-23 12:58:21 +0530

    [Nikhil]refactor instane variable added and constructor initialized

--- Behavioral/Visitor/Apparel.cs
+++ Behavioral/Visitor/Apparel.cs
@@ -4,7 +4,20 @@ namespace DesignPattern.Behavioral.Visitor
     using System;
     using System.Collections.Generic;
     using System.Text;
-    public class Apparel
+    public class Apparel : IItemElement
     {
+        private string brandName;
+        private int price;
+        private int quantity;
+        public Apparel(string brand, int price, int quantity)
+        {
+            brandName = brand;
+            this.price = price;
+            this.quantity = quantity;
+        }
+        public int Accept(ShoppingCartVisitor visitor)
+        {
+
+        }
     }
 }
