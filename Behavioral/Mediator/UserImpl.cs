a7a2e1aebb5d161b2cf39fb1b5667a7267fcaab5
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-23 17:39:15 +0530

    [Nikhil]New class created and constructor initialized

---
+++ Behavioral/Mediator/UserImpl.cs
@@ -0,0 +1,15 @@
+﻿
+namespace DesignPattern.Behavioral.Mediator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class UserImpl : User
+    {
+        public UserImpl(IChatMediator med, string name) : base(med,name)
+        {
+            
+        }
+      
+    }
+}
