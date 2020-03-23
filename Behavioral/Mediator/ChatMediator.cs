ce063666f697d58786997d61b9647fde549def4d
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-23 18:07:12 +0530

    [Nikhil]refactor file header added

--- Behavioral/Mediator/ChatMediator.cs
+++ Behavioral/Mediator/ChatMediator.cs
@@ -9,9 +9,23 @@ namespace DesignPattern.Behavioral.Mediator
     using System;
     using System.Collections.Generic;
     using System.Text;
+
+    /// <summary>
+    /// interface of ChatMediator
+    /// </summary>
     public interface IChatMediator
     {
+        /// <summary>
+        /// Adds the user.
+        /// </summary>
+        /// <param name="user">The user.</param>
         void AddUser(User user);
+
+        /// <summary>
+        /// Sends the message.
+        /// </summary>
+        /// <param name="msg">The Message.</param>
+        /// <param name="user">The user.</param>
         void SendMessage(string msg, User user);
     }
 }
