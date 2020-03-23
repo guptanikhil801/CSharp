3ef6a6a62733f91e6e38a70f32ba4ff9cd9ddb35
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-23 12:47:38 +0530

    [Nikhil]refactor some implementation done

--- Behavioral/Visitor/Mobile.cs
+++ Behavioral/Visitor/Mobile.cs
@@ -4,7 +4,32 @@ namespace DesignPattern.Behavioral.Visitor
     using System;
     using System.Collections.Generic;
     using System.Text;
-    public class Mobile
+    public class Mobile : IItemElement
     {
+        private string modelName;
+        private int price;
+        private int quantity;
+        public Mobile(string model, int price, int quantity)
+        {
+            modelName = model;
+            this.price = price;
+            this.quantity = quantity;
+        }
+        public string GetModelName()
+        {
+            return this.modelName;
+        }
+        public int GetPrice()
+        {
+            return this.price;
+        }
+        public int GetQuantity()
+        {
+            return this.quantity;
+        }
+        public int Accept(ShoppingCartVisitor visitor)
+        {
+           
+        }
     }
 }
