fc3a2d33ae3171edbdd110c7418d3bf1708d726d
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-23 17:24:41 +0530

    [Nikhil]refactor implementation done

--- Behavioral/Mediator/ChatMediatorImpl.cs
+++ Behavioral/Mediator/ChatMediatorImpl.cs
@@ -4,7 +4,27 @@ namespace DesignPattern.Behavioral.Mediator
     using System;
     using System.Collections.Generic;
     using System.Text;
-    public class ChatMediatorImpl
+    public class ChatMediatorImpl : IChatMediator
     {
+        private List<User> users;
+        public ChatMediatorImpl()
+        {
+            this.users = new List<User>();
+        }
+        public void AddUser(User user)
+        {
+            this.users.Add(user);
+        }
+
+        public void SendMessage(string msg, User user)
+        {
+            foreach (User us in this.users)
+            {
+                if(us!=user)
+                {
+                    us.Receive(msg);
+                }
+            }
+        }
     }
 }
