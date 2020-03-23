d4c778b5926d201e8d969d3fff7f7b62882849ce
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-23 17:53:07 +0530

    [Nikhil]refactor implementation done

--- Behavioral/Mediator/ChatClient.cs
+++ Behavioral/Mediator/ChatClient.cs
@@ -6,5 +6,20 @@ namespace DesignPattern.Behavioral.Mediator
 {
    public class ChatClient
     {
+        public static void DriverMethodMediator()
+        {
+            IChatMediator mediator = new ChatMediatorImpl();
+            User user1 = new UserImpl(mediator, "Nikhil");
+            User user2 = new UserImpl(mediator, "Ashish");
+            User user3 = new UserImpl(mediator, "Abhishek");
+            User user4 = new UserImpl(mediator, "Rohit");
+            User user5 = new UserImpl(mediator, "Sunny");
+            mediator.AddUser(user1);
+            mediator.AddUser(user2);
+            mediator.AddUser(user3);
+            mediator.AddUser(user4);
+            mediator.AddUser(user5);
+            user1.Send("Hello friends");
+        }
     }
 }
