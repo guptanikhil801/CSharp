0af6e48356deed7dfcecda82792b6630b3fa91ef
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-23 17:07:40 +0530

    [Nikhil]refactor variable added and constructor initialized

--- Behavioral/Mediator/User.cs
+++ Behavioral/Mediator/User.cs
@@ -4,7 +4,14 @@ namespace DesignPattern.Behavioral.Mediator
     using System;
     using System.Collections.Generic;
     using System.Text;
-   public class User
+   public abstract class User
     {
+        protected IChatMediator mediator;
+        protected string name;
+        public User(IChatMediator med, string name)
+        {
+            mediator = med;
+            this.name = name;
+        }
     }
 }
