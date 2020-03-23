2bc92861e3420ff470d36778eb9053b0ed2bb421
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-23 15:48:39 +0530

    [Nikhil]refactor commenting done

--- Behavioral/Visitor/Apparel.cs
+++ Behavioral/Visitor/Apparel.cs
@@ -9,29 +9,62 @@ namespace DesignPattern.Behavioral.Visitor
     using System;
     using System.Collections.Generic;
     using System.Text;
+
+    /// <summary>
+    /// Apparel class which implements IItemElement interface
+    /// </summary>
+    /// <seealso cref="DesignPattern.Behavioral.Visitor.IItemElement" />
     public class Apparel : IItemElement
     {
         private string brandName;
         private int price;
         private int quantity;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Apparel"/> class.
+        /// </summary>
+        /// <param name="brand">The brand.</param>
+        /// <param name="price">The price.</param>
+        /// <param name="quantity">The quantity.</param>
         public Apparel(string brand, int price, int quantity)
         {
             brandName = brand;
             this.price = price;
             this.quantity = quantity;
         }
+
+        /// <summary>
+        /// Gets the name of the brand.
+        /// </summary>
+        /// <returns></returns>
         public string GetBrandName()
         {
             return this.brandName;
         }
+
+        /// <summary>
+        /// Gets the price of Apparel.
+        /// </summary>
+        /// <returns></returns>
         public int GetPrice()
         {
             return this.price;
         }
+
+        /// <summary>
+        /// Gets the quantity of Apparel.
+        /// </summary>
+        /// <returns></returns>
         public int GetQuantity()
         {
             return this.quantity;
         }
+
+        /// <summary>
+        /// Accepts the specified visitor.
+        /// </summary>
+        /// <param name="visitor">The visitor.</param>
+        /// <returns></returns>
         public int Accept(ShoppingCartVisitor visitor)
         {
             return visitor.Visit(this);
