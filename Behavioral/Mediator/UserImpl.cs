edb369881393f81d60572a582041a1fe20f857ca
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-23 18:19:33 +0530

    [Nikhil]refactor final modification done

--- Behavioral/Mediator/UserImpl.cs
+++ Behavioral/Mediator/UserImpl.cs
@@ -9,18 +9,35 @@ namespace DesignPattern.Behavioral.Mediator
     using System;
     using System.Collections.Generic;
     using System.Text;
+
+    /// <summary>
+    /// implementation class of user
+    /// </summary>
+    /// <seealso cref="DesignPattern.Behavioral.Mediator.User" />
     public class UserImpl : User
     {
-        public UserImpl(IChatMediator med, string name) : base(med,name)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserImpl"/> class.
+        /// </summary>
+        /// <param name="med">The mediator.</param>
+        /// <param name="name">The name.</param>
+        public UserImpl(IChatMediator med, string name) : base(med, name)
         {
-            
         }
+
+        /// <summary>
+        /// Receives the specified Message.
+        /// </summary>
+        /// <param name="msg">The MSG.</param>
         public override void Receive(string msg)
         {
             Console.WriteLine(this.name + " [Received Message] : " + msg);
-
         }
 
+        /// <summary>
+        /// Sends the specified Message.
+        /// </summary>
+        /// <param name="msg">The MSG.</param>
         public override void Send(string msg)
         {
             Console.WriteLine(this.name + " [Sending Message] : " + msg);
