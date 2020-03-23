1d80eedab4784e10a27d778ceacf877728933ab3
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-23 16:17:41 +0530

    [Nikhil]refactor final modification done

--- Behavioral/Visitor/ShoppingCartImpl.cs
+++ Behavioral/Visitor/ShoppingCartImpl.cs
@@ -9,27 +9,43 @@ namespace DesignPattern.Behavioral.Visitor
     using System;
     using System.Collections.Generic;
     using System.Text;
+
+    /// <summary>
+    /// implementation class of ShoppingCartVisitor
+    /// </summary>
+    /// <seealso cref="DesignPattern.Behavioral.Visitor.ShoppingCartVisitor" />
     public class ShoppingCartImpl : ShoppingCartVisitor
     {
+        /// <summary>
+        /// visit mobile method.
+        /// </summary>
+        /// <param name="mobile">The mobile.</param>
+        /// <returns>total price of mobile</returns>
         public int Visit(Mobile mobile)
         {
             int value = 0;
-            if(mobile.GetPrice()>20000)
+            if (mobile.GetPrice() > 20000)
             {
-                value = (mobile.GetPrice() - 1000)*mobile.GetQuantity();   //// 1000 discount in each if price is over 20000
+                value = (mobile.GetPrice() - 1000) * mobile.GetQuantity();   //// 1000 discount in each if price is over 20000
             }
             else
             {
-               value =  mobile.GetPrice() * mobile.GetQuantity();   //// else no discount
+                value = mobile.GetPrice() * mobile.GetQuantity();   //// else no discount
             }
-            Console.WriteLine("Total Price of Mobile : " + mobile.GetModelName() + " is " + value + " and quantity is " + mobile.GetQuantity()+", discount : "+1000*mobile.GetQuantity());
+
+            Console.WriteLine("Total Price of Mobile : " + mobile.GetModelName() + " is " + value + " and quantity is " + mobile.GetQuantity() + ", discount : " + 1000 * mobile.GetQuantity());
             return value;
         }
 
+        /// <summary>
+        /// Visit apparel method
+        /// </summary>
+        /// <param name="apparel">The apparel.</param>
+        /// <returns>total price of apparel</returns>
         public int Visit(Apparel apparel)
         {
             int value = 0;
-            if(apparel.GetPrice()*apparel.GetQuantity()>2000)
+            if (apparel.GetPrice() * apparel.GetQuantity() > 2000)
             {
                 value = (apparel.GetPrice() * apparel.GetQuantity()) - 100;      //// Rs 100 discount if total apparel valued over 2000
             }
@@ -37,8 +53,9 @@ namespace DesignPattern.Behavioral.Visitor
             {
                 value = apparel.GetPrice() * apparel.GetQuantity();  //// else no discount
             }
-            Console.WriteLine("Total Price of Apparel : " + apparel.GetBrandName() + " is " + value + " and quantity is " +apparel.GetQuantity());
-            return value; 
+
+            Console.WriteLine("Total Price of Apparel : " + apparel.GetBrandName() + " is " + value + " and quantity is " + apparel.GetQuantity());
+            return value;
         }
     }
 }
