9c4c4eb357f6f467b623b2c7032fce251e3f6d93
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-25 22:02:40 +0530

    [Nikhil]refactor implementation done

--- Logger/FileLogger.cs
+++ Logger/FileLogger.cs
@@ -3,8 +3,21 @@ namespace DesignPattern.Logger
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Text;
-   public class FileLogger
+    public class FileLogger : LogBase
     {
+        public string filepath = @"C:\Users\Nikhil Gupta\source\repos\CSharp\DesignPattern\Logger\LogFile.txt";
+        public override void Log(string message)
+        {
+            lock (lockobj)
+            {
+                using(StreamWriter swriter = new StreamWriter(filepath))
+                {
+                    swriter.WriteLine(message);
+                    swriter.Close();
+                }
+            }
+        }
     }
 }
