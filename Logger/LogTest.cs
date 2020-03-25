edeb048db6be7a4ee82cc11fffb1c2ea154cc685
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-25 23:18:58 +0530

    [Nikhil]refactor final modification done

--- Logger/LogTest.cs
+++ Logger/LogTest.cs
@@ -1,11 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿// -----------------------------------------------------------------------------------------------------------
+// <copyright file="LogTarget.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// --------------------------------------------------------------------------------------------------------
 namespace DesignPattern.Logger
 {
-   public class LogTest
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// for testing logger
+    /// </summary>
+    public class LogTest
     {
+        /// <summary>
+        /// Driver method.
+        /// </summary>
         public static void DriverMethod()
         {
             LogHelper.Log(LogTarget.File, "Log Message");
