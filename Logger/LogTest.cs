a13bba35ebcb3d07d0baddf04ad5d5e32582506a
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-25 22:52:46 +0530

    [Nikhil]new class created with implementation

---
+++ Logger/LogTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Logger
+{
+   public class LogTest
+    {
+        public static void DriverMethod()
+        {
+            LogHelper.Log(LogTarget.File, "Log Message");
+            Console.WriteLine("done");
+        }
+    }
+}
