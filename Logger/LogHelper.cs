3c7e956c97eb7aa7a40b59d5dbe18cf1e3475b50
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-25 22:44:09 +0530

    [Nikhil]refactor implementation done

---
+++ Logger/LogHelper.cs
@@ -0,0 +1,22 @@
+﻿
+namespace DesignPattern.Logger
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public static class LogHelper
+    {
+        public static void Log(LogTarget target, string message)
+        {
+            if(target==LogTarget.File)
+            {
+                FileLogger loger = new FileLogger();
+                loger.Log(message);
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
