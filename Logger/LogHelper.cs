2ad2e8562e027672dc3295117339bb99a3044a4a
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-25 23:14:36 +0530

    [Nikhil]refactor final modification done

--- Logger/LogHelper.cs
+++ Logger/LogHelper.cs
@@ -1,5 +1,4 @@
-﻿
-// -----------------------------------------------------------------------------------------------------------
+﻿// -----------------------------------------------------------------------------------------------------------
 // <copyright file="LogHelper.cs" company="Bridgelabz">
 //   Copyright © 2020 Company="BridgeLabz"
 // </copyright>
@@ -10,11 +9,20 @@ namespace DesignPattern.Logger
     using System;
     using System.Collections.Generic;
     using System.Text;
+
+    /// <summary>
+    /// static class to access Log Method
+    /// </summary>
     public static class LogHelper
     {
+        /// <summary>
+        /// Logs the specified target.
+        /// </summary>
+        /// <param name="target">The target.</param>
+        /// <param name="message">The message.</param>
         public static void Log(LogTarget target, string message)
         {
-            if(target==LogTarget.File)
+            if (target == LogTarget.File)
             {
                 FileLogger loger = new FileLogger();
                 loger.Log(message);
