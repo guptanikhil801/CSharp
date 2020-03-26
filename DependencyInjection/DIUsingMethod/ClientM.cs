cbca70b08f57738d60e45006570506e36e3f59a4
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-26 19:57:09 +0530

    [Nikhil]new class created and implemented

---
+++ DependencyInjection/DIUsingMethod/ClientM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.DependencyInjection.DIUsingMethod
+{
+   public class ClientM
+    {
+        private DIUsingConstructor.IService service;
+        public void Start(DIUsingConstructor.IService servicel)
+        {
+            servicel.Serve();
+        }
+    }
+}
