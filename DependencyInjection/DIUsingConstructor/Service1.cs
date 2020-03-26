90c7567aab0bd415997eaf325cc7407e92d35570
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-26 19:33:56 +0530

    [Nikhil]refactor final modification done

--- DependencyInjection/DIUsingConstructor/Service1.cs
+++ DependencyInjection/DIUsingConstructor/Service1.cs
@@ -1,5 +1,5 @@
 ﻿// -----------------------------------------------------------------------------------------------------------
-// <copyright file="IService.cs" company="Bridgelabz">
+// <copyright file="Service1.cs" company="Bridgelabz">
 //   Copyright © 2020 Company="BridgeLabz"
 // </copyright>
 // <creator name="Nikhil Gupta"/>
@@ -9,8 +9,16 @@ namespace DesignPattern.DependencyInjection.DIUsingConstructor
     using System;
     using System.Collections.Generic;
     using System.Text;
+
+    /// <summary>
+    /// 1st dependent class
+    /// </summary>
+    /// <seealso cref="DesignPattern.DependencyInjection.DIUsingConstructor.IService" />
     public class Service1 : IService
     {
+        /// <summary>
+        /// method of this class
+        /// </summary>
         public void Serve()
         {
             Console.WriteLine("Service1 called");
