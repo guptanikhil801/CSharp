3a8e53f86420debb9cbb0a430d4aadc355141d9e
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-26 20:18:25 +0530

    [Nikhil]refactor final modification done

--- DependencyInjection/DIUsingMethod/DITest.cs
+++ DependencyInjection/DIUsingMethod/DITest.cs
@@ -1,10 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿// -----------------------------------------------------------------------------------------------------------
+// <copyright file="DITest.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -----------------------------------------------------------------------------------------------------------
 namespace DesignPattern.DependencyInjection.DIUsingMethod
 {
-    class DITest
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using DesignPattern.DependencyInjection.DIUsingConstructor;
+
+    /// <summary>
+    /// main class to test DI using method
+    /// </summary>
+    public class DITest
     {
+        /// <summary>
+        /// Driver method.
+        /// </summary>
+        public static void DriverMethod()
+        {
+            Service1 s1 = new Service1();     //// creating object of dependecy 
+            ClientM client = new ClientM();
+            client.Start(s1);                //// passing dependency and calling method
+            Service2 s2 = new Service2();
+            client.Start(s2);
+        }
     }
 }
