a0c8e7b6593f6943713a403277f42becf376848d
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-26 21:31:22 +0530

    [Nikhil]refactor class implemented

---
+++ DependencyInjection/AutoFac/MessageService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.DependencyInjection.AutoFac
+{
+    public class MessageService : IMessage
+    {
+        public void Execute()
+        {
+            Console.WriteLine("you hava a new message");
+        }
+    }
+}
