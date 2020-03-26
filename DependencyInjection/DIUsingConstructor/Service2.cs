cd3b021e9e0666cf983095f21efaff012f3e407f
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-26 19:23:28 +0530

    [Nikhil]refactor file header added

--- DependencyInjection/DIUsingConstructor/Service2.cs
+++ DependencyInjection/DIUsingConstructor/Service2.cs
@@ -1,9 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿// -----------------------------------------------------------------------------------------------------------
+// <copyright file="Service2.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -----------------------------------------------------------------------------------------------------------
 namespace DesignPattern.DependencyInjection.DIUsingConstructor
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
     public class Service2 : IService
     {
         public void Serve()
