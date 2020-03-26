34cb56b6b44eb68a5a2eaf5971b43508ecffb847
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-26 20:14:41 +0530

    [Nikhil]refactor final modification done

--- DependencyInjection/DIUsingMethod/ClientM.cs
+++ DependencyInjection/DIUsingMethod/ClientM.cs
@@ -1,13 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿// -----------------------------------------------------------------------------------------------------------
+// <copyright file="ClientM.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -----------------------------------------------------------------------------------------------------------
 namespace DesignPattern.DependencyInjection.DIUsingMethod
 {
-   public class ClientM
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using DesignPattern.DependencyInjection.DIUsingConstructor;
+
+    /// <summary>
+    /// Test class
+    /// </summary>
+    public class ClientM
     {
-        private DIUsingConstructor.IService service;
-        public void Start(DIUsingConstructor.IService servicel)
+        private IService service;
+
+        /// <summary>
+        /// takes dependency class as parameter and calls its methods
+        /// </summary>
+        /// <param name="servicel">The servicel.</param>
+        public void Start(IService servicel)
         {
             servicel.Serve();
         }
