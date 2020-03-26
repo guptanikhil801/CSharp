d0c3a54aaefff847b0b1835d62c33e465262d789
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-26 17:56:44 +0530

    [Nikhil]refactor class created and implemented

---
+++ DependencyInjection/DIUsingConstructor/Service2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.DependencyInjection.DIUsingConstructor
+{
+    public class Service2 : IService
+    {
+        public void Serve()
+        {
+            Console.WriteLine("Service2 called");
+        }
+    }
+}
