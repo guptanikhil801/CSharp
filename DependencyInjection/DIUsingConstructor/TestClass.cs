e88a00ec4c4ae8dbf845d47475b067a3f1623fba
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-26 18:23:37 +0530

    [Nikhil]new class created and implemented

---
+++ DependencyInjection/DIUsingConstructor/TestClass.cs
@@ -0,0 +1,17 @@
+﻿
+namespace DesignPattern.DependencyInjection.DIUsingConstructor
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class TestClass
+    {
+        public static void DriverMethod()
+        {
+            Service1 s1 = new Service1();     //// creating object of dependecy 
+            Client c1 = new Client(s1);        /// passing dependency
+            Service2 s2 = new Service2();
+            Client c2 = new Client(s2);
+        }
+    }
+}
