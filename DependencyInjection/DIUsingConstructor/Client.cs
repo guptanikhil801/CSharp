b2ed1bb54850badf4a1d1e956327d3715c70eebf
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-26 19:29:46 +0530

    [Nikhil]refactor final modification done

--- DependencyInjection/DIUsingConstructor/Client.cs
+++ DependencyInjection/DIUsingConstructor/Client.cs
@@ -1,5 +1,5 @@
 ﻿// -----------------------------------------------------------------------------------------------------------
-// <copyright file="FileLogger.cs" company="Bridgelabz">
+// <copyright file="Client.cs" company="Bridgelabz">
 //   Copyright © 2020 Company="BridgeLabz"
 // </copyright>
 // <creator name="Nikhil Gupta"/>
@@ -11,13 +11,29 @@ namespace DesignPattern.DependencyInjection.DIUsingConstructor
     using System;
     using System.Collections.Generic;
     using System.Text;
+
+    /// <summary>
+    /// this is the class which has dependency on other classes
+    /// </summary>
     public class Client
     {
+        /// <summary>
+        /// The service type variable for dependent classes
+        /// </summary>
         private IService service;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Client"/> class.
+        /// </summary>
+        /// <param name="service">The service.</param>
         public Client(IService service)
         {
             this.service = service;
         }
+
+        /// <summary>
+        /// for calling methods of dependent class.
+        /// </summary>
         public void ServeMethod()
         {
             this.service.Serve();
