7643364c094c54a35bec824c9aed3c1b1ac6c5a5
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-26 19:39:55 +0530

    [Nikhil]refactor final modification done

--- DependencyInjection/DIUsingConstructor/TestClass.cs
+++ DependencyInjection/DIUsingConstructor/TestClass.cs
@@ -9,13 +9,20 @@ namespace DesignPattern.DependencyInjection.DIUsingConstructor
     using System;
     using System.Collections.Generic;
     using System.Text;
+
+    /// <summary>
+    /// main class to test dependency injection using constructor
+    /// </summary>
     public class TestClass
     {
+        /// <summary>
+        /// Driver method.
+        /// </summary>
         public static void DriverMethod()
         {
             Service1 s1 = new Service1();     //// creating object of dependecy 
-            Client c1 = new Client(s1);        /// passing dependency
-            c1.ServeMethod();
+            Client c1 = new Client(s1);       //// passing dependency
+            c1.ServeMethod();                 //// calling method
             Service2 s2 = new Service2();
             Client c2 = new Client(s2);
             c2.ServeMethod();
