9fb8287f4ab39fd04d45640c01e717ba94ba2964
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-26 22:29:22 +0530

    [Nikhil]new class created and implemented

---
+++ DependencyInjection/AutoFac/NotificationManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.DependencyInjection.AutoFac
+{
+   public class NotificationManager
+    {
+        public IMessage msgobj = null;
+        public IEmail mailobj = null;
+        public NotificationManager(IMessage msg)
+        {
+            msgobj = msg;
+        }
+        public IEmail SetEmail
+        {
+            set
+            {
+                mailobj = value;
+            }
+        }
+        public void SendNotification()
+        {
+            msgobj.Execute();
+            mailobj.Execute();
+        }
+    }
+}
