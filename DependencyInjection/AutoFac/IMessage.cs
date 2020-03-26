20cacb9b80c246cb9bef9b48d3948d30cf86cbbe
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-26 21:24:37 +0530

    [Nikhil]new Interface created and implemented

---
+++ DependencyInjection/AutoFac/IMessage.cs
@@ -0,0 +1,11 @@
+﻿
+namespace DesignPattern.DependencyInjection.AutoFac
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public interface IMessage
+    {
+        void Execute();
+    }
+}
