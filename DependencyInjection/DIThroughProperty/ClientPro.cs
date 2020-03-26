1f726e22aa2c69987408b2783589056b13fdfc91
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-26 20:45:25 +0530

    [Nikhil]new class created and implemented

---
+++ DependencyInjection/DIThroughProperty/ClientPro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DesignPattern.DependencyInjection.DIUsingConstructor;
+
+namespace DesignPattern.DependencyInjection.DIThroughProperty
+{
+   public class ClientPro
+    {
+        private IService servicep;
+        public IService Service
+        {
+            set
+            {
+                this.servicep = value;
+            }
+        }
+        public void ServeMethod()
+        {
+            servicep.Serve();
+        }
+    }
+}
