9a84fd69e1d4e4c7d4590f3857990d446fafd9be
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-26 23:59:22 +0530

    [Nikhil]refactor final modification done

--- DependencyInjection/AutoFac/EmailService.cs
+++ DependencyInjection/AutoFac/EmailService.cs
@@ -1,11 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿// -----------------------------------------------------------------------------------------------------------
+// <copyright file="EmailService.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -----------------------------------------------------------------------------------------------------------
 namespace DesignPattern.DependencyInjection.AutoFac
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// implementation class of IEmail
+    /// </summary>
+    /// <seealso cref="DesignPattern.DependencyInjection.AutoFac.IEmail" />
     public class EmailService : IEmail
     {
+        /// <summary>
+        /// method to execute email service
+        /// </summary>
         public void Execute()
         {
             Console.WriteLine("You have a new Mail");
