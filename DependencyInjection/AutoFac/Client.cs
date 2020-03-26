a9a5a7df815f09bf4de325fe258e07e3e62286b2
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-26 23:49:22 +0530

    [Nikhil]new class created and implemented

---
+++ DependencyInjection/AutoFac/Client.cs
@@ -0,0 +1,20 @@
+﻿
+namespace DesignPattern.DependencyInjection.AutoFac
+{
+    using Autofac;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class Client
+    {
+        public static void DriverMethod()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<MessageService>().As<IMessage>();
+            builder.RegisterType<EmailService>().As<IEmail>();
+            var container = builder.Build();
+            container.Resolve<IMessage>().Execute();
+            container.Resolve<IEmail>().Execute();
+        }
+    }
+}
