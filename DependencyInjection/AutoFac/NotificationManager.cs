6e969b51585873b9bc0f627469554d51e13cdde5
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-27 00:18:29 +0530

    [Nikhil]refactor final modification done

--- DependencyInjection/AutoFac/NotificationManager.cs
+++ DependencyInjection/AutoFac/NotificationManager.cs
@@ -1,28 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿// -----------------------------------------------------------------------------------------------------------
+// <copyright file="NotificationManager.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -----------------------------------------------------------------------------------------------------------
 namespace DesignPattern.DependencyInjection.AutoFac
 {
-   public class NotificationManager
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// This class has dependency on IMobile and IEmail
+    /// </summary>
+    public class NotificationManager
     {
-        public IMessage msgobj = null;
-        public IEmail mailobj = null;
+        private IMessage msgobj = null;
+        private IEmail mailobj = null;
+
+        /// <summary>
+        /// Injecting msgobj using constructor.
+        /// </summary>
+        /// <param name="msg">The MSG.</param>
         public NotificationManager(IMessage msg)
         {
-            msgobj = msg;
+            this.msgobj = msg;
         }
+
+        /// <summary>
+        /// Injecting mailobj using property.
+        /// </summary>
+        /// <value>
+        /// The set email.
+        /// </value>
         public IEmail SetEmail
         {
             set
             {
-                mailobj = value;
+                this.mailobj = value;
             }
+
         }
         public void SendNotification()
         {
-            msgobj.Execute();
-            mailobj.Execute();
+            this.msgobj.Execute();
+            this.mailobj.Execute();
         }
     }
 }
