83da2083645cc9a36825a198105b3db44b2dd6a8
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-27 16:56:53 +0530

    [Nikhil]refactor class defined

--- Automapping/EmployeeBlrBranch.cs
+++ Automapping/EmployeeBlrBranch.cs
@@ -5,5 +5,10 @@
     using System.Text;
     public  class EmployeeBlrBranch
     {
+        public string FullName { get; set; }
+        public int Contact { get; set; }
+        public string Address { get; set; }
+        public int EmpId { get; set; }
+        public string Department { get; set; }
     }
 }
