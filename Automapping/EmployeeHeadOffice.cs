62822b091c567a6d2d673ff2d077bc6a206a8ec1
guptanikhil801 <guptanikhil801@gmail.com>
2020-03-27 17:44:30 +0530

    [Nikhil]refactor final modification done

--- Automapping/EmployeeHeadOffice.cs
+++ Automapping/EmployeeHeadOffice.cs
@@ -9,12 +9,50 @@ namespace DesignPattern.Automapping
     using System;
     using System.Collections.Generic;
     using System.Text;
+
+    /// <summary>
+    /// this class will give data to another object
+    /// </summary>
     public class EmployeeHeadOffice
     {
+        /// <summary>
+        /// Gets or sets the full name.
+        /// </summary>
+        /// <value>
+        /// The full name.
+        /// </value>
         public string FullName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the contact.
+        /// </summary>
+        /// <value>
+        /// The contact.
+        /// </value>
         public long Contact { get; set; }
+
+        /// <summary>
+        /// Gets or sets the address.
+        /// </summary>
+        /// <value>
+        /// The address.
+        /// </value>
         public string Address { get; set; }
+
+        /// <summary>
+        /// Gets or sets the emp identifier.
+        /// </summary>
+        /// <value>
+        /// The emp identifier.
+        /// </value>
         public int EmpId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the department.
+        /// </summary>
+        /// <value>
+        /// The department.
+        /// </value>
         public string Department { get; set; }
     }
 }
