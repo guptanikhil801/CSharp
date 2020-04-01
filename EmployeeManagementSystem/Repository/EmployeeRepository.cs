7019c5c398369e4939a1453f280c9e1d723b5cc7
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-01 22:53:48 +0530

    Commenting and final modification done

--- EmployeeManagementSystem/Repository/EmployeeRepository.cs
+++ EmployeeManagementSystem/Repository/EmployeeRepository.cs
@@ -1,20 +1,38 @@
-﻿using EmployeeManagementSystem.Common;
-using EmployeeManagementSystem.Utility;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="EmployeeRepository.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
 namespace EmployeeManagementSystem.Repository
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.SqlClient;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using EmployeeManagementSystem.Common;
+    using EmployeeManagementSystem.Utility;
+    
+    /// <summary>
+    /// implementation class of IEmployeeRepository
+    /// </summary>
+    /// <seealso cref="EmployeeManagementSystem.Repository.IEmployeeRepository" />
     public class EmployeeRepository : IEmployeeRepository
     {
-        string connectionString = ConnectionString.CName;
+        /// <summary>
+        /// The connection to database
+        /// </summary>
+        private string connectionString = ConnectionString.CName;
+
+        /// <summary>
+        /// it Adds the employee to database.
+        /// </summary>
+        /// <param name="employee">The employee.</param>
         public void AddEmployee(Employee employee)
         {
-            using (SqlConnection con = new SqlConnection(connectionString))
+            using (SqlConnection con = new SqlConnection(this.connectionString))
             {
                 SqlCommand cmd = new SqlCommand("spAddEmployee", con);
                 cmd.CommandType = CommandType.StoredProcedure;
@@ -29,9 +47,13 @@ namespace EmployeeManagementSystem.Repository
             }
         }
 
+        /// <summary>
+        /// this method Deletes the employee from database
+        /// </summary>
+        /// <param name="Id">The identifier.</param>
         public void DeleteEmployee(int Id)
         {
-            using (SqlConnection con = new SqlConnection(connectionString))
+            using (SqlConnection con = new SqlConnection(this.connectionString))
             {
                 SqlCommand cmd = new SqlCommand("spDeleteEmployee", con);
                 cmd.CommandType = CommandType.StoredProcedure;
@@ -42,11 +64,14 @@ namespace EmployeeManagementSystem.Repository
             }
         }
 
-
+        /// <summary>
+        /// it shows all employee.
+        /// </summary>
+        /// <returns>list of employee</returns>
         public IEnumerable<Employee> GetAllEmployee()
         {
             List<Employee> lstemp = new List<Employee>();
-            using (SqlConnection con = new SqlConnection(connectionString))
+            using (SqlConnection con = new SqlConnection(this.connectionString))
             {
                 SqlCommand cmd = new SqlCommand("spGetEmployee", con);
                 cmd.CommandType = CommandType.StoredProcedure;
@@ -69,10 +94,15 @@ namespace EmployeeManagementSystem.Repository
             return lstemp;
         }
 
+        /// <summary>
+        /// this method has the logic to Gets the employee by its id.
+        /// </summary>
+        /// <param name="Empid">The empid.</param>
+        /// <returns>employee data</returns>
         public Employee GetEmployeeById(int Empid)
         {
             Employee employee = new Employee();
-            using (SqlConnection con = new SqlConnection(connectionString))
+            using (SqlConnection con = new SqlConnection(this.connectionString))
             {
                 string sqlQuery = "SELECT * FROM Employee WHERE Empid= " + Empid;
                 SqlCommand cmd = new SqlCommand(sqlQuery, con);
@@ -87,12 +117,17 @@ namespace EmployeeManagementSystem.Repository
                     employee.Password = rdr["Password"].ToString();
                 }
             }
+
             return employee;
         }
 
+        /// <summary>
+        /// This method Updates the employee details.
+        /// </summary>
+        /// <param name="employee">The employee.</param>
         public void UpdateEmployee(Employee employee)
         {
-            using (SqlConnection con = new SqlConnection(connectionString))
+            using (SqlConnection con = new SqlConnection(this.connectionString))
             {
                 SqlCommand cmd = new SqlCommand("spUpdateEmployee", con);
                 cmd.CommandType = CommandType.StoredProcedure;
