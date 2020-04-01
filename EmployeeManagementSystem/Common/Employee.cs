8e7eb2517a9907fe971c548ec087dcab98d9adae
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-01 22:13:53 +0530

    Commenting and final modification done

--- EmployeeManagementSystem/Common/Employee.cs
+++ EmployeeManagementSystem/Common/Employee.cs
@@ -1,15 +1,59 @@
-﻿namespace EmployeeManagementSystem.Common
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="Employee.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// ------------------------------------------------------------------------------------------------------
+namespace EmployeeManagementSystem.Common
 {
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
+
+    /// <summary>
+    /// Employee class to store info of employee
+    /// </summary>
     public class Employee
     {
+        /// <summary>
+        /// Gets or sets the empid.
+        /// </summary>
+        /// <value>
+        /// The employee id.
+        /// </value>
         public int Empid { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        /// <value>
+        /// The first name.
+        /// </value>
         public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last name.
+        /// </summary>
+        /// <value>
+        /// The last name.
+        /// </value>
         public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
         public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        /// <value>
+        /// The password.
+        /// </value>
         public string Password { get; set; }
     }
 }
