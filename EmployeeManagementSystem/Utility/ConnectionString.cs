b7f2787f6dc5ec225986741c448e673e91a785ff
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-01 23:04:31 +0530

    Commenting and final modification done

--- EmployeeManagementSystem/Utility/ConnectionString.cs
+++ EmployeeManagementSystem/Utility/ConnectionString.cs
@@ -1,13 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="ConnectionString.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
 namespace EmployeeManagementSystem.Utility
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    
+    /// <summary>
+    /// This class is for making connection with database
+    /// </summary>
     public class ConnectionString
     {
-        private static string cName = "Data Source=.; Initial Catalog=EmployeeManagement;Integrated Security=true";
+        /// <summary>
+        /// The cname variable to use further
+        /// </summary>
+        private static string cName = @"Data Source=.; Initial Catalog=EmployeeManagement;Integrated Security=true";
+
+        /// <summary>
+        /// Gets the Cname.
+        /// </summary>
+        /// <value>
+        /// The name of the CName.
+        /// </value>
         public static string CName
         {
             get => cName;
