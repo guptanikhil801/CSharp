ce39c06afc52ee179a58099214114bbabeb46a8b
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-02 18:51:04 +0530

    [Nikhil]refactor Login controller created

---
+++ EmployeeManagementSystem/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManagementSystem.Manager;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IEmployeeManager loginmanager;
+        public LoginController(IEmployeeManager loginmanager)
+        {
+            this.loginmanager = loginmanager;
+        }
+        public bool Login(string email,string password)
+        {
+           return this.loginmanager.Login(email, password);
+        }
+    }
+}
