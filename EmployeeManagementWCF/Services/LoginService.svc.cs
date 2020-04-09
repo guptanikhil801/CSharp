f6e7fb9fb54fbf825e5e11a9c70a1d40936891c8
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-09 17:09:09 +0530

    [Nikhil]refactor connection string added

--- EmployeeManagementWCF/Services/LoginService.svc.cs
+++ EmployeeManagementWCF/Services/LoginService.svc.cs
@@ -5,11 +5,16 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
-
+using System.Data.SqlClient;
+using System.Configuration;
 namespace EmployeeManagementWCF
 {
     public class LoginService : ILoginService
     {
-       
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["conString"].ConnectionString);
+        public bool LoginAuthentication(string email, string password)
+        {
+
+        }
     }
 }
