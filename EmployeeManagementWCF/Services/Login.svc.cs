5c97ad8cb2d3da94d9428d477f1629e2093f7410
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-10 11:36:17 +0530

    [Nikhil]refactor new Login service added

---
+++ EmployeeManagementWCF/Services/Login.svc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace EmployeeManagementWCF.Services
+{
+    public class Login : ILogin
+    {
+        public bool LoginAuthentication(string email, string password)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["conString"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("spCheckLoginWcf", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Email", email);
+            cmd.Parameters.AddWithValue("@Password", password);
+            con.Open();
+            using (SqlDataReader sdr = cmd.ExecuteReader())
+            {
+                if (sdr.HasRows)
+                {
+                    con.Close();
+                    return true;
+                }
+            }
+            con.Close();
+            return false;
+        }
+    }
+}
---
+++ EmployeeManagementWCF/Services/Service1.svc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+
+namespace EmployeeManagementWCF
+{
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
