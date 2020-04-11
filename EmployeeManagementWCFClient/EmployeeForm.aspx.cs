b06beb220578b83e22ef23dc9e04e0fc6e7fd77f
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-11 22:56:49 +0530

    [Nikhil]refactor final modification done

--- EmployeeManagementWCFClient/EmployeeForm.aspx.cs
+++ EmployeeManagementWCFClient/EmployeeForm.aspx.cs
@@ -1,33 +1,51 @@
 ﻿// -------------------------------------------------------------------------------------------------------
-// <copyright file="EmployeeForm.cs" company="Bridgelabz">
+// <copyright file="EmployeeForm.aspx.cs" company="Bridgelabz">
 //   Copyright © 2020 Company="BridgeLabz"
 // </copyright>
 // <creator name="Nikhil Gupta"/>
 // -------------------------------------------------------------------------------------------------------s
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using EmployeeManagementWCFClient.LoginReference;
-using EmployeeManagementWCFClient.EmployeeReference;
+
 namespace EmployeeManagementWCFClient
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Linq;
+    using System.Web;
+    using System.Web.UI;
+    using System.Web.UI.WebControls;
+    using EmployeeManagementWCFClient.EmployeeReference;
+    using EmployeeManagementWCFClient.LoginReference;
+
+    /// <summary>
+    /// this class contains code for page load and events
+    /// </summary>
+    /// <seealso cref="System.Web.UI.Page" />
     public partial class EmployeeForm : System.Web.UI.Page
     {
-        EmployeeReference.EmployeeServiceClient client = new EmployeeReference.EmployeeServiceClient();
+      protected EmployeeReference.EmployeeServiceClient client = new EmployeeReference.EmployeeServiceClient();
+
+        /// <summary>
+        /// Handles the Load event of the Page control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!Page.IsPostBack)
             {
                 DataSet ds = new DataSet();
-                ds = client.GetEmployeeDetails();
+                ds = this.client.GetEmployeeDetails();
                 grdAllEmployees.DataSource = ds;
                 grdAllEmployees.DataBind();
             }
         }
+
+        /// <summary>
+        /// this method contains code when Add Button will be clicked
+        /// </summary>
+        /// <param name="sender"> the object</param>
+        /// <param name="e">the EventArgs</param>
         protected void Add_Button_Click(object sender, EventArgs e)
         {
             Employee employee = new Employee();
@@ -36,8 +54,14 @@ namespace EmployeeManagementWCFClient
             employee.Email = EmailTextBox.Text.ToString();
             employee.Password = PasswordTextBox.Text.ToString();
             employee.DeptId = Convert.ToInt32(DeptIdTextBox.Text.ToString());
-            ResponseLabel.Text = client.AddEmployee(employee);
+            ResponseLabel.Text = this.client.AddEmployee(employee);
         }
+
+        /// <summary>
+        /// this method contains code when Update Button will be clicked
+        /// </summary>
+        /// <param name="sender">The object</param>
+        /// <param name="e">The EventArgs</param>
         protected void Update_Button_Click(object sender, EventArgs e)
         {
             Employee employee = new Employee();
@@ -47,18 +71,30 @@ namespace EmployeeManagementWCFClient
             employee.Email = EmailTextBox.Text.ToString();
             employee.Password = PasswordTextBox.Text.ToString();
             employee.DeptId = Convert.ToInt32(DeptIdTextBox.Text.ToString());
-            ResponseLabel.Text = client.UpdateEmployee(employee);
+            ResponseLabel.Text = this.client.UpdateEmployee(employee);
         }
+
+        /// <summary>
+        /// this method contains code when delete Button will be clicked
+        /// </summary>
+        /// <param name="sender">The Object</param>
+        /// <param name="e">the EventArgs</param>
         protected void Delete_Button_Click(object sender, EventArgs e)
         {
-          int id = int.Parse(EmpTextBox.Text.ToString());
-          ResponseLabel.Text = client.DeleteEmployee(id);
+            int id = int.Parse(EmpTextBox.Text.ToString());
+            ResponseLabel.Text = this.client.DeleteEmployee(id);
         }
+
+        /// <summary>
+        /// this method contains code when delete Button will be clicked
+        /// </summary>
+        /// <param name="sender">The Object</param>
+        /// <param name="e">the EventArgs</param>
         protected void EmployeeById_Button_Click(object sender, EventArgs e)
         {
             int id = int.Parse(EmpTextBox.Text.ToString());
             DataSet ds = new DataSet();
-            ds = client.EmployeeDetailsById(id);
+            ds = this.client.EmployeeDetailsById(id);
             grdAllEmployees.DataSource = ds;
             grdAllEmployees.DataBind();
         }
