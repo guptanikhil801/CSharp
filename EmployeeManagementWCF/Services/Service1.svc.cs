d5df4a804170ca8ee1edf57add7ad8da258ee7a7
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-11 23:59:26 +0530

    [Nikhil]refactor final modification done

--- EmployeeManagementWCF/Services/Service1.svc.cs
+++ EmployeeManagementWCF/Services/Service1.svc.cs
@@ -1,14 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.ServiceModel.Web;
-using System.Text;
-
-
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="Service1.svc.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
 namespace EmployeeManagementWCF
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.Serialization;
+    using System.ServiceModel;
+    using System.ServiceModel.Web;
+    using System.Text;
     public class Service1 : IService1
     {
         public string GetData(int value)
