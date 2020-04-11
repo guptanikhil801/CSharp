1183dac0cc892be55e675c7491fc44a1eafbc61f
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-11 20:28:15 +0530

    [Nikhil]refactor Add_ButtonClick() method implemented

--- EmployeeManagementWCFClient/EmployeeForm.aspx.cs
+++ EmployeeManagementWCFClient/EmployeeForm.aspx.cs
@@ -11,18 +11,26 @@ namespace EmployeeManagementWCFClient
 {
     public partial class EmployeeForm : System.Web.UI.Page
     {
+        EmployeeReference.EmployeeServiceClient client = new EmployeeReference.EmployeeServiceClient();
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!Page.IsPostBack)
             {
                 DataSet ds = new DataSet();
-                EmployeeReference.EmployeeServiceClient client = new EmployeeReference.EmployeeServiceClient();
                 ds = client.GetEmployeeDetails();
                 grdAllEmployees.DataSource = ds;
                 grdAllEmployees.DataBind();
             }
-
         }
-
+        protected void Add_Button_Click(object sender, EventArgs e)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = FirstNameTextBox.Text.ToString();
+            employee.LastName = LastNameTextBox.Text.ToString();
+            employee.Email = EmailTextBox.Text.ToString();
+            employee.Password = PasswordTextBox.Text.ToString();
+            employee.DeptId = Convert.ToInt32(DeptIdTextBox.Text.ToString());
+            client.AddEmployee(employee);
+        }
     }
 }
