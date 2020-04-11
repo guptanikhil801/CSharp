69ba05f99d1c1ce67559bf98766e2389de3ffcb4
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-11 23:09:50 +0530

    [Nikhil]refactor final modification done

--- EmployeeManagementWCFClient/LoginForm.aspx.cs
+++ EmployeeManagementWCFClient/LoginForm.aspx.cs
@@ -10,31 +10,48 @@ namespace EmployeeManagementWCFClient
     using System.Configuration;
     using System.Data;
     using System.Data.SqlClient;
-    using System.Collections.Generic;
     using System.Linq;
     using System.Web;
     using System.Web.UI;
     using System.Web.UI.WebControls;
-    using EmployeeManagementWCFClient.LoginReference;
     using EmployeeManagementWCFClient.EmployeeReference;
+    using EmployeeManagementWCFClient.LoginReference;
+
+    /// <summary>
+    /// This class contains code for Login Web Form
+    /// </summary>
+    /// <seealso cref="System.Web.UI.Page" />
     public partial class LoginForm : System.Web.UI.Page
     {
-        LoginReference.LoginClient client = new LoginReference.LoginClient();
+        protected LoginReference.LoginClient client = new LoginReference.LoginClient();
+
+        /// <summary>
+        /// Handles the Load event of the Page control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         protected void Page_Load(object sender, EventArgs e)
         {
-
         }
+
+        /// <summary>
+        /// Handles the Click event of the Login_Button control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         protected void Login_Button_Click(object sender, EventArgs e)
         {
             string email = EmailTextBox.Text.ToString();
             string password = PasswordTextBox.Text.ToString();
-                if (client.LoginAuthentication(email,password))
-                {
-                    ResponseLabel.Text = "Login Successfull";
+            if (this.client.LoginAuthentication(email, password))
+            {
+                ResponseLabel.Text = "Login Successfull";
                 Server.Transfer("EmployeeForm.aspx");
             }
-            else 
+            else
+            {
                 ResponseLabel.Text = "Invalid Email and Password combination";
+            }
         }
     }
 }
