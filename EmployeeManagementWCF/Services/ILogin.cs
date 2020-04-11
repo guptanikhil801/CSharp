11d65175681508bb8219f57742e192671996fc4a
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-11 23:47:30 +0530

    [Nikhil]refactor final modification done

--- EmployeeManagementWCF/Services/ILogin.cs
+++ EmployeeManagementWCF/Services/ILogin.cs
@@ -1,16 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.Text;
-
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="ILogin.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
 namespace EmployeeManagementWCF.Services
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.Serialization;
+    using System.ServiceModel;
+    using System.Text;
+
+    /// <summary>
+    /// Interface for Login Service
+    /// </summary>
     [ServiceContract]
     public interface ILogin
     {
+        /// <summary>
+        /// method for Login authentication.
+        /// </summary>
+        /// <param name="email">The email.</param>
+        /// <param name="password">The password.</param>
+        /// <returns>boolean value</returns>
         [OperationContract]
-        bool LoginAuthentication(String email, string password);
+        bool LoginAuthentication(string email, string password);
     }
 }
