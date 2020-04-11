5ac83f581482d1ad719910f858c3c154e4ee2c72
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-11 23:43:08 +0530

    [Nikhil]refactor final modification done

--- EmployeeManagementWCF/Services/IEmployeeService.cs
+++ EmployeeManagementWCF/Services/IEmployeeService.cs
@@ -14,36 +14,110 @@ namespace EmployeeManagementWCF.Services
     using System.ServiceModel;
     using System.Text;
 
+    /// <summary>
+    /// Interface for EmployeeService
+    /// </summary>
     [ServiceContract]
     public interface IEmployeeService
     {
+        /// <summary>
+        /// Adds the employee.
+        /// </summary>
+        /// <param name="emp">The emp.</param>
+        /// <returns>the response</returns>
         [OperationContract]
         string AddEmployee(Employee emp);
+
+        /// <summary>
+        /// Gets the employee details.
+        /// </summary>
+        /// <returns>employee list</returns>
         [OperationContract]
         DataSet GetEmployeeDetails();
+
+        /// <summary>
+        /// Employees details by id.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>the employee details</returns>
         [OperationContract]
         DataSet EmployeeDetailsById(int id);
+
+        /// <summary>
+        /// Deletes the employee.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>the response</returns>
         [OperationContract]
         string DeleteEmployee(int id);
+
+        /// <summary>
+        /// Updates the employee details.
+        /// </summary>
+        /// <param name="emp">The emp.</param>
+        /// <returns>The response</returns>
         [OperationContract]
         string UpdateEmployee(Employee emp);
     }
 
+    /// <summary>
+    /// To define Employee
+    /// </summary>
     [DataContract]
-public class Employee
+    public class Employee
     {
+        /// <summary>
+        /// Gets or sets the emp id.
+        /// </summary>
+        /// <value>
+        /// The emp identifier.
+        /// </value>
         [DataMember]
         public int EmpId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        /// <value>
+        /// The first name.
+        /// </value>
         [DataMember]
         public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last name.
+        /// </summary>
+        /// <value>
+        /// The last name.
+        /// </value>
         [DataMember]
         public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
         [DataMember]
         public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        /// <value>
+        /// The password.
+        /// </value>
         [DataMember]
         public string Password { get; set; }
+
+        /// <summary>
+        /// Gets or sets the dept identifier.
+        /// </summary>
+        /// <value>
+        /// The dept identifier.
+        /// </value>
         [DataMember]
         public int DeptId { get; set; }
     }
 }
-
