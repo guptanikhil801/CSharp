aca8a95b1965715ffb29ea242f4d98de4af9cf74
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-11 15:59:34 +0530

    [Nikhil]refactor code implemented in LoginForm.aspx.cs

--- EmployeeManagementWCFClient/LoginForm.aspx.cs
+++ EmployeeManagementWCFClient/LoginForm.aspx.cs
@@ -1,18 +1,34 @@
 ﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using EmployeeManagementWCFClient.LoginReference;
+using EmployeeManagementWCFClient.EmployeeReference;
 
 namespace EmployeeManagementWCFClient
 {
     public partial class LoginForm : System.Web.UI.Page
     {
+        LoginReference.LoginClient client = new LoginReference.LoginClient();
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        protected void Login_Button_Click(object sender, EventArgs e)
+        {
+            string email = EmailTextBox.Text.ToString();
+            string password = PasswordTextBox.Text.ToString();
+                if (client.LoginAuthentication(email,password))
+                {
+                    ResponseLabel.Text = "Login Successfull";
+                }
+            else 
+                ResponseLabel.Text = "unsucessfull";
         }
     }
 }
