b805ef83b556048ef0f2edd38040ec860b8bcb61
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-13 11:41:43 +0530

    [Nikhil]refactor validation done in Loggin Service

--- EmployeeManagementWCF/Services/Login.svc.cs
+++ EmployeeManagementWCF/Services/Login.svc.cs
@@ -15,6 +15,7 @@ namespace EmployeeManagementWCF.Services
     using System.Runtime.Serialization;
     using System.ServiceModel;
     using System.Text;
+    using System.Text.RegularExpressions;
 
     /// <summary>
     /// implementation class of ILogin
@@ -32,22 +33,25 @@ namespace EmployeeManagementWCF.Services
         /// </returns>
         public bool LoginAuthentication(string email, string password)
         {
-            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["conString"].ConnectionString);
-            SqlCommand cmd = new SqlCommand("spCheckLoginWcf", con);
-            cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.AddWithValue("@Email", email);
-            cmd.Parameters.AddWithValue("@Password", password);
-            con.Open();
-            using (SqlDataReader sdr = cmd.ExecuteReader())
+            Regex emailval = new Regex("^[A-Za-z]{1,30}[0-9]{0,20}@[A-Za-z]{1,10}.[A-Za-z]{1,10}$");
+            Regex passval = new Regex("^[A-Za-z0-9]{4,12}$");
+            if (emailval.IsMatch(email) && passval.IsMatch(password))
             {
-                if (sdr.HasRows)
+                SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["conString"].ConnectionString);
+                SqlCommand cmd = new SqlCommand("spCheckLoginWcf", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Email", email);
+                cmd.Parameters.AddWithValue("@Password", password);
+                con.Open();
+                using (SqlDataReader sdr = cmd.ExecuteReader())
                 {
-                    con.Close();
-                    return true;
+                    if (sdr.HasRows)
+                    {
+                        con.Close();
+                        return true;
+                    }
                 }
             }
-
-            con.Close();
             return false;
         }
     }
