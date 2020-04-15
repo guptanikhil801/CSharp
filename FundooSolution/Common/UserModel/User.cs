a58d7216471fa20baa4ae04bcb54877018d68ea6
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-15 18:34:36 +0530

    [Nikhil]new user.cs created with implementation

---
+++ FundooSolution/Common/UserModel/User.cs
@@ -0,0 +1,23 @@
+﻿namespace Common.UserModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Linq;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("UserTable")]
+    public class User
+    {
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        public int MobileNumber { get; set; }
+        [Key]
+        public string Email { get; set; }
+        [MaxLength(8), MinLength(4)]
+        public string Password { get; set; }
+    }
+}
