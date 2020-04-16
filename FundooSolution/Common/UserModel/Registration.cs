49a115d0d8265ab46ec5cdca3e8081869a476551
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-16 12:16:44 +0530

    [Nikhil]new Registration model class created with implementation

---
+++ FundooSolution/Common/UserModel/Registration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Common.UserModel
+{
+   public class Registration
+    {
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        public int MobileNumber { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [MaxLength(8), MinLength(4), Required]
+        public string Password { get; set; }
+       
+    }
+}
