d79a4f870ea84a36a9d760f6b97b64614d4d4ec6
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-16 12:21:24 +0530

    [Nikhil]new ForgotPassword model class created with implementation

---
+++ FundooSolution/Common/UserModel/ForgotPassword.cs
@@ -0,0 +1,13 @@
+﻿namespace Common.UserModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.ComponentModel.DataAnnotations;
+    
+    public class ForgotPassword
+    {
+        [Required]
+        public string Email { get; set; }
+    }
+}
