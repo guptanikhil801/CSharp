2f84e8eca17cf117da4b3e36cc365e8c8b2d3f61
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-16 12:26:31 +0530

    [Nikhil]new ResetPassword model class created with implementation

---
+++ FundooSolution/Common/UserModel/ResetPassword.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace Common.UserModel
+{
+   public class ResetPassword
+    {
+        [Required]
+        public string NewPassword { get; set; }
+        [Required]
+        public string ConfirmPassword { get; set; }
+    }
+}
