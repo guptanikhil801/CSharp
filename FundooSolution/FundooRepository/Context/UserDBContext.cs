f225b2705b723b0b95ad105c2289048c6a4b67f1
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-17 14:45:43 +0530

    [Nikhil]new Migration done after fixing all errors

--- FundooSolution/FundooRepository/Context/UserDBContext.cs
+++ FundooSolution/FundooRepository/Context/UserDBContext.cs
@@ -10,6 +10,10 @@ namespace FundooRepository.Context
 {
    public class UserDBContext : DbContext
     {
+        public UserDBContext(DbContextOptions<UserDBContext> options) : base(options)
+        {
+
+        }
         public DbSet<User> Users { get; set; }
     }
 }
---
+++ FundooSolution/FundooRepository/Migrations/20200417072535_initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FundooRepository.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserTable",
+                columns: table => new
+                {
+                    FirstName = table.Column<string>(maxLength: 50, nullable: true),
+                    LastName = table.Column<string>(maxLength: 50, nullable: true),
+                    MobileNumber = table.Column<int>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(maxLength: 8, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserTable", x => x.Email);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserTable");
+        }
+    }
+}
