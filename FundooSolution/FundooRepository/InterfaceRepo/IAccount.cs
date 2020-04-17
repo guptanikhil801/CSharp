14b287fa4dd7575872e0423f74e3b4b7bdecf36a
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-17 15:54:51 +0530

    [Nikhil]new IAccount interface created with implementation

---
+++ FundooSolution/FundooRepository/InterfaceRepo/IAccount.cs
@@ -0,0 +1,14 @@
+﻿namespace FundooRepository.InterfaceRepo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Common;
+   public interface IAccount
+    {
+        bool DoLogin();
+        bool DoRegistration();
+        bool ForgotPasswordUser();
+        bool ResetPasswordUser();
+    }
+}
