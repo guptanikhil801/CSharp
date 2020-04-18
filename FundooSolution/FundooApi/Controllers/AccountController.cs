92e4acadcb05e90e2a87b9b210483e45671d18c3
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-18 17:24:03 +0530

    [Nikhil]new controller created and some implementation done

---
+++ FundooSolution/FundooApi/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessManager.InterfaceManager;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Common.UserModel;
+
+namespace FundooApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountManager manager;
+        private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
+
+        AccountController(IAccountManager manager, UserManager<User> userManager
+            , SignInManager<User> signInManager)
+        {
+            this.manager = manager;
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+        // POST: api/Account
+        [HttpPost]
+        [Route("api/Account/Register")]
+        public async Task<IActionResult> Register([FromBody] RegistrationModel model)
+        {
+            var result = await this.userManager.CreateAsync(this.manager.DoRegistration(model), model.Password);
+            if (result.Succeeded)
+            {
+                return this.Ok();
+            }
+            else
+            {
+                return this.BadRequest();
+            }
+        }
+        [HttpPost]
+        [Route("api/Account/Login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            var result = await this.signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+            if (result.Succeeded)
+            {
+                return this.Ok();
+            }
+            else
+            {
+                return this.BadRequest();
+            }
+        }
+
+    }
+}
