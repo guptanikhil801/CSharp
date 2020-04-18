f78f798ac2cdb1cb6471681390cc911e2bdbcb77
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-18 17:21:29 +0530

    [Nikhil]new interface some implementation done

---
+++ FundooSolution/BusinessManager/ImplementationClassManager/AccountManagerImpl.cs
@@ -0,0 +1,39 @@
+﻿using BusinessManager.InterfaceManager;
+using Common.UserModel;
+using FundooRepository.InterfaceRepo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessManager.ImplementationClassManager
+{
+    public class AccountManagerImpl : IAccountManager
+    {
+        private readonly IAccount accountrepo;
+
+        AccountManagerImpl(IAccount accountrepo)
+        {
+            this.accountrepo = accountrepo;
+        }
+
+        public LoginModel DoLogin(LoginModel manager)
+        {
+            return this.accountrepo.DoLogin(manager);
+        }
+
+        public User DoRegistration(RegistrationModel manager)
+        {
+            return this.accountrepo.DoRegistration(manager);
+        }
+
+        public ForgotPassword ForgotPasswordUser(ForgotPassword manager)
+        {
+            return this.accountrepo.ForgotPasswordUser(manager);
+        }
+
+        public ResetPassword ResetPasswordUser(ResetPassword manager)
+        {
+            return this.accountrepo.ResetPasswordUser(manager);
+        }
+    }
+}
