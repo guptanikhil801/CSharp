279f8bab86c968ed8b02623aaac6c38b3e76f10d
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-18 15:44:20 +0530

    [Nikhil]new interface created and declared for account manager

---
+++ FundooSolution/BusinessManager/InterfaceManager/IAccountManager.cs
@@ -0,0 +1,15 @@
+﻿using Common.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessManager.InterfaceManager
+{
+   public interface IAccountManager
+    {
+        LoginModel DoLogin(LoginModel loginmodel);
+        User DoRegistration(RegistrationModel model);
+        ForgotPassword ForgotPasswordUser(ForgotPassword forgotmodel);
+        ResetPassword ResetPasswordUser(ResetPassword resetmodel);
+    }
+}
