87bae4c7a98da7db7ea41966d3ce9bd8de2a8567
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-18 17:24:59 +0530

    [Nikhil]refactor some changges done

--- FundooSolution/FundooApi/Startup.cs
+++ FundooSolution/FundooApi/Startup.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Common.UserModel;
+using FundooRepository.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -21,11 +25,19 @@ namespace FundooApi
         }
 
         public IConfiguration Configuration { get; }
+        public string UserDBConnection { get; private set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContextPool<UserDBContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("UserDBConnection"));
+              
+            });
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<UserDBContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +53,7 @@ namespace FundooApi
             }
 
             app.UseHttpsRedirection();
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
