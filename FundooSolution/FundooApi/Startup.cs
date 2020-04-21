d8efed427672195a216cfbf7583fd4ce900b08f0
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-21 20:53:27 +0530

    [Nikhil]refactor jwt configured in Startup.cs

--- FundooSolution/FundooApi/Startup.cs
+++ FundooSolution/FundooApi/Startup.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using BusinessManager.ImplementationClassManager;
+using BusinessManager.InterfaceManager;
 using Common.UserModel;
 using FundooRepository.Context;
+using FundooRepository.ImplementationClassRepo;
+using FundooRepository.InterfaceRepo;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -14,6 +20,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace FundooApi
 {
@@ -25,19 +32,45 @@ namespace FundooApi
         }
 
         public IConfiguration Configuration { get; }
-        public string UserDBConnection { get; private set; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+           
             services.AddDbContextPool<UserDBContext>(options =>
             {
                 options.UseSqlServer(Configuration.GetConnectionString("UserDBConnection"));
               
             });
-            services.AddIdentity<User, IdentityRole>()
-                .AddEntityFrameworkStores<UserDBContext>();
+            services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<UserDBContext>().AddDefaultTokenProviders();
+            services.AddTransient<IAccount, AccountImplClass>();
+            services.AddTransient<IAccountManager, AccountManagerImpl>();
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequiredLength = 4;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequiredUniqueChars = 0;
+                options.Password.RequireNonAlphanumeric = false;
+            });
+            services.AddAuthentication(Options =>
+            {
+                Options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                Options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                Options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(Options =>
+            {
+                Options.SaveToken = true;
+                Options.RequireHttpsMetadata = true;
+                Options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = Configuration["Jwt:Site"],
+                    ValidIssuer = Configuration["Jwt:Site"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Signingkey"]))
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
