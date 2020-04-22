4e4b6544a794ae0c8197ab685d7333dc84fdf7f5
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-22 13:05:22 +0530

    [Nikhil]refactor file header added and commenting done

--- FundooSolution/Common/UserModel/Login.cs
+++ FundooSolution/Common/UserModel/Login.cs
@@ -1,14 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.ComponentModel.DataAnnotations;
-
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="Login.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
 namespace Common.UserModel
 {
-   public class Login
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Text;
+
+    /// <summary>
+    /// model class for login
+    /// </summary>
+    public class LoginModel
     {
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
         [Required]
         public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        /// <value>
+        /// The password.
+        /// </value>
         [Required]
         public string Password { get; set; }
     }
