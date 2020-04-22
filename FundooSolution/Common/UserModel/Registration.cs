ff2a1cb71b5a63b35a3ae9bdc75401bee787bea6
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-22 13:04:56 +0530

    [Nikhil]refactor file header added and commenting done

--- FundooSolution/Common/UserModel/Registration.cs
+++ FundooSolution/Common/UserModel/Registration.cs
@@ -1,27 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
-
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="Registration.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
 namespace Common.UserModel
 {
-   public class RegistrationModel
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Text;
+
+    /// <summary>
+    /// model class for Registration of new user
+    /// </summary>
+    public class RegistrationModel
     {
-        [Display(Name ="First Name")]
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        /// <value>
+        /// The first name.
+        /// </value>
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last name.
+        /// </summary>
+        /// <value>
+        /// The last name.
+        /// </value>
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
-        public int PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the phone number.
+        /// </summary>
+        /// <value>
+        /// The phone number.
+        /// </value>
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
         [Required]
         [EmailAddress]
         public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        /// <value>
+        /// The password.
+        /// </value>
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        /// <summary>
+        /// Gets or sets the confirm password.
+        /// </summary>
+        /// <value>
+        /// The confirm password.
+        /// </value>
         [DataType(DataType.Password)]
-        [Display(Name ="Confirm password")]
-        [Compare("Password", 
-            ErrorMessage ="Password and confirmation password do not match")]
+        [Display(Name = "Confirm password")]
+        [Compare("Password",
+            ErrorMessage = "Password and confirmation password do not match")]
         public string ConfirmPassword { get; set; }
     }
 }
