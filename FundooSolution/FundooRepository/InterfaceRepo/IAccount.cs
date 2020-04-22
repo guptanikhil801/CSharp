b659de5b85b85b73474c49716603df9c93131154
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-22 13:26:10 +0530

    [Nikhil]refactor file header added and commenting done

--- FundooSolution/FundooRepository/InterfaceRepo/IAccount.cs
+++ FundooSolution/FundooRepository/InterfaceRepo/IAccount.cs
@@ -1,15 +1,44 @@
-﻿namespace FundooRepository.InterfaceRepo
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="IAccount.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
+namespace FundooRepository.InterfaceRepo
 {
     using System;
     using System.Collections.Generic;
     using System.Text;
     using System.Threading.Tasks;
     using Common.UserModel;
+
+    /// <summary>
+    /// Interface for operations in Account
+    /// </summary>
     public interface IAccount
     {
+        /// <summary>
+        /// method for login.
+        /// </summary>
+        /// <param name="loginmodel">The loginmodel.</param>
+        /// <returns>the user</returns>
         User DoLogin(LoginModel loginmodel);
+
+        /// <summary>
+        /// Does the registration.
+        /// </summary>
+        /// <param name="model">The model.</param>
+        /// <returns>new user</returns>
         User DoRegistration(RegistrationModel model);
-        bool ForgotPasswordUser(string email,string url);
-       // ResetPassword ResetPasswordUser(ResetPassword resetmodel);
+
+        /// <summary>
+        /// method for Forgot password
+        /// </summary>
+        /// <param name="email">The email.</param>
+        /// <param name="url">The URL.</param>
+        /// <returns>boolean value</returns>
+        bool ForgotPasswordUser(string email, string url);
+
+        // ResetPassword ResetPasswordUser(ResetPassword resetmodel);
     }
 }
