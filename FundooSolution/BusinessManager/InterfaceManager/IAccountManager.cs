83221c3438e570054268d4c85393766bf666d7d9
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-22 13:36:46 +0530

    [Nikhil]refactor file header added and commenting done

--- FundooSolution/BusinessManager/InterfaceManager/IAccountManager.cs
+++ FundooSolution/BusinessManager/InterfaceManager/IAccountManager.cs
@@ -1,15 +1,41 @@
-﻿using Common.UserModel;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="IAccountManager.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
 namespace BusinessManager.InterfaceManager
 {
-   public interface IAccountManager
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Common.UserModel;
+
+    /// <summary>
+    /// Account manager interface
+    /// </summary>
+    public interface IAccountManager
     {
+        /// <summary>
+        /// Does the login.
+        /// </summary>
+        /// <param name="manager">The manager.</param>
+        /// <returns>the user</returns>
         User DoLogin(LoginModel manager);
+
+        /// <summary>
+        /// Does the registration.
+        /// </summary>
+        /// <param name="manager">The manager.</param>
+        /// <returns>the user</returns>
         User DoRegistration(RegistrationModel manager);
+
+        /// <summary>
+        /// Forgot password method.
+        /// </summary>
+        /// <param name="email">The email.</param>
+        /// <param name="url">The URL.</param>
+        /// <returns>boolean value</returns>
         bool ForgotPasswordUser(string email, string url);
-       // ResetPassword ResetPasswordUser(ResetPassword manager);
     }
 }
