bf82b6dc80d711146b230059444bf014b7416fae
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-22 13:37:11 +0530

    [Nikhil]refactor file header added and commenting done

--- FundooSolution/BusinessManager/ImplementationClassManager/AccountManagerImpl.cs
+++ FundooSolution/BusinessManager/ImplementationClassManager/AccountManagerImpl.cs
@@ -1,39 +1,70 @@
-﻿using BusinessManager.InterfaceManager;
-using Common.UserModel;
-using FundooRepository.InterfaceRepo;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="AccountManagerImpl.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
 namespace BusinessManager.ImplementationClassManager
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using BusinessManager.InterfaceManager;
+    using Common.UserModel;
+    using FundooRepository.InterfaceRepo;
+
+    /// <summary>
+    /// implementation class of IAccountManager
+    /// </summary>
+    /// <seealso cref="BusinessManager.InterfaceManager.IAccountManager" />
     public class AccountManagerImpl : IAccountManager
     {
         private readonly IAccount accountrepo;
 
-      public  AccountManagerImpl(IAccount accountrepo)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AccountManagerImpl"/> class.
+        /// </summary>
+        /// <param name="accountrepo">The accountrepo.</param>
+        public AccountManagerImpl(IAccount accountrepo)
         {
             this.accountrepo = accountrepo;
         }
 
+        /// <summary>
+        /// Does the login.
+        /// </summary>
+        /// <param name="manager">The manager.</param>
+        /// <returns>
+        /// the user
+        /// </returns>
         public User DoLogin(LoginModel manager)
         {
             return this.accountrepo.DoLogin(manager);
         }
 
+        /// <summary>
+        /// Does the registration.
+        /// </summary>
+        /// <param name="manager">The manager.</param>
+        /// <returns>
+        /// the user
+        /// </returns>
         public User DoRegistration(RegistrationModel manager)
         {
             return this.accountrepo.DoRegistration(manager);
         }
 
+        /// <summary>
+        /// Forgot password method.
+        /// </summary>
+        /// <param name="email">The email.</param>
+        /// <param name="url">The URL.</param>
+        /// <returns>
+        /// boolean value
+        /// </returns>
         public bool ForgotPasswordUser(string email, string url)
         {
-            return this.accountrepo.ForgotPasswordUser(email,url);
+            return this.accountrepo.ForgotPasswordUser(email, url);
         }
-
-       /* public ResetPassword ResetPasswordUser(ResetPassword manager)
-        {
-            return this.accountrepo.ResetPasswordUser(manager);
-        }*/
     }
 }
