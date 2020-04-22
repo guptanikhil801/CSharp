8f61009b83b822d8e777cf9da060c150409866b7
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-22 13:24:55 +0530

    [Nikhil]refactor file header added and commenting done

--- FundooSolution/FundooRepository/Context/UserDBContext.cs
+++ FundooSolution/FundooRepository/Context/UserDBContext.cs
@@ -1,21 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Linq;
-using System.Data;
-using Microsoft.EntityFrameworkCore;
-using Common.UserModel;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="UserDbContext.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
 namespace FundooRepository.Context
 {
-   public class UserDBContext : IdentityDbContext<User>
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Linq;
+    using System.Text;
+    using Common.UserModel;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    /// <summary>
+    /// defining userdbcontext
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityDbContext{Common.UserModel.User}" />
+    public class UserDBContext : IdentityDbContext<User>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserDBContext"/> class.
+        /// </summary>
+        /// <param name="options">The options.</param>
         public UserDBContext(DbContextOptions<UserDBContext> options) : base(options)
         {
-
         }
+
+        /// <summary>
+        /// Gets or sets the DbSet of Users.
+        /// </summary>
         public new DbSet<User> Users { get; set; }
+
+        /// <summary>
+        /// Configures the schema needed for the identity framework.
+        /// </summary>
+        /// <param name="builder">The builder being used to construct the model for this context.</param>
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
