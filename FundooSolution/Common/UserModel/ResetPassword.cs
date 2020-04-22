a93c4659cf03d8cc4e66eb5f552310e16d9a2732
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-22 13:04:28 +0530

    [Nikhil]refactor file header added and commenting done

--- FundooSolution/Common/UserModel/ResetPassword.cs
+++ FundooSolution/Common/UserModel/ResetPassword.cs
@@ -1,17 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.ComponentModel.DataAnnotations;
-
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="ResetPassword.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
 namespace Common.UserModel
 {
-   public class ResetPassword
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Text;
+
+    /// <summary>
+    /// model class for ResetPassword
+    /// </summary>
+    public class ResetPassword
     {
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
         [Required]
         public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets new password.
+        /// </summary>
+        /// <value>
+        /// The new password.
+        /// </value>
         [Required]
         public string NewPassword { get; set; }
+
+        /// <summary>
+        /// Gets or sets the confirm password.
+        /// </summary>
+        /// <value>
+        /// The confirm password.
+        /// </value>
         [Required]
         public string ConfirmPassword { get; set; }
+
+        /// <summary>
+        /// Gets or sets the code.
+        /// </summary>
+        /// <value>
+        /// The code.
+        /// </value>
+        public string Code { get; set; }
     }
 }
