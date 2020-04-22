cf9be54cef8f39c8823b9eba297ca656be9ed0ae
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-22 13:04:00 +0530

    [Nikhil]refactor file header added and commenting done

--- FundooSolution/Common/UserModel/User.cs
+++ FundooSolution/Common/UserModel/User.cs
@@ -1,4 +1,9 @@
-﻿
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="User.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
 namespace Common.UserModel
 {
     using System;
@@ -9,11 +14,32 @@ namespace Common.UserModel
     using System.ComponentModel.DataAnnotations.Schema;
     using Microsoft.AspNetCore.Identity;
 
+    /// <summary>
+    /// class to define user
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Identity.IdentityUser" />
     public class User : IdentityUser
     {
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        /// <value>
+        /// The first name.
+        /// </value>
         [Required]
         public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last name.
+        /// </summary>
+        /// <value>
+        /// The last name.
+        /// </value>
         public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email address for this user.
+        /// </summary>
         [Key,Required]
         public override string Email { get; set; }
     }
