5b875e303be2195f07f0dde42d37a08dd683e65f
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-22 13:05:41 +0530

    [Nikhil]refactor file header added and commenting done

--- FundooSolution/Common/UserModel/ForgotPassword.cs
+++ FundooSolution/Common/UserModel/ForgotPassword.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.ComponentModel.DataAnnotations;
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="ForgotPassword.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
 
 namespace Common.UserModel
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Text;
+
+    /// <summary>
+    /// model class for forgot password
+    /// </summary>
     public class ForgotPassword
     {
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
         [Required]
         public string Email { get; set; }
     }
