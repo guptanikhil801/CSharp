e60b1098539438c0d555c023fa92925a7e97df54
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-23 20:59:15 +0530

    [Nikhil]new one method created and implemented

--- FundooSolution/FundooRepository/ImplementationClassRepo/NotesRepoImpl.cs
+++ FundooSolution/FundooRepository/ImplementationClassRepo/NotesRepoImpl.cs
@@ -7,12 +7,53 @@
     using Microsoft.AspNetCore.Http;
     using CloudinaryDotNet;
     using CloudinaryDotNet.Actions;
+    using Common.ModelsOfNotes;
+    using FundooRepository.Context;
+    using System.Linq;
 
     public class NotesRepoImpl : INotesRepository
     {
+        private readonly UserDBContext dbcontext;
+
+        public NotesRepoImpl( UserDBContext dbcontext)
+        {
+            this.dbcontext = dbcontext;
+        }
+
+        public bool AddNote(string email, NewNote notemodel, IFormFile file)
+        {
+            var imagelink = CloudImage(file);
+            var note = new NotesModel
+            {
+                Email = email,
+                Image = imagelink,
+                Description = notemodel.Description,
+                NoteId = notemodel.NoteId,
+                Reminder = notemodel.Reminder,
+                Title = notemodel.Title,
+                Colour = notemodel.Colour,
+                CreatedDate = DateTime.UtcNow,
+                ModifiedDate = DateTime.UtcNow,
+                IsArchive = false,
+                IsPin = false,
+                IsTrash = false,
+            };
+
+            var result = this.dbcontext.Notes.FirstOrDefault(o => o.NoteId == note.NoteId);
+            if(result == null)
+            {
+                this.dbcontext.Notes.Add(note);
+                if(this.dbcontext.SaveChanges() == 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
         public string CloudImage(IFormFile file)
         {
-            if(file == null)
+            if (file == null)
             {
                 return null;
             }
