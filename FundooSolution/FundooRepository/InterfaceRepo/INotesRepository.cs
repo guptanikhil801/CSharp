dfc2626c3b04de147ed4c8eb95e14f0646ea702e
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-23 16:24:47 +0530

    [Nikhil]new one method added in INotesRepository interface

--- FundooSolution/FundooRepository/InterfaceRepo/INotesRepository.cs
+++ FundooSolution/FundooRepository/InterfaceRepo/INotesRepository.cs
@@ -1,9 +1,11 @@
 ﻿namespace FundooRepository.InterfaceRepo
 {
+    using Microsoft.AspNetCore.Http;
     using System;
     using System.Collections.Generic;
     using System.Text;
     public interface INotesRepository
     {
+        string CloudImage(IFormFile file)
     }
 }
