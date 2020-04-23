5e02cec0ca17652b4b86dbdc4bf041b0d93e91ec
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-23 20:14:35 +0530

    [Nikhil]new model class implemented

--- FundooSolution/Common/ModelsOfNotes/NewNote.cs
+++ FundooSolution/Common/ModelsOfNotes/NewNote.cs
@@ -6,5 +6,11 @@ namespace Common.ModelsOfNotes
 {
    public class NewNote
     {
+        public int NoteId { get; set; }
+        public string Email { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Colour { get; set; }
+        public string Reminder { get; set; }
     }
 }
