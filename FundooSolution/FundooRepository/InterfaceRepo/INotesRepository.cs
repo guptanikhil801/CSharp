644346419ba467d05202991a063b8d0266b3ccdb
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-23 20:59:56 +0530

    [Nikhil]new one method declared

--- FundooSolution/FundooRepository/InterfaceRepo/INotesRepository.cs
+++ FundooSolution/FundooRepository/InterfaceRepo/INotesRepository.cs
@@ -1,11 +1,13 @@
 ﻿namespace FundooRepository.InterfaceRepo
 {
+    using Common.ModelsOfNotes;
     using Microsoft.AspNetCore.Http;
     using System;
     using System.Collections.Generic;
     using System.Text;
     public interface INotesRepository
     {
-        string CloudImage(IFormFile file)
+        string CloudImage(IFormFile file);
+        bool AddNote(string email, NewNote notemodel, IFormFile file);
     }
 }
