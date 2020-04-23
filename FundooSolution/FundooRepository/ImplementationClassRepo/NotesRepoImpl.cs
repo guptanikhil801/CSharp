a25fae1d28606d3f75bfe80cfa0a80d2e9782818
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-23 17:21:26 +0530

    [Nikhil]new cloudimage method created and implemented

---
+++ FundooSolution/FundooRepository/ImplementationClassRepo/NotesRepoImpl.cs
@@ -0,0 +1,31 @@
+﻿namespace FundooRepository.ImplementationClassRepo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using InterfaceRepo;
+    using Microsoft.AspNetCore.Http;
+    using CloudinaryDotNet;
+    using CloudinaryDotNet.Actions;
+
+    public class NotesRepoImpl : INotesRepository
+    {
+        public string CloudImage(IFormFile file)
+        {
+            if(file == null)
+            {
+                return null;
+            }
+            var filepath = file.OpenReadStream();
+            string uniquename = Guid.NewGuid().ToString() + "_" + file.FileName;
+            Account account = new Account("nikhilcloud007", "857761978269428", "Dp0h4mHl84o2zDL8HwINokEe6hM");
+            Cloudinary cloud = new Cloudinary(account);
+            var uploadparam = new ImageUploadParams()
+            {
+                File = new FileDescription(uniquename, filepath)
+            };
+            var uploadresult = cloud.Upload(uploadparam);
+            return uploadresult.Uri.ToString();
+        }
+    }
+}
