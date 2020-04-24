319cc039682242b97a2231ef529dca4d809f04cd
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-24 19:38:38 +0530

    [Nikhil]new new interface created

---
+++ FundooSolution/BusinessManager/InterfaceManager/INotesManager.cs
@@ -0,0 +1,10 @@
+﻿namespace BusinessManager.InterfaceManager
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public interface INotesManager
+    {
+    }
+}
