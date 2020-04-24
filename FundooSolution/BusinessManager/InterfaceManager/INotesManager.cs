93d8f91b4995e433f1f90f468f56926750414afe
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-24 20:04:50 +0530

    [Nikhil]refactor INotesManager methods declared

--- FundooSolution/BusinessManager/InterfaceManager/INotesManager.cs
+++ FundooSolution/BusinessManager/InterfaceManager/INotesManager.cs
@@ -1,10 +1,28 @@
 ﻿namespace BusinessManager.InterfaceManager
 {
+    using Common.ModelsOfNotes;
+    using Microsoft.AspNetCore.Http;
     using System;
     using System.Collections.Generic;
     using System.Text;
 
     public interface INotesManager
     {
+        string CloudImage(IFormFile file);
+        bool AddNote(string email, NewNote notemodel, IFormFile file);
+        List<NotesModel> PutDataToCache(string email);
+        List<NotesModel> RetreiveDataFromCache(string key);
+        NotesModel GetNote(string email, int id);
+        IEnumerable<NotesModel> GetAllNotes(string email);
+        bool UpdateNote(string email, NewNote note);
+        bool DeleteNote(string email, int id);
+        bool UpdateNoteImage(string email, int noteid, IFormFile imagefile);
+        bool Archive(string email, int id);
+        IEnumerable<NotesModel> ArchivedNoteList(String email);
+        bool Pin(string email, int id);
+        IEnumerable<NotesModel> PinnedNoteList(String email);
+        bool Trash(string email, int noteid);
+        IEnumerable<NotesModel> TrashNoteList(string email);
+        bool Restore(string email, int noteid);
     }
 }
