14071836af0a64a384f8fa96ee7057e2ca71ca9a
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-25 19:57:00 +0530

    [Nikhilnew methods declared

--- FundooSolution/FundooRepository/InterfaceRepo/ILabelRepository.cs
+++ FundooSolution/FundooRepository/InterfaceRepo/ILabelRepository.cs
@@ -1,10 +1,17 @@
 ﻿namespace FundooRepository.InterfaceRepo
 {
+    using Common.ModelsOfLabel;
     using System;
     using System.Collections.Generic;
     using System.Text;
 
    public interface ILabelRepository
     {
+        bool AddLabel(string email, string details);
+        LabelModel GetLabel(string email, int labelid);
+        IEnumerable<LabelModel> GetAllLabels(string email);
+        bool Update(string email, int labelid);
+        bool Delete(string email, int labelid);
+
     }
 }
