168add50b875fe98558ef226ce064c56cf91e355
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-25 20:03:32 +0530

    [Nikhilnew implementation class created

---
+++ FundooSolution/FundooRepository/ImplementationClassRepo/LabelRepoImpl.cs
@@ -0,0 +1,36 @@
+﻿namespace FundooRepository.ImplementationClassRepo
+{
+    using Common.ModelsOfLabel;
+    using FundooRepository.InterfaceRepo;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class LabelRepoImpl : ILabelRepository
+    {
+        public bool AddLabel(string email, string details)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(string email, int labelid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<LabelModel> GetAllLabels(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LabelModel GetLabel(string email, int labelid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(string email, int labelid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
