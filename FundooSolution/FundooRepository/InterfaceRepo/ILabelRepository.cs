b1cff3c2dc37669441cd9fe5259a1e2d60709189
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-25 19:41:56 +0530

    [Nikhilnew Interface created

---
+++ FundooSolution/FundooRepository/InterfaceRepo/ILabelRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace FundooRepository.InterfaceRepo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+   public interface ILabelRepository
+    {
+    }
+}
