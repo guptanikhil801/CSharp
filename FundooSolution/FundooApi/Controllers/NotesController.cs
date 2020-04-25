0e66726204285e234b2d0a8db91ed27703494dab
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-25 15:07:19 +0530

    [Nikhilrefactor constructor initialized

--- FundooSolution/FundooApi/Controllers/NotesController.cs
+++ FundooSolution/FundooApi/Controllers/NotesController.cs
@@ -4,13 +4,26 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
+    using BusinessManager.InterfaceManager;
+    using Common.UserModel;
+    using FundooRepository.Context;
     using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
 
-    [Route("api/[controller]")]
+    //[Route("api/[controller]")]
     [ApiController]
     public class NotesController : ControllerBase
     {
-        
+        private readonly UserManager<User> userManager;
+        private readonly UserDBContext dbcontext;
+        private readonly INotesManager manager;
+
+        public NotesController(UserManager<User> userManager, UserDBContext dbcontext, INotesManager manager)
+        {
+            this.userManager = userManager;
+            this.dbcontext = dbcontext;
+            this.manager = manager;
+        }
     }
 }
