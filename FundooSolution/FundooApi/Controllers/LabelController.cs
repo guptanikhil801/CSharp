431d0778efbbc8fc1a55d5eb220cbc6fb2fa3dcf
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-27 16:23:53 +0530

    [Nikhil]refactor Label controller created and constructor initialized

---
+++ FundooSolution/FundooApi/Controllers/LabelController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessManager.InterfaceManager;
+using Common.UserModel;
+using FundooRepository.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FundooApi.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class LabelController : ControllerBase
+    {
+        private readonly UserManager<User> userManager;
+        private readonly UserDBContext dbcontext;
+        private readonly ILabelManager manager;
+
+        public LabelController(UserManager<User> userManager, UserDBContext dbcontext, ILabelManager manager)
+        {
+            this.userManager = userManager;
+            this.dbcontext = dbcontext;
+            this.manager = manager;
+        }
+    }
+}
