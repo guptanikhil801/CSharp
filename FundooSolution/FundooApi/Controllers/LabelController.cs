e05f9c9a213bac9db167c92ff115eceb3ab3715b
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-27 16:38:29 +0530

    [Nikhil]refactor All actions defined in Label controller

--- FundooSolution/FundooApi/Controllers/LabelController.cs
+++ FundooSolution/FundooApi/Controllers/LabelController.cs
@@ -25,5 +25,85 @@ namespace FundooApi.Controllers
             this.dbcontext = dbcontext;
             this.manager = manager;
         }
+
+        [HttpPost]
+        [Route("api/Labels/AddLabel")]
+        public IActionResult AddLabel([FromBody]  string details)
+        {
+            string useremail = this.User.Identity.Name;
+            if (useremail != null)
+            {
+                if (manager.AddLabel(useremail, details))
+                {
+                    return this.Ok("Label created");
+                }
+            }
+
+            return this.BadRequest();
+        }
+
+        [HttpDelete]
+        [Route("api/Labels/DeleteLabel")]
+        public IActionResult DeleteNote([FromBody] int id)
+        {
+            string useremail = this.User.Identity.Name;
+            if (useremail != null)
+            {
+                if (this.manager.DeleteLabel(useremail, id))
+                {
+                    return this.Ok("Label deleted Successfully");
+                }
+            }
+
+            return this.BadRequest();
+        }
+
+        [HttpGet]
+        [Route("api/Labels/GetLabel")]
+        public IActionResult GetNote([FromBody] int id)
+        {
+            string useremail = this.User.Identity.Name;
+            if (useremail != null)
+            {
+                var label = manager.GetLabel(useremail, id);
+                {
+                    return this.Ok(label);
+                }
+            }
+
+            return this.BadRequest();
+        }
+
+        [HttpGet]
+        [Route("api/Notes/GetAllLabels")]
+        public IActionResult GetAllLabelss()
+        {
+            string useremail = this.User.Identity.Name;
+            if (useremail != null)
+            {
+                var alllabels = manager.GetAllLabels(useremail);
+                {
+                    return this.Ok(alllabels);
+                }
+            }
+
+            return this.BadRequest();
+        }
+
+        [HttpPut]
+        [Route("api/Notes/UpdateNote")]
+        public IActionResult UpdateNote([FromBody] String newdetail, int id)
+        {
+            string useremail = this.User.Identity.Name;
+            if (useremail != null)
+            {
+                if (this.manager.UpdateLabel(useremail, id, newdetail))
+                {
+                    return this.Ok("label has been updated successfully");
+                }
+            }
+
+            return this.BadRequest();
+        }
     }
 }
