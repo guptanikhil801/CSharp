35c0872b7308935a7ddaf19802cb583a968f538f
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-27 16:08:21 +0530

    methods declared in interface

--- FundooSolution/BusinessManager/InterfaceManager/ILabelManager.cs
+++ FundooSolution/BusinessManager/InterfaceManager/ILabelManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common.ModelsOfLabel;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,10 @@ namespace BusinessManager.InterfaceManager
 {
    public interface ILabelManager
     {
+        bool AddLabel(string email, string details);
+        LabelModel GetLabel(string email, int labelid);
+        IEnumerable<LabelModel> GetAllLabels(string email);
+        bool UpdateLabel(string email, int labelid, string details);
+        bool DeleteLabel(string email, int labelid);
     }
 }
