9c8596c914e7daa89a4b292bc5bc46888297998c
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-28 21:04:41 +0530

    [Nikhil]new model class created and defined for collabrator

---
+++ FundooSolution/Common/ModelsOfNotes/CollabModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Common.ModelsOfNotes
+{
+   public class CollabModel
+    {
+        [Required]
+        public string UserEmail { get; set; }
+        public int NoteId { get; set; }
+        [Key]
+        public int collabId { get; set; }
+        public string CollabratorEmail { get; set; }
+    }
+}
