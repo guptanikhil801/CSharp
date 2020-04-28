2debfce6f02b98874a88099dfff7562bb2dee66b
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-28 21:07:42 +0530

    [Nikhil]new interface created for manager of collabrator

---
+++ FundooSolution/BusinessManager/InterfaceManager/ICollabratorManager.cs
@@ -0,0 +1,14 @@
+﻿namespace BusinessManager.InterfaceManager
+{
+    using Common.ModelsOfNotes;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public interface ICollabratorManager
+    {
+        bool AddCollabrator(string useremail, string collabemail, int noteid);
+        bool RemoveCollabrator(int collabeid);
+        IEnumerable<CollabModel> GetAllCollabrator(string useremail);
+    }
+}
