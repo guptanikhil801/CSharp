6d7adbff600671555fe1f2792c5acc4bf01ca18b
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-28 21:05:43 +0530

    [Nikhil]new interface cretaed and methods declared as well for collabrators

---
+++ FundooSolution/FundooRepository/InterfaceRepo/CollabratorRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace FundooRepository.InterfaceRepo
+{
+    using Common.ModelsOfNotes;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+   public interface CollabratorRepository
+    {
+        bool AddCollabrator(string useremail, string collabemail, int noteid);
+        bool RemoveCollabrator(int collabeid);
+        IEnumerable<CollabModel> GetAllCollabrator(string useremail);
+    }
+}
