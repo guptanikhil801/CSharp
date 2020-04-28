5fecf2fd4f26a08a9f956ec38232d24447fbd98d
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-28 21:08:38 +0530

    [Nikhil]new implementation class created and methods implemented for manager of collabrator

---
+++ FundooSolution/BusinessManager/ImplementationClassManager/CollabratorManagerImpl.cs
@@ -0,0 +1,34 @@
+﻿namespace BusinessManager.ImplementationClassManager
+{
+    using BusinessManager.InterfaceManager;
+    using Common.ModelsOfNotes;
+    using FundooRepository.InterfaceRepo;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class CollabratorManagerImpl : ICollabratorManager
+    {
+        private readonly CollabratorRepository repository;
+
+        public CollabratorManagerImpl(CollabratorRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public bool AddCollabrator(string useremail, string collabemail, int noteid)
+        {
+            return this.repository.AddCollabrator(useremail, collabemail, noteid);
+        }
+
+        public IEnumerable<CollabModel> GetAllCollabrator(string useremail)
+        {
+            return this.repository.GetAllCollabrator(useremail);
+        }
+
+        public bool RemoveCollabrator(int collabeid)
+        {
+            return this.repository.RemoveCollabrator(collabeid);
+        }
+    }
+}
