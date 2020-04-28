bd7e1389790aedb5c5c828265da8f312a57bb50d
guptanikhil801 <guptanikhil801@gmail.com>
2020-04-28 21:09:42 +0530

    [Nikhil]new controller for collabrator alongg with action implemented

---
+++ FundooSolution/FundooApi/Controllers/CollabratorController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessManager.InterfaceManager;
+using Common.UserModel;
+using FundooRepository.Context;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FundooApi.Controllers
+{
+    [ApiController] [Authorize]
+    public class CollabratorController : ControllerBase
+    {
+        private readonly UserManager<User> userManager;
+        private readonly UserDBContext dbcontext;
+        private readonly ICollabratorManager manager;
+
+        public CollabratorController(UserManager<User> userManager, UserDBContext dbcontext, ICollabratorManager manager)
+        {
+            this.userManager = userManager;
+            this.dbcontext = dbcontext;
+            this.manager = manager;
+        }
+
+        [HttpPost]
+        [Route("api/Collabrators/AddCollabrator")]
+        public IActionResult AddCollabrator(string collabemail, int noteid)
+        {
+            string useremail = this.User.Identity.Name;
+            if (useremail != null)
+            {
+                if (manager.AddCollabrator(useremail, collabemail, noteid));
+                {
+                    return this.Ok("Collabrator Added");
+                }
+            }
+
+            return this.BadRequest();
+        }
+
+        [HttpGet]
+        [Route("api/Collabrators/GetAllCollabrators")]
+        public IActionResult GetAllCollabrators()
+        {
+            string useremail = this.User.Identity.Name;
+            if (useremail != null)
+            {
+                var alllabels = manager.GetAllCollabrator(useremail);
+                {
+                    return this.Ok(alllabels);
+                }
+            }
+
+            return this.BadRequest();
+        }
+
+        [HttpDelete]
+        [Route("api/Collabrators/RemoveCollabrator")]
+        public IActionResult RemoveCollabrator([FromBody] int id)
+        {
+            string useremail = this.User.Identity.Name;
+            if (useremail != null)
+            {
+                if (this.manager.RemoveCollabrator(id))
+                {
+                    return this.Ok("Collabrator Removed Successfully");
+                }
+            }
+
+            return this.BadRequest();
+        }
+    }
+}
