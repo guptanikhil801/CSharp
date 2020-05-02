ca09b7da655a5ffcabf415449cf7c95e8b9a2f26
guptanikhil801 <guptanikhil801@gmail.com>
2020-05-02 20:20:42 +0530

    deletelabel method defined for unit testing in Labelcontrollertesting

--- FundooSolution/FundooNotesXUnitTesting/LabelControllerTesting.cs
+++ FundooSolution/FundooNotesXUnitTesting/LabelControllerTesting.cs
@@ -23,5 +23,18 @@ namespace FundooNotesXUnitTesting
             //// Assert
             Assert.IsAssignableFrom<IActionResult>(result);
         }
+
+        [Fact]
+        public void DeleteLabel_Returns_OkResult()
+        {
+            //// Arrange
+            var service = new Mock<ILabelManager>();
+            var controller = new LabelController(service.Object);
+
+            //// Act
+            var result = controller.DeleteLabel(01,"newgmail801@gmail.com");
+            //// Assert
+            Assert.IsAssignableFrom<IActionResult>(result);
+        }
     }
 }
