c31c9338ae49f90dc422e4be3d42920d7a88a2df
guptanikhil801 <guptanikhil801@gmail.com>
2020-05-02 20:14:23 +0530

    NotesController Restored to its origginal state after successfull unit testing

---
+++ FundooSolution/FundooNotesXUnitTesting/LabelControllerTesting.cs
@@ -0,0 +1,27 @@
+﻿using BusinessManager.InterfaceManager;
+using FundooApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace FundooNotesXUnitTesting
+{
+   public class LabelControllerTesting
+    {
+        [Fact]
+        public void AddLabel_Returns_OkResult()
+        {
+            //// Arrange
+            var service = new Mock<ILabelManager>();
+            var controller = new LabelController(service.Object);
+
+            //// Act
+            var result = controller.AddLabel("new label","newgmail801@gmail.com");
+            //// Assert
+            Assert.IsAssignableFrom<IActionResult>(result);
+        }
+    }
+}
