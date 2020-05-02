0403e3d0a6210d989ee2dce98ad416c7ad72575d
guptanikhil801 <guptanikhil801@gmail.com>
2020-05-02 16:29:39 +0530

    A new class created for unit testing of notes controller

---
+++ FundooSolution/FundooNotesXUnitTesting/NotesControllerTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BusinessManager.InterfaceManager;
+using Common.ModelsOfNotes;
+using FundooApi.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace FundooNotesXUnitTesting
+{
+    public class NotesControllerTest
+    {
+        [Fact]
+        public void AddNote_Returns_OkResult()
+        {
+            //// Arrange
+            var service = new Mock<INotesManager>();
+            var controller = new NotesController(service.Object);
+            var model = new NewNote
+            {
+                Title = "new note",
+                Colour = "Blue",
+                Description = "new note created",
+                Reminder = "empty"
+            };
+
+
+            //// Act
+            var result = controller.AddNote(model, "newgmail801@gmail.com");
+            //// Assert
+            Assert.IsAssignableFrom<IActionResult>(result);
+        }
+
+      /*  [Fact]
+        public void UpdateNote_Returns_OkResult()
+        {
+            //// Arrange
+            var service = new Mock<INotesManager>();
+            var controller = new NotesController(service.Object);
+            var model = new NewNote
+            {
+                
+                Colour = "Pink",
+                Description = "updated Description",
+                Reminder = "Nothing to remember",
+                Title = "TestTitle"
+            };
+            
+            //// Act
+            var result = controller.UpdateNote(model, 01);
+            //// Assert
+            Assert.IsAssignableFrom<IActionResult>(result);
+        }*/
+    }
+}
