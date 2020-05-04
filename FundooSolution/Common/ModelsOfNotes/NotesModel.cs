eb8bd51270c4b3113f5332b340d7342e4e4ce41f
guptanikhil801 <guptanikhil801@gmail.com>
2020-05-04 21:21:24 +0530

    [Nikhil]refactor commenting and final modification done

--- FundooSolution/Common/ModelsOfNotes/NotesModel.cs
+++ FundooSolution/Common/ModelsOfNotes/NotesModel.cs
@@ -1,23 +1,117 @@
-﻿namespace Common.ModelsOfNotes
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="NotesModel.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
+
+namespace Common.ModelsOfNotes
 {
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.Text;
+
+    /// <summary>
+    /// model class of Notes
+    /// </summary>
     public class NotesModel
     {
+        /// <summary>
+        /// Gets or sets the note identifier.
+        /// </summary>
+        /// <value>
+        /// The note identifier.
+        /// </value>
         [Key]
         public int NoteId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
         public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        /// <value>
+        /// The title.
+        /// </value>
         public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        /// <value>
+        /// The description.
+        /// </value>
         public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the created date.
+        /// </summary>
+        /// <value>
+        /// The created date.
+        /// </value>
         public DateTime? CreatedDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the modified date.
+        /// </summary>
+        /// <value>
+        /// The modified date.
+        /// </value>
         public DateTime? ModifiedDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the image.
+        /// </summary>
+        /// <value>
+        /// The image.
+        /// </value>
         public string Image { get; set; }
+
+        /// <summary>
+        /// Gets or sets the colour.
+        /// </summary>
+        /// <value>
+        /// The colour.
+        /// </value>
         public string Colour { get; set; }
+
+        /// <summary>
+        /// Gets or sets the reminder.
+        /// </summary>
+        /// <value>
+        /// The reminder.
+        /// </value>
         public string Reminder { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is archive.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is archive; otherwise, <c>false</c>.
+        /// </value>
         public bool IsArchive { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is trash.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is trash; otherwise, <c>false</c>.
+        /// </value>
         public bool IsTrash { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is pin.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is pin; otherwise, <c>false</c>.
+        /// </value>
         public bool IsPin { get; set; }
         
     }
