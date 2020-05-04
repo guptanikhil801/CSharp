469d43481c821eff5789a7967d7d1dbe63e16427
guptanikhil801 <guptanikhil801@gmail.com>
2020-05-04 10:56:34 +0530

    [Nikhil]new AddCollabrator method defined in collabratorControllerTesting

--- FundooSolution/FundooNotesXUnitTesting/CollabratorControllerTesting.cs
+++ FundooSolution/FundooNotesXUnitTesting/CollabratorControllerTesting.cs
@@ -1,10 +1,27 @@
 ﻿namespace FundooNotesXUnitTesting
 {
+    using BusinessManager.InterfaceManager;
+    using FundooApi.Controllers;
+    using Microsoft.AspNetCore.Mvc;
+    using Moq;
     using System;
     using System.Collections.Generic;
     using System.Text;
+    using Xunit;
 
-   public class CollabratorControllerTesting
+    public class CollabratorControllerTesting
     {
+        [Fact]
+        public void AddCollabrator_Returns_OkResult()
+        {
+            //// Arrange
+            var service = new Mock<ICollabratorManager>();
+            var controller = new CollabratorController(service.Object);
+
+            //// Act
+            var result = controller.AddCollabrator("newgmail801@gmail.com", "guptanikhil801@gmail.com", 01);
+            //// Assert
+            Assert.IsAssignableFrom<IActionResult>(result);
+        }
     }
 }
