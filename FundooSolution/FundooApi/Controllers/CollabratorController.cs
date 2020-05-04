8b119af48d605a3a43ec151347ce1acbf22620fe
guptanikhil801 <guptanikhil801@gmail.com>
2020-05-04 21:21:44 +0530

    [Nikhil]refactor commenting and final modification done

--- FundooSolution/FundooApi/Controllers/CollabratorController.cs
+++ FundooSolution/FundooApi/Controllers/CollabratorController.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="CollabratorController.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,19 +19,44 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FundooApi.Controllers
 {
+    /// <summary>
+    /// Collabrator controller
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.ControllerBase" />
     [ApiController] [Authorize]
     public class CollabratorController : ControllerBase
     {
+        /// <summary>
+        /// The user manager
+        /// </summary>
         private readonly UserManager<User> userManager;
+
+        /// <summary>
+        /// The dbcontext
+        /// </summary>
         private readonly UserDBContext dbcontext;
+
+        /// <summary>
+        /// The manager
+        /// </summary>
         private readonly ICollabratorManager manager;
         private ICollabratorManager @object;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CollabratorController"/> class.
+        /// </summary>
+        /// <param name="object">The object.</param>
         public CollabratorController(ICollabratorManager @object)
         {
             this.@object = @object;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CollabratorController"/> class.
+        /// </summary>
+        /// <param name="userManager">The user manager.</param>
+        /// <param name="dbcontext">The dbcontext.</param>
+        /// <param name="manager">The manager.</param>
         public CollabratorController(UserManager<User> userManager, UserDBContext dbcontext, ICollabratorManager manager)
         {
             this.userManager = userManager;
@@ -32,6 +64,12 @@ namespace FundooApi.Controllers
             this.manager = manager;
         }
 
+        /// <summary>
+        /// Adds the collabrator.
+        /// </summary>
+        /// <param name="collabemail">The collabemail.</param>
+        /// <param name="noteid">The noteid.</param>
+        /// <returns>200 status if success otherwise 400 status</returns>
         [HttpPost]
         [Route("api/Collabrators/AddCollabrator")]
         public IActionResult AddCollabrator(string collabemail, int noteid)
@@ -48,6 +86,10 @@ namespace FundooApi.Controllers
             return this.BadRequest();
         }
 
+        /// <summary>
+        /// Gets all collabrators.
+        /// </summary>
+        /// <returns>all Collabrators</returns>
         [HttpGet]
         [Route("api/Collabrators/GetAllCollabrators")]
         public IActionResult GetAllCollabrators()
@@ -55,15 +97,20 @@ namespace FundooApi.Controllers
             string useremail = this.User.Identity.Name;
             if (useremail != null)
             {
-                var alllabels = manager.GetAllCollabrator(useremail);
+                var allcollabrator = manager.GetAllCollabrator(useremail);
                 {
-                    return this.Ok(alllabels);
+                    return this.Ok(allcollabrator);
                 }
             }
 
             return this.BadRequest();
         }
 
+        /// <summary>
+        /// Removes the collabrator.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>200 status if success otherwise 400 status</returns>
         [HttpDelete]
         [Route("api/Collabrators/RemoveCollabrator")]
         public IActionResult RemoveCollabrator([FromBody] int id)
