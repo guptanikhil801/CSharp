e2d8eb5f0eb50e71804ed9dabc735dea8e123586
guptanikhil801 <guptanikhil801@gmail.com>
2020-05-04 21:23:48 +0530

    [Nikhil]refactor commenting and final modification done

--- FundooSolution/FundooRepository/ImplementationClassRepo/CollabratorImpl.cs
+++ FundooSolution/FundooRepository/ImplementationClassRepo/CollabratorImpl.cs
@@ -1,4 +1,11 @@
-﻿namespace FundooRepository.ImplementationClassRepo
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="CollabratoImpl.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
+
+namespace FundooRepository.ImplementationClassRepo
 {
     using Common.ModelsOfNotes;
     using FundooRepository.Context;
@@ -8,15 +15,33 @@
     using System.Linq;
     using System.Text;
 
+    /// <summary>
+    /// implementation class of CollabratorRepository
+    /// </summary>
+    /// <seealso cref="FundooRepository.InterfaceRepo.CollabratorRepository" />
     public class CollabratorImpl : CollabratorRepository
     {
+        /// <summary>
+        /// The dbcontext
+        /// </summary>
         private readonly UserDBContext dbcontext;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CollabratorImpl"/> class.
+        /// </summary>
+        /// <param name="dbcontext">The dbcontext.</param>
         public CollabratorImpl( UserDBContext dbcontext)
         {
             this.dbcontext = dbcontext;
         }
 
+        /// <summary>
+        /// Adds the collabrator.
+        /// </summary>
+        /// <param name="useremail">The useremail.</param>
+        /// <param name="collabemail">The collabemail.</param>
+        /// <param name="noteid">The noteid.</param>
+        /// <returns>boolean value</returns>
         public bool AddCollabrator(string useremail, string collabemail, int noteid)
         {
             if(useremail!=null && collabemail!=null )
@@ -37,12 +62,22 @@
             return false;
         }
 
+        /// <summary>
+        /// Gets all collabrator.
+        /// </summary>
+        /// <param name="useremail">The useremail.</param>
+        /// <returns>boolean value</returns>
         public IEnumerable<CollabModel> GetAllCollabrator(string useremail)
         {
             var allcollab = dbcontext.Collabrators.Where(op => op.UserEmail == useremail);
             return allcollab;
         }
 
+        /// <summary>
+        /// Removes the collabrator.
+        /// </summary>
+        /// <param name="collabeid">The collabeid.</param>
+        /// <returns>boolean value</returns>
         public bool RemoveCollabrator(int collabeid)
         {
             var collabarator = dbcontext.Collabrators.FirstOrDefault(op => op.collabId == collabeid);
