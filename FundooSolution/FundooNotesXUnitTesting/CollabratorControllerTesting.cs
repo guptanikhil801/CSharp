a87da5c461f62786dbc9d92ccf34bfc517eda6b9
guptanikhil801 <guptanikhil801@gmail.com>
2020-05-04 11:02:39 +0530

    [Nikhil]new GetAllCollabrators and removeCollabrator method defined in collabratorControllerTesting

--- FundooSolution/FundooNotesXUnitTesting/CollabratorControllerTesting.cs
+++ FundooSolution/FundooNotesXUnitTesting/CollabratorControllerTesting.cs
@@ -23,5 +23,31 @@
             //// Assert
             Assert.IsAssignableFrom<IActionResult>(result);
         }
+
+        [Fact]
+        public void GetAllCollabrators_Returns_OkResult()
+        {
+            //// Arrange
+            var service = new Mock<ICollabratorManager>();
+            var controller = new CollabratorController(service.Object);
+
+            //// Act
+            var result = controller.GetAllCollabrators("newgmail801@gmail.com");
+            //// Assert
+            Assert.IsAssignableFrom<IActionResult>(result);
+        }
+
+        [Fact]
+        public void RemoveCollabrator_Returns_OkResult()
+        {
+            //// Arrange
+            var service = new Mock<ICollabratorManager>();
+            var controller = new CollabratorController(service.Object);
+
+            //// Act
+            var result = controller.RemoveCollabrator("newgmail801@gmail.com", 01);
+            //// Assert
+            Assert.IsAssignableFrom<IActionResult>(result);
+        }
     }
 }
