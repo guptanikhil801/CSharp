20b0ae2eb3910c1dbc8e73351f0e1d1487c5bee1
guptanikhil801 <guptanikhil801@gmail.com>
2020-05-04 21:20:57 +0530

    [Nikhil]refactor commenting and final modification done

--- FundooSolution/Common/ModelsOfNotes/NewNote.cs
+++ FundooSolution/Common/ModelsOfNotes/NewNote.cs
@@ -1,14 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="NewNote.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
 namespace Common.ModelsOfNotes
 {
-   public class NewNote
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// model class for adding a note
+    /// </summary>
+    public class NewNote
     {
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        /// <value>
+        /// The title.
+        /// </value>
         public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        /// <value>
+        /// The description.
+        /// </value>
         public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the colour.
+        /// </summary>
+        /// <value>
+        /// The colour.
+        /// </value>
         public string Colour { get; set; }
+
+        /// <summary>
+        /// Gets or sets the reminder.
+        /// </summary>
+        /// <value>
+        /// The reminder.
+        /// </value>
         public string Reminder { get; set; }
     }
 }
