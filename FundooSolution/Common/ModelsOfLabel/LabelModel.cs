839dbd631f3633a1548c3af5925070ae579e5adb
guptanikhil801 <guptanikhil801@gmail.com>
2020-05-04 21:20:15 +0530

    [Nikhil]refactor commenting and final modification done

--- FundooSolution/Common/ModelsOfLabel/LabelModel.cs
+++ FundooSolution/Common/ModelsOfLabel/LabelModel.cs
@@ -1,15 +1,45 @@
-﻿using System;
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="LabelModel.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Common.ModelsOfLabel
 {
-   public class LabelModel
+    /// <summary>
+    /// Model class for Label
+    /// </summary>
+    public class LabelModel
     {
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
         public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the label identifier.
+        /// </summary>
+        /// <value>
+        /// The label identifier.
+        /// </value>
         [Key]
         public int LabelId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the details.
+        /// </summary>
+        /// <value>
+        /// The details.
+        /// </value>
         public string Details { get; set; }
     }
 }
