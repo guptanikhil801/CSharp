1b444d6f0840c863eaf386eb008a12058b6b4108
guptanikhil801 <guptanikhil801@gmail.com>
2020-05-04 21:25:56 +0530

    [Nikhil]refactor commenting and final modification done

--- FundooSolution/FundooRepository/InterfaceRepo/ILabelRepository.cs
+++ FundooSolution/FundooRepository/InterfaceRepo/ILabelRepository.cs
@@ -1,16 +1,60 @@
-﻿namespace FundooRepository.InterfaceRepo
+﻿// -------------------------------------------------------------------------------------------------------
+// <copyright file="ILabelRepository.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Nikhil Gupta"/>
+// -------------------------------------------------------------------------------------------------------
+
+namespace FundooRepository.InterfaceRepo
 {
     using Common.ModelsOfLabel;
     using System;
     using System.Collections.Generic;
     using System.Text;
 
-   public interface ILabelRepository
+    /// <summary>
+    /// interface for label
+    /// </summary>
+    public interface ILabelRepository
     {
+        /// <summary>
+        /// Adds the label.
+        /// </summary>
+        /// <param name="email">The email.</param>
+        /// <param name="details">The details.</param>
+        /// <returns>true or false</returns>
         bool AddLabel(string email, string details);
+
+        /// <summary>
+        /// Gets the label.
+        /// </summary>
+        /// <param name="email">The email.</param>
+        /// <param name="labelid">The labelid.</param>
+        /// <returns>true or false</returns>
         LabelModel GetLabel(string email, int labelid);
+
+        /// <summary>
+        /// Gets all labels.
+        /// </summary>
+        /// <param name="email">The email.</param>
+        /// <returns>all labels</returns>
         IEnumerable<LabelModel> GetAllLabels(string email);
+
+        /// <summary>
+        /// Updates the label.
+        /// </summary>
+        /// <param name="email">The email.</param>
+        /// <param name="labelid">The labelid.</param>
+        /// <param name="details">The details.</param>
+        /// <returns>true or false</returns>
         bool UpdateLabel(string email, int labelid, string details);
+
+        /// <summary>
+        /// Deletes the label.
+        /// </summary>
+        /// <param name="email">The email.</param>
+        /// <param name="labelid">The labelid.</param>
+        /// <returns>true or false</returns>
         bool DeleteLabel(string email, int labelid);
 
     }
