a093d7b5f86335cfd37b22028f456fed1ba98357
guptanikhil801 <guptanikhil801@gmail.com>
2020-06-02 12:49:29 +0530

    addnote method added in interface

--- FundooNotesMVC/FundooRepository/InterfaceRepo/INote.cs
+++ FundooNotesMVC/FundooRepository/InterfaceRepo/INote.cs
@@ -1,14 +1,15 @@
-﻿using System;
+﻿using Common.NotesModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
 namespace FundooRepository.InterfaceRepo
 {
    public interface INote
     {
+        bool AddNote(AddNoteModel note);
         bool Archive(int Noteid);
         bool TrashAndUnTrash(int NoteId);
         bool NoteImage(int NoteId, string Imageurl);
-        bool ProfilePicture(string Email, string Imageurl);
-        
+        bool ProfilePicture(string Email, string Imageurl);      
     }
 }
