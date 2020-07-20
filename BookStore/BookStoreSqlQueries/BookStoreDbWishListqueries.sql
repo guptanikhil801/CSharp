6f63d4b81a46795312cd0a5b22abf4cce5aa334d
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-07-20 20:58:03 +0530

    stored procedures for updatebook and wishlists uploaded

---
+++ BookStore/BookStoreSqlQueries/BookStoreDbWishListqueries.sql
@@ -0,0 +1,52 @@
+﻿create table WishLists(
+WishlistId int primary key identity(1,1),
+Email NVARCHAR (MAX),
+BookId int );
+go
+
+CREATE PROCEDURE spAddBookToWishList     -- to add book to wishlist
+    @Email varchar(50),    
+    @BookId int
+  as 
+BEGIN TRY
+    Insert into WishLists(Email,BookId)           
+    Values(@Email,@BookId)
+END TRY
+BEGIN CATCH
+  SELECT
+    ERROR_NUMBER() AS ErrorNumber,
+    ERROR_MESSAGE() AS ErrorMessage;
+END CATCH
+  go
+
+  Create procedure spDeleteBookFromWishList           -- To delete book from wishlist
+(            
+    @WishlistId  int        
+)            
+as  
+BEGIN TRY                        
+   Delete from WishLists where WishlistId=@WishlistId 
+END TRY 
+BEGIN CATCH
+  SELECT
+    ERROR_NUMBER() AS ErrorNumber,
+    ERROR_MESSAGE() AS ErrorMessage;
+END CATCH                 
+go
+
+Create procedure spGetAllBooksFromWishLists                                         --  To Retreive All Books 
+(            
+    @Email varchar(50)        
+) 
+as           
+    
+BEGIN TRY                     
+   select * from WishLists where Email=@Email
+END TRY 
+BEGIN CATCH
+  SELECT
+    ERROR_NUMBER() AS ErrorNumber,
+    ERROR_PROCEDURE() AS ErrorProcedure,
+    ERROR_MESSAGE() AS ErrorMessage;
+END CATCH          
+go
---
+++ BookStore/BookStoreSqlQueries/BookStoreDbspUpdateBookByAdmin.sql
@@ -0,0 +1,28 @@
+﻿Create procedure    spUpdateBookByAdmin                                         -- to update Book details
+@BookId int,
+@Name NVARCHAR (MAX),   
+@Author NVARCHAR (MAX),
+@Description NVARCHAR (MAX),
+@Image NVARCHAR (MAX),
+@Price float,
+@AvailableStock int,
+@Rating float
+
+as  
+BEGIN TRY                     
+   Update books            
+   set Name=@Name,
+   Author=@Author,
+   Description=@Description,
+   Image=@Image,
+   Price=@Price,
+   AvailableStock=@AvailableStock,
+   Rating=@Rating
+   where BookId=@BookId 
+END TRY 
+BEGIN CATCH 
+  SELECT
+    ERROR_NUMBER() AS ErrorNumber,
+    ERROR_MESSAGE() AS ErrorMessage;
+END CATCH           
+go
