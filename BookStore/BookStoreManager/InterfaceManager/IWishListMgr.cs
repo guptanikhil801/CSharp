f4621d317ab4baa30102ce19ee9a6f98d7bb67c2
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-07-21 12:23:25 +0530

    wishlist interface manager added

---
+++ BookStore/BookStoreManager/InterfaceManager/IWishListMgr.cs
@@ -0,0 +1,12 @@
+﻿using BookStoreModal.Modals;
+using System.Collections.Generic;
+
+namespace BookStoreManager.InterfaceManager
+{
+    public interface IWishListMgr
+    {
+        bool AddBookToWishList(string Email, int BookId);
+        bool DeleteBookFromWishList(int WishListId);
+        IEnumerable<WishList> GetallWishLists(string Email);
+    }
+}
