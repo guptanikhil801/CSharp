55e1db645b0dc240e7a0ceef134d68978d291f4e
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-07-21 12:26:01 +0530

    wishlist manager class added

---
+++ BookStore/BookStoreManager/ImplManager/WishListMgr.cs
@@ -0,0 +1,27 @@
+﻿using BookStoreManager.InterfaceManager;
+using BookStoreModal.Modals;
+using BookStoreRepository.ImplementationRepo;
+using System.Collections.Generic;
+
+namespace BookStoreManager.ImplManager
+{
+    public class WishListMgr : IWishListMgr
+    {
+        private WishListRepoImpl repo = new WishListRepoImpl();
+
+        public bool AddBookToWishList(string Email, int BookId)
+        {
+            return repo.AddBookToWishList(Email, BookId);
+        }
+
+        public bool DeleteBookFromWishList(int WishListId)
+        {
+            return repo.DeleteBookFromWishList(WishListId);
+        }
+
+        public IEnumerable<WishList> GetallWishLists(string Email)
+        {
+            return repo.GetallWishLists(Email);
+        }
+    }
+}
