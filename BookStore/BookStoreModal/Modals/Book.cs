35445de0a1d366b358a8c78cf3b0eb6f00811296
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-07-21 12:17:17 +0530

    modal class of wishlist added

--- BookStore/BookStoreModal/Modals/Book.cs
+++ BookStore/BookStoreModal/Modals/Book.cs
@@ -2,6 +2,7 @@
 {
     public class Book
     {
+
         public int BookId { get; set; }
         public string Name { get; set; }
         public string Author { get; set; }
---
+++ BookStore/BookStoreModal/Modals/WishList.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStoreModal.Modals
+{
+    public class WishList
+    {
+        public int WishListId { get; set; }
+        public string Email { get; set; }
+        public int BookId { get; set; }
+    }
+}
