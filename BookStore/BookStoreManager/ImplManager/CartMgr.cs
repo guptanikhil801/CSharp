9f6178a3022b526ab84413b0c59ff6e604b1002f
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-07-22 20:28:32 +0530

    manager class added for cart

---
+++ BookStore/BookStoreManager/ImplManager/CartMgr.cs
@@ -0,0 +1,31 @@
+﻿using BookStoreManager.InterfaceManager;
+using BookStoreModal.Modals;
+using BookStoreRepository.ImplementationRepo;
+using System.Collections.Generic;
+
+namespace BookStoreManager.ImplManager
+{
+    public class CartMgr : ICartMgr
+    {
+        private CartRepoImpl repo = new CartRepoImpl();
+        public bool AddBookToCart(string Email, int BookId)
+        {
+            return repo.AddBookCart(Email, BookId);
+        }
+
+        public bool DeleteBookFromCart(int CartId)
+        {
+            return repo.DeleteBookFromCart(CartId);
+        }
+
+        public IEnumerable<Cart> GetallBooksOfCart(string Email)
+        {
+            return repo.GetallBooksOfCart(Email);
+        }
+
+        public bool UpdateCart(int CartId, int BookQuantity)
+        {
+            return UpdateCart(CartId, BookQuantity);
+        }
+    }
+}
