ad7942353fc2c5837bec40b59c8bbb8bb3f085fb
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-07-22 20:21:59 +0530

    stored procedure for addbooktocart done

---
+++ BookStore/BookStoreSqlQueries/BookStoreDbspAddBookToCart.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE spAddBookToCart    -- to add book to Cart
+    @Email varchar(50),    
+    @BookId int
+  as 
+BEGIN TRY
+    Insert into Carts(Email,BookId)           
+    Values(@Email,@BookId)
+END TRY
+BEGIN CATCH
+  SELECT
+    ERROR_NUMBER() AS ErrorNumber,
+    ERROR_MESSAGE() AS ErrorMessage;
+END CATCH
+  go
