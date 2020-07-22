95a15b39720017594ad3a27eb44279a48e76ccc0
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-07-22 20:27:13 +0530

    manager interface for cart done

---
+++ BookStore/BookStoreManager/InterfaceManager/ICartMgr.cs
@@ -0,0 +1,13 @@
+﻿using BookStoreModal.Modals;
+using System.Collections.Generic;
+
+namespace BookStoreManager.InterfaceManager
+{
+    public interface ICartMgr
+    {
+        bool AddBookToCart(string Email, int BookId);
+        bool UpdateCart(int CartId, int BookQuantity);
+        bool DeleteBookFromCart(int CartId);
+        IEnumerable<Cart> GetallBooksOfCart(string Email);
+    }
+}
