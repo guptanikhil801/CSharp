1ff661ccea63edf98a0baf464c79acfefbc8eb6d
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-07-22 19:32:19 +0530

    Cart modal class defined

---
+++ BookStore/BookStoreModal/Modals/Cart.cs
@@ -0,0 +1,10 @@
+﻿namespace BookStoreModal.Modals
+{
+    public class Cart
+    {
+        public int CartId { get; set; }
+        public string Email { get; set; }
+        public int BookId { get; set; }
+        public int BookQuantity { get; set; }
+    }
+}
