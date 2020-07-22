029bc889e8b2fd280988ac304cace8414305b79d
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-07-22 20:23:55 +0530

    interface for cart added

---
+++ BookStore/BookStoreRepository/InterfacesRepo/ICartRepo.cs
@@ -0,0 +1,17 @@
+﻿using BookStoreModal.Modals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreRepository.InterfacesRepo
+{
+    public interface ICartRepo
+    {
+        bool AddBookCart(string Email, int BookId);
+        bool UpdateCart(int CartId, int BookQuantity);
+        bool DeleteBookFromCart(int CartId);
+        IEnumerable<Cart> GetallBooksOfCart(string Email);
+    }
+}
