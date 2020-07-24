b2c3ba6a6006c41475a483eca9119ecb0515d44c
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-07-24 22:15:40 +0530

    BookInCart modal class defined

--- BookStore/BookStoreModal/Modals/Book.cs
+++ BookStore/BookStoreModal/Modals/Book.cs
@@ -2,7 +2,6 @@
 {
     public class Book
     {
-
         public int BookId { get; set; }
         public string Name { get; set; }
         public string Author { get; set; }
---
+++ BookStore/BookStoreModal/Modals/BookInCart.cs
@@ -0,0 +1,13 @@
+﻿namespace BookStoreModal.Modals
+{
+    public class BookInCart
+    {
+        public int CartId { get; set; }
+        public int BookId { get; set; }
+        public int BookQuantity { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+    }
+}
