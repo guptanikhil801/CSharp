7f5beac836a639a7b01b805cf97e3349f86a071c
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-07-26 22:30:40 +0530

    stored procedure for customerdetails added

---
+++ BookStore/BookStoreSqlQueries/BookStoreDbspCustomerDetails.sql
@@ -0,0 +1,14 @@
+﻿create procedure spCustomerDetails
+(
+@Email varchar(50)
+)
+as
+begin try
+select FirstName, LastName, PhoneNumber, Address, City, PinCode from Customers
+where Email=@Email
+End try
+BEGIN CATCH
+  SELECT
+    ERROR_NUMBER() AS ErrorNumber,
+    ERROR_MESSAGE() AS ErrorMessage;
+END CATCH
