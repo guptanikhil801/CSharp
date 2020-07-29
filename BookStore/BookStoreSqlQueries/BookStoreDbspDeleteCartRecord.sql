f931fe2eeb280275acb5697292cb8b2ba773a71a
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-07-29 21:58:45 +0530

    stored procedure spDeleteCartRecord added

---
+++ BookStore/BookStoreSqlQueries/BookStoreDbspDeleteCartRecord.sql
@@ -0,0 +1,13 @@
+﻿  Create procedure spDeleteCartRecord           -- To delete cart after order
+(            
+    @Email  nvarchar(max)       
+)            
+as  
+BEGIN TRY                        
+   Delete from Carts where Email=@Email
+END TRY 
+BEGIN CATCH
+  SELECT
+    ERROR_NUMBER() AS ErrorNumber,
+    ERROR_MESSAGE() AS ErrorMessage;
+END CATCH
