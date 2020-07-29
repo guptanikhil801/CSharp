56bd1c4b5909c92659234151d59f650c756fdcdc
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-07-29 22:00:05 +0530

    a method added

--- BookStore/BookStoreRepository/InterfacesRepo/ICartRepo.cs
+++ BookStore/BookStoreRepository/InterfacesRepo/ICartRepo.cs
@@ -8,6 +8,7 @@ namespace BookStoreRepository.InterfacesRepo
         bool AddBookCart(string Email, int BookId);
         bool UpdateCart(int CartId, int BookQuantity);
         bool DeleteBookFromCart(int CartId);
+        bool DeleteCartRecord(string Email);
         IEnumerable<BookInCart> GetallBooksOfCart(string Email);
     }
 }
