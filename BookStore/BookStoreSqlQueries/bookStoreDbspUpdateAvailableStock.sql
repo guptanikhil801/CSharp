489c19f1e7fc32f6ae0c986ff3b1725f850e8763
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-07-31 17:07:39 +0530

    stored procedure for updating available stock

---
+++ BookStore/BookStoreSqlQueries/bookStoreDbspUpdateAvailableStock.sql
@@ -0,0 +1,16 @@
+﻿Create procedure    spUpdateAvailableStock                                         -- to update Book details
+@BookId int,
+@UpdatedAvailableStock int
+
+as  
+BEGIN TRY                     
+   Update books            
+   set 
+   AvailableStock=@UpdatedAvailableStock
+   where BookId=@BookId 
+END TRY 
+BEGIN CATCH 
+  SELECT
+    ERROR_NUMBER() AS ErrorNumber,
+    ERROR_MESSAGE() AS ErrorMessage;
+END CATCH
