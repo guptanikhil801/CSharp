5a72fac41da001427542f0ddb60267cd41694594
Nikhil Gupta <55235656+guptanikhil801@users.noreply.github.com>
2020-08-06 15:53:34 +0530

    final project uploaded

--- BookStore/BookStoreModal/Modals/Customer.cs
+++ BookStore/BookStoreModal/Modals/Customer.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BookStoreModal.Modals
+﻿namespace BookStoreModal.Modals
 {
     /// <summary>
     /// class to deine Customer
